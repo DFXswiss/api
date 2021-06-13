95286b53b117cf6352917a0f28ef458c0fb40ec9
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-06-14 01:40:18 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,17 +1,10 @@
-
-# TODO
-# Doku anzeigen lassen (swagger.json oder yaml)
-# Banküberweisungen ziehen via API: Verwendungszweck,
-# Transaktionsgebühren: legacy adresse, signatur, wallet id? -> Robin?
-# UpdateServer secret + GitHub webhooks
-# Config von DB in extra File
 import dash
 import dash_html_components as html
-import appIndexString
+from appIndexString import appIndexStringClass
 
 app = dash.Dash(__name__)
 app.layout =html.Div("")
-app.index_string = appIndexString.getAppIndexString()
+app.index_string = appIndexStringClass.getAppIndexString()
 
 from routes import *
 
