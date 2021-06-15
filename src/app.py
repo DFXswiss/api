9cee52078978f60864fb5cf1574744c666ee8a2e
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-06-15 14:27:31 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,6 +6,5 @@ app = Flask(__name__)
 app.layout =html.Div("")
 app.index_string = appIndexStringClass.getAppIndexString()
 from routes import *
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2)
 if __name__ == "__main__":
    app.run()
