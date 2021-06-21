abf1bd4518fc342b9ed8a20aeaa86347d738f6f3
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-06-22 00:24:41 +0200

    Revert "Test"
    This reverts commit 472daff3aafdf81f557f2fda70eda870264cf3dc.

--- api-dfx/dist/app.service.js
+++ api-dfx/dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello Test!';
+        return 'Hello World!';
     }
 };
 AppService = __decorate([
