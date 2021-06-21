472daff3aafdf81f557f2fda70eda870264cf3dc
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-06-22 00:22:53 +0200

    Test

--- api-dfx/dist/app.service.js
+++ api-dfx/dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello World!';
+        return 'Hello Test!';
     }
 };
 AppService = __decorate([
