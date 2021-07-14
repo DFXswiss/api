823182761f93d1700f8f3e83c0e83a8311baf782
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-07-14 12:48:52 +0200

    Add verify signature

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,17 +2,33 @@ import { InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./user.entity";
+import * as request from "request-promise-native";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(createUserDto: CreateUserDto): Promise<void> {
         const user = this.create(createUserDto);
+       
+        const baseUrl = 'http://defichain-node.de/api/v1/test/verifymessage';
+        const signatureMessage = String("Test123")
+        const queryString = '?address='+user.address+'&signature='+user.signature+'&message='+ signatureMessage;
+        var options = {
+                uri: baseUrl + queryString,
+        };
+          
+        const result = await request.get(options);
 
+        if(result.response === 'True'){
+            
+      
         try {
             await this.save(user);
         } catch (error) {
             console.log(error);
             throw new InternalServerErrorException();
-        }
+        }    
+    }else{
+        throw new InternalServerErrorException();
+    }
     }
 }
