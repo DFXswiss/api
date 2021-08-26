1b9bc5436f1540b11f59bba3af2110a4593af974
David May <david.leo.may@gmail.com>
2021-08-26 03:06:42 +0200

    Added wallet seed

--- migration/seed/seed.js
+++ migration/seed/seed.js
@@ -1,6 +1,7 @@
 const { createConnection } = require("typeorm");
 const { AssetSeed } = require('./asset.seed');
 const { LanguageSeed } = require('./language.seed');
+const { WalletSeed } = require('./wallet.seed');
 const { FiatSeed } = require('./fiat.seed');
 const { CountrySeed } = require('./country.seed');
 
@@ -8,6 +9,7 @@ async function doSeed() {
     const connection = await createConnection()
     await connection.getRepository('asset').save(AssetSeed);
     await connection.getRepository('language').save(LanguageSeed);
+    await connection.getRepository('wallet').save(WalletSeed);
     await connection.getRepository('fiat').save(FiatSeed);
     await connection.getRepository('country').save(CountrySeed);
 }
---
+++ migration/seed/wallet.seed.js
@@ -0,0 +1,10 @@
+let WalletSeed = [
+  {
+    address: '<put-address-here>',
+    signature: '<put-signature-here>',
+    mail: 'robin.torque@dfx.swiss',
+    description: 'DFX Wallet',
+  },
+];
+
+module.exports = { WalletSeed };
