12f7d5063afc86f12d54627b3ab551854e1ba3f9
Yannick1712 <nickbusch1998@gmail.com>
2021-08-31 03:46:26 +0200

    Update all: add UserData

--- src/all/all.service.ts
+++ src/all/all.service.ts
@@ -6,15 +6,17 @@ import { LogRepository } from 'src/log/log.repository';
 import { SellPaymentRepository } from 'src/payment/payment-sell.repository';
 import { BuyPaymentRepository } from 'src/payment/payment-buy.repository';
 import { getManager } from 'typeorm';
+import { UserDataRepository } from 'src/userData/userData.repository';
 
 
 export class AllDataService {
 
   async getAllData(): Promise<any> {
 
-    let result = { users: {}, buys: {}, sells: {} , wallets: {}, logs: {}, payments: { buy: {}, sell: {}} };
+    let result = { users: {}, userData: {}, buys: {}, sells: {} , wallets: {}, logs: {}, payments: { buy: {}, sell: {}} };
 
     result.users = await getManager().getCustomRepository(UserRepository).getAllUser();
+    result.userData = await getManager().getCustomRepository(UserDataRepository).getAllUserData();
     result.buys= await getManager().getCustomRepository(BuyRepository).getAll();
     result.sells = await getManager().getCustomRepository(SellRepository).getAll();
     result.wallets = await getManager().getCustomRepository(WalletRepository).getAllWallet();
