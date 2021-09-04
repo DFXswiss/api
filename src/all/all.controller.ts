dad4efd5aba06e7a5f6760bfe4e2d2e3bfe6b932
Yannick1712 <nickbusch1998@gmail.com>
2021-09-04 12:44:49 +0200

    Update all: add getMethods, ExcludeEndpoint

--- src/all/all.controller.ts
+++ src/all/all.controller.ts
@@ -4,7 +4,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiHideProperty, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExcludeEndpoint, ApiHideProperty, ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/guards/role.guard';
 import { UserRole } from 'src/user/user.entity';
 import { AllDataService } from './all.service';
@@ -15,9 +15,66 @@ export class AllDataController {
   
   @Get()
   @ApiBearerAuth()
+  @ApiExcludeEndpoint()
   @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
   async getAllRoute(): Promise<any> {
     return this.allService.getAllData();
+  }
+
+  @Get('user')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllUserRoute(): Promise<any> {
+    return this.allService.getAllUser();
+  }
+
+  @Get('userData')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllUserDataRoute(): Promise<any> {
+    return this.allService.getAllUserData();
+  }
+
+  @Get('buy')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllBuyRoute(): Promise<any> {
+    return this.allService.getAllBuy();
+  }
+
+  @Get('sell')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllSellRoute(): Promise<any> {
+    return this.allService.getAllSell();
+  }
+
+  @Get('wallet')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllWalletRoute(): Promise<any> {
+    return this.allService.getAllWallet();
+  }
+
+  @Get('log')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllLogRoute(): Promise<any> {
+    return this.allService.getAllLog();
+  }
+
+  @Get('payment')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async getAllPaymentRoute(): Promise<any> {
+    return this.allService.getAllPayment();
   }
 
 }
