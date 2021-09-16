15e1c4a977c3a811cad0709bc8d576a5f6c1e61e
Yannick1712 <nickbusch1998@gmail.com>
2021-09-16 14:58:07 +0200

    Update batch: add balance in dto, add in appModule

--- src/batch/dto/create-batch.dto.ts
+++ src/batch/dto/create-batch.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsBoolean,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateBatchDto {
-
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  balanceAfter: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  balanceBefore: number;
 }
