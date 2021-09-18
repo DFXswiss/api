543e9576fe42fd45766cc76fc1fb6b9341ae47cf
David May <david.leo.may@gmail.com>
2021-09-18 15:37:10 +0200

    Improved ref link system: redirect to app stores

--- src/referral/ref.controller.ts
+++ src/referral/ref.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query, Res, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { Details, UserAgent } from 'express-useragent';
 import { RealIP } from 'nestjs-real-ip';
 import { RefService } from './ref.service';
 
@@ -12,13 +13,24 @@ export class RefController {
   @Get()
   @ApiExcludeEndpoint()
   @UsePipes(ValidationPipe)
-  async createRef(@RealIP() ip: string, @Query('code') ref, @Res() res: Response): Promise<void> {
+  async createRef(@RealIP() ip: string, @Query('code') ref, @Req() req: Request, @Res() res: Response): Promise<void> {
     if (ip && ref) {
       await this.refService.addOrUpdate(ip, ref);
-      res.redirect('https://dfx.swiss', 307);
+
+      // redirect user depending on platform
+      let url = 'https://dfx.swiss';
+      const agent = this.getAgentDetails(req);
+      if (agent.isAndroid) url = 'https://play.google.com/store/apps/details?id=com.defichain.app.dfx';
+      if (agent.isiPhone) url = 'https://apps.apple.com/app/id1582633093';
+
+      res.redirect(url, 307);
     } else {
-      const ref = await this.refService.get(ip);
+      ref = await this.refService.get(ip);
       res.status(200).send({ ref });
     }
+  }
+
+  private getAgentDetails(req: Request): Details {
+    return new UserAgent().parse(req.headers['user-agent']);
   }
 }
