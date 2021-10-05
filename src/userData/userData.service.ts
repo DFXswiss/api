c302ef525fd341a3d6790622ad6ba96e403928f9
Yannick1712 <nickbusch1998@gmail.com>
2021-10-05 23:23:12 +0200

    Update userData: add tryAndcatch

--- src/userData/userData.service.ts
+++ src/userData/userData.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, ServiceUnavailableException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, ServiceUnavailableException } from '@nestjs/common';
 import { UpdateUserDataDto } from './dto/update-userData.dto';
 import { UserDataRepository } from './userData.repository';
 import { KycStatus, UserData } from './userData.entity';
@@ -96,36 +96,40 @@ export class UserDataService {
   }
 
   async getManyCheckStatus(startUserDataId: number, endUserDataId: number): Promise<UserDataChecks[]> {
-    const userDataChecks: UserDataChecks[] = [];
-    for (let userDataId = startUserDataId; userDataId <= endUserDataId; userDataId++) {
-      const userData = await this.userDataRepo.findOne({ where: { id: userDataId }, relations: ['bankDatas'] });
-      if (userData) {
-        if (userData.bankDatas.length > 0) {
-          const customer = await this.getCustomer(userDataId);
-          userDataChecks.push({
-            userDataId: userDataId.toString(),
-            customerId: customer.customer.id.toString(),
-            kycFileReference: userData.kycFile?.id.toString() ?? null,
-            nameCheckRisk: customer.checkResult.risks[0].categoryKey,
-            activationDate: new Date(
-              Number.parseInt(customer.customer.activationDate.year),
-              Number.parseInt(customer.customer.activationDate.month) - 1,
-              Number.parseInt(customer.customer.activationDate.day),
-              2,
-            ),
-          });
-        } else {
-          userDataChecks.push({
-            userDataId: userDataId.toString(),
-            customerId: null,
-            kycFileReference: null,
-            nameCheckRisk: null,
-            activationDate: null,
-          });
+    try {
+      const userDataChecks: UserDataChecks[] = [];
+      for (let userDataId = startUserDataId; userDataId <= endUserDataId; userDataId++) {
+        const userData = await this.userDataRepo.findOne({ where: { id: userDataId }, relations: ['bankDatas'] });
+        if (userData) {
+          if (userData.bankDatas.length > 0) {
+            const customer = await this.getCustomer(userDataId);
+            userDataChecks.push({
+              userDataId: userDataId.toString(),
+              customerId: customer.customer.id.toString(),
+              kycFileReference: userData.kycFile?.id.toString() ?? null,
+              nameCheckRisk: customer.checkResult.risks[0].categoryKey,
+              activationDate: new Date(
+                Number.parseInt(customer.customer.activationDate.year),
+                Number.parseInt(customer.customer.activationDate.month) - 1,
+                Number.parseInt(customer.customer.activationDate.day),
+                2,
+              ),
+            });
+          } else {
+            userDataChecks.push({
+              userDataId: userDataId.toString(),
+              customerId: null,
+              kycFileReference: null,
+              nameCheckRisk: null,
+              activationDate: null,
+            });
+          }
         }
       }
+      return userDataChecks;
+    } catch (error) {
+      throw new ConflictException(error.message);
     }
-    return userDataChecks;
   }
 
   async requestKyc(userDataId: number): Promise<boolean> {
