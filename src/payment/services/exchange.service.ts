6408edba8977738eec328b588932bd6545016a29
sandrich <mail@chrissandrini.ch>
2021-10-17 15:58:28 +0200

    Initial exchange service implementation

--- src/payment/services/exchange.service.ts
+++ src/payment/services/exchange.service.ts
@@ -1,16 +1,70 @@
 import { Injectable } from '@nestjs/common';
-import { Exchange, kraken as Kraken } from 'ccxt';
+import { Exchange, OrderBook, Order, Balances, kraken } from 'ccxt';
+
+enum OrderSide {
+  BUY = 'buy',
+  SELL = 'sell'
+}
 
 @Injectable()
-export class ExchangeService {
-  private readonly kraken: Exchange;
+class ExchangeService {
+  private readonly _exchange: Exchange;
+  public balances: Promise<any>;
 
-  constructor() {
-    this.kraken = new Kraken({ apiKey: 'todo-api-key-here', secret: 'todo-secret-here', timeout: 30000 });
+  constructor(exchange: Exchange) {
+      this._exchange = exchange;
+      this.balances = this.updateBalances();
   }
 
-  async test() {
-    const markets = await this.kraken.loadMarkets();
-    console.log(markets);
+  async updateBalances(): Promise<Balances> {
+      return this._exchange.fetchBalance();
+  }
+
+  async fetchBalances() {
+     return this.balances;
+  }
+
+  async fetchOrderBook(currencyPair: string): Promise<OrderBook> {
+      return this._exchange.fetchOrderBook(currencyPair);
+  }
+
+  async createOrder(orderSide: OrderSide, currencyPair: string, exchangeAmount: number): Promise<Order> {
+      const orderbook = await this.fetchOrderBook(currencyPair);
+
+      /* 
+          If 'buy' we want to buy token1 using token2. Example BTC/EUR on 'buy' means we buy BTC using EUR
+              > We want to have the highest 'bids' price in the orderbook
+          If 'sell' we want to sell token1 using token2. Example BTC/EUR on 'sell' means we sell BTC using EUR
+              > We want to have the lowest 'asks' price in the orderbook
+      */
+      const currentPrice = (orderSide == OrderSide.BUY) ? orderbook.bids[0][0] : orderbook.asks[0][0];
+      const wantedCurrencyAmount = (orderSide == OrderSide.BUY) ? exchangeAmount / currentPrice: exchangeAmount;
+      const order = await this._exchange.createOrder(currencyPair, 'limit', orderSide, wantedCurrencyAmount, currentPrice, {'oflags': 'post'});
+
+      let checkOrder = await this._exchange.fetchOrder(order.id, currencyPair)
+      let checkOrderCounter = 0
+
+      while (!['closed', 'canceled'].includes(checkOrder.status) && checkOrderCounter < 100) {
+          checkOrder = await this._exchange.fetchOrder(order.id, currencyPair)
+          checkOrderCounter++;
+      }
+
+      // Retry
+      if (checkOrder.status == 'canceled') {
+          console.log("Order was canceled! Lets try again");
+          return this.createOrder(orderSide, currencyPair, exchangeAmount);
+      }
+
+      // TODO: What if order was partially filled?
+      // TODO: Check if we have enough balance?
+      // TODO: How many retries?
+
+      return order;
+  }
+}
+
+class Kraken extends ExchangeService {
+  constructor(params: any) {
+      super(new kraken(params));
   }
 }
