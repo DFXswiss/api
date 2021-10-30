fd3e6381ac58bdeff84eee7eb635fc2a7714ee5e
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2021-10-30 22:24:22 +0200

    Remove @ApiPropertyOptional() in send-mai dto

--- src/dto/send-mail.dto.ts
+++ src/dto/send-mail.dto.ts
@@ -1,23 +1,18 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SendMailDto {
-  @ApiPropertyOptional()
   @IsNotEmpty()
   @IsEmail()
   mail: string;
 
-  @ApiPropertyOptional()
   @IsNotEmpty()
   @IsString()
   salutation: string;
 
-  @ApiPropertyOptional()
   @IsNotEmpty()
   @IsString()
   subject: string;
 
-  @ApiPropertyOptional()
   @IsNotEmpty()
   @IsString()
   body: string;
