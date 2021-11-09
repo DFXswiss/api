90c6a7a92ac79880408c2596725d39dfb9312d83
Yannick1712 <nickbusch1998@gmail.com>
2021-11-09 10:00:03 +0100

    Update log: conflictException, currentAddress

--- src/user/models/log/log.service.ts
+++ src/user/models/log/log.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable } from '@nestjs/common';
 import { LogRepository } from './log.repository';
 import { CreateLogDto } from './dto/create-log.dto';
 import { MailService } from 'src/shared/services/mail.service';
@@ -28,32 +28,25 @@ export class LogService {
 
   async createLog(createLogDto: CreateLogDto): Promise<any> {
     let existingLog;
+    let currentAddress;
 
     if (createLogDto.blockchainTx) {
       if (createLogDto.address) {
-        existingLog = (
-          await this.logRepository
-            .createQueryBuilder('log')
-            .innerJoin('log.user', 'user')
-            .where('user.address = :address', { address: createLogDto.address })
-            .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
-            .andWhere('log.type = :type', { type: createLogDto.type })
-            .getRawMany()
-        )[0];
+        currentAddress = createLogDto.address;
       } else if (createLogDto.user) {
-        existingLog = (
-          await this.logRepository
-            .createQueryBuilder('log')
-            .innerJoin('log.user', 'user')
-            .where('user.address = :address', { address: createLogDto.user.address })
-            .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
-            .andWhere('log.type = :type', { type: createLogDto.type })
-            .getRawMany()
-        )[0];
+        currentAddress = createLogDto.user.address;
       }
+
+      existingLog = await this.logRepository
+        .createQueryBuilder('log')
+        .innerJoin('log.user', 'user')
+        .where('user.address = :address', { address: currentAddress })
+        .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
+        .andWhere('log.type = :type', { type: createLogDto.type })
+        .getRawOne();
     }
 
-    if (existingLog) throw new ForbiddenException('Log already existing - duplicate log');
+    if (existingLog) throw new ConflictException('Log already existing - duplicate log');
 
     if (createLogDto.type === LogType.TRANSACTION && !createLogDto.status) {
       if (!createLogDto.user && createLogDto.address) {
@@ -87,35 +80,28 @@ export class LogService {
   async createVolumeLog(createLogDto: CreateVolumeLogDto): Promise<any> {
     let assetObject = null;
     let fiatObject = null;
+    let currentAddress;
     let existingLog;
 
     createLogDto.type = LogType.VOLUME;
 
     if (createLogDto.blockchainTx) {
       if (createLogDto.address) {
-        existingLog = (
-          await this.logRepository
-            .createQueryBuilder('log')
-            .innerJoin('log.user', 'user')
-            .where('user.address = :address', { address: createLogDto.address })
-            .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
-            .andWhere('log.type = :type', { type: createLogDto.type })
-            .getRawMany()
-        )[0];
+        currentAddress = createLogDto.address;
       } else if (createLogDto.user) {
-        existingLog = (
-          await this.logRepository
-            .createQueryBuilder('log')
-            .innerJoin('log.user', 'user')
-            .where('user.address = :address', { address: createLogDto.user.address })
-            .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
-            .andWhere('log.type = :type', { type: createLogDto.type })
-            .getRawMany()
-        )[0];
+        currentAddress = createLogDto.user.address;
       }
+
+      existingLog = await this.logRepository
+        .createQueryBuilder('log')
+        .innerJoin('log.user', 'user')
+        .where('user.address = :address', { address: currentAddress })
+        .andWhere('log.blockchainTx = :blockchainTx', { blockchainTx: createLogDto.blockchainTx })
+        .andWhere('log.type = :type', { type: createLogDto.type })
+        .getRawOne();
     }
 
-    if (existingLog) throw new ForbiddenException('Log already existing - duplicate log');
+    if (existingLog) throw new ConflictException('Log already existing - duplicate log');
 
     if (createLogDto.payment) {
       // TODO: re-enable
