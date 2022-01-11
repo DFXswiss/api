43038f68b8acc270589c78dad9aeb3d86a19ded0
Yannick1712 <nickbusch1998@gmail.com>
2022-01-11 12:06:45 +0100

    Update bankDataDto: set name optional

--- src/user/models/bankData/dto/bankData.dto.ts
+++ src/user/models/bankData/dto/bankData.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class BankDataDto {
   @ApiProperty()
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   name: string;
 
