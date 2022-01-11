8d0bc4d1b0768c416b8019568ea73e457c01c991
Yannick1712 <nickbusch1998@gmail.com>
2022-01-12 00:11:17 +0100

    Delete unused Code

--- src/user/models/bankData/dto/bankData.dto.ts
+++ src/user/models/bankData/dto/bankData.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class BankDataDto {
   @ApiProperty()
