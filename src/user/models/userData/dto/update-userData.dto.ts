d096dcf6525a0522121bc80e42e8680173153ec0
Yannick1712 <nickbusch1998@gmail.com>
2022-01-14 01:53:56 +0100

    Update userDataDto: add isInt

--- src/user/models/userData/dto/update-userData.dto.ts
+++ src/user/models/userData/dto/update-userData.dto.ts
@@ -8,116 +8,94 @@ export class UpdateUserDataDto {
   @IsNotEmpty()
   id: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsBoolean()
   isMigrated: boolean;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsEnum(AccountType)
   accountType: AccountType;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsEmail()
   mail: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   firstname: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   surname: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   street: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   houseNumber: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   location: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   zip: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   language: any;
 
-  @ApiPropertyOptional()
   @IsOptional()
+  @IsInt()
   countryId: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   // TODO: user phonenumber decorator instead of string --> Figure it out
   // @IsPhoneNumber()
   phone: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   organizationName: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   organizationStreet: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   organizationHouseNumber: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   organizationLocation: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   organizationZip: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   organizationCountryId: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsInt()
   depositLimit: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsInt()
   kycFileId: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsEnum(KycStatus)
   kycStatus: KycStatus;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsEnum(KycState)
   kycState: KycState;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsInt()
   mainBankDataId: number;
