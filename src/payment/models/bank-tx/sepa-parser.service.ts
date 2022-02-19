9cfb64ac548928d1fd0957babb22b168554ae50c
David May <david.leo.may@gmail.com>
2022-02-19 14:20:53 +0100

    Updated packages

--- src/payment/models/bank-tx/sepa-parser.service.ts
+++ src/payment/models/bank-tx/sepa-parser.service.ts
@@ -3,18 +3,18 @@ import { SepaFile } from './dto/sepa-file.dto';
 import { SepaCdi, SepaAddress, ChargeRecord } from './dto/sepa.dto';
 import { BankTxBatch } from './bank-tx-batch.entity';
 import { BankTx } from './bank-tx.entity';
-import * as XmlParser from 'fast-xml-parser';
+import { XMLParser, XMLValidator } from 'fast-xml-parser';
 import { Config } from 'src/config/config';
 import { Util } from 'src/shared/util';
 
 export class SepaParser {
   static parseSepaFile(xmlFile: string): SepaFile {
-    const validationResult = XmlParser.validate(xmlFile);
+    const validationResult = XMLValidator.validate(xmlFile);
     if (validationResult !== true) {
       throw validationResult;
     }
 
-    return XmlParser.parse(xmlFile, { ignoreAttributes: false }).Document;
+    return new XMLParser({ ignoreAttributes: false }).parse(xmlFile).Document;
   }
 
   static parseBatch(file: SepaFile): Partial<BankTxBatch> {
