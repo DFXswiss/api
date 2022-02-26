e40e0361ea93529a31c8c4dba919219b6d82e698
Yannick1712 <nickbusch1998@gmail.com>
2022-02-26 15:46:39 +0100

    Add stakingRewardType

--- src/payment/models/staking-reward/staking-reward.entity.ts
+++ src/payment/models/staking-reward/staking-reward.entity.ts
@@ -2,6 +2,12 @@ import { Column, ManyToOne, Index, Entity } from 'typeorm';
 import { Reward } from '../reward/reward.entity';
 import { Staking } from '../staking/staking.entity';
 
+export enum StakingRewardType {
+  WALLET = 'Wallet',
+  REINVEST = 'Reinvest',
+  SELL = 'Sell',
+}
+
 @Entity()
 @Index('oneRewardPerRouteCheck', (reward: StakingReward) => [reward.txId, reward.staking], { unique: true })
 export class StakingReward extends Reward {
@@ -10,6 +16,9 @@ export class StakingReward extends Reward {
 
   @Column({ type: 'datetime2', nullable: true })
   inputDate: Date;
+
+  @Column({ length: 256, nullable: false })
+  stakingRewardType: StakingRewardType;
 
   @ManyToOne(() => Staking, (staking) => staking.rewards, { nullable: false })
   staking: Staking;
