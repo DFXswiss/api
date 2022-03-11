6e64f80ac1ecb118520eb1631d71e989eaa30230
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2022-03-12 00:55:33 +0100

    Add masternodeService to spec

--- src/statistic/cfp.service.spec.ts
+++ src/statistic/cfp.service.spec.ts
@@ -4,15 +4,19 @@ import { CryptoService } from 'src/ain/services/crypto.service';
 import { HttpService } from 'src/shared/services/http.service';
 import { SettingService } from 'src/shared/models/setting/setting.service';
 import { CfpService } from './cfp.service';
+import { MasternodeService } from 'src/payment/models/masternode/masternode.service';
 
 describe('CfpService', () => {
   let service: CfpService;
   let settingService: SettingService;
+  let masternodeService: MasternodeService;
 
   beforeEach(async () => {
     settingService = createMock<SettingService>();
+    masternodeService = createMock<MasternodeService>();
 
     jest.spyOn(settingService, 'getObj').mockResolvedValueOnce({});
+    jest.spyOn(masternodeService, 'getObj').mockResolvedValueOnce({});
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -20,6 +24,7 @@ describe('CfpService', () => {
         { provide: HttpService, useValue: {} },
         { provide: CryptoService, useValue: {} },
         { provide: SettingService, useValue: settingService },
+        { provide: MasternodeService, useValue: masternodeService },
         { provide: 'VALID_MNS', useValue: [] },
       ],
     }).compile();
