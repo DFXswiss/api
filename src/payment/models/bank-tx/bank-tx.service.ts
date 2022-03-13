74cd355f283fb4c055798893dd8b6f4648670995
David May <david.leo.may@gmail.com>
2022-03-13 20:35:14 +0100

    Fixed ref error catch, disabled SEPA file import

--- src/payment/models/bank-tx/bank-tx.service.ts
+++ src/payment/models/bank-tx/bank-tx.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { FtpService } from 'src/shared/services/ftp.service';
-import { Interval } from '@nestjs/schedule';
 import { BankTxRepository } from './bank-tx.repository';
 import { BankTxBatchRepository } from './bank-tx-batch.repository';
 import { BankTxBatch } from './bank-tx-batch.entity';
 import { SepaParser } from './sepa-parser.service';
 import { In } from 'typeorm';
-import { Config } from 'src/config/config';
 import { MailService } from 'src/shared/services/mail.service';
 
 @Injectable()
@@ -17,41 +14,11 @@ export class BankTxService {
     private readonly mailService: MailService,
   ) {}
 
-  @Interval(60000)
-  async importSepaFiles(): Promise<void> {
-    try {
-      await this.fetchAndStoreSepaFiles();
-    } catch (e) {
-      console.error('Exception during SEPA import:', e);
-    }
-  }
-
   async storeSepaFiles(files: string[]): Promise<(BankTxBatch | Error)[]> {
     return Promise.all(files.map((f) => this.storeSepaFile(f).catch((e: Error) => e)));
   }
 
   // --- HELPER METHODS --- //
-  private async fetchAndStoreSepaFiles(): Promise<void> {
-    const client = await FtpService.connect(Config.ftp);
-
-    // read file list
-    const fileInfos = await client.listFiles().then((i) => i.filter((f) => f.name.endsWith('.xml')));
-    if (fileInfos.length > 0) console.log('New SEPA files to import:', fileInfos);
-
-    // store and move files to archive folder
-    for (const fileInfo of fileInfos) {
-      try {
-        const file = await client.readFile(fileInfo);
-        const batch = await this.storeSepaFile(file);
-        await client.moveFile(fileInfo.name, 'archive', `${batch.identification}.xml`);
-      } catch (e) {
-        console.error(`Failed to import SEPA file ${fileInfo.name}:`, e);
-      }
-    }
-
-    client.close();
-  }
-
   private async storeSepaFile(xmlFile: string): Promise<BankTxBatch> {
     const sepaFile = SepaParser.parseSepaFile(xmlFile);
 
