ffc263a54d213d73d02be4990830f554afe41728
Yannick1712 <nickbusch1998@gmail.com>
2022-05-03 14:28:14 +0200

    Merge two Migrations into one

--- migration/1651145926011-newFeeCol.js
+++ migration/1651145926011-newFeeCol.js
@@ -5,9 +5,13 @@ module.exports = class newFeeCol1651145926011 {
 
     async up(queryRunner) {
         await queryRunner.query(`EXEC sp_rename "buy_crypto.fee", "feePercent"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD "feePercentAmount" float`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD "feeAbsolute" float`);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`EXEC sp_rename "buy_crypto.feePercent", "fee"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP COLUMN "feeAbsolute"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP COLUMN "feePercentAmount"`);
     }
 }
