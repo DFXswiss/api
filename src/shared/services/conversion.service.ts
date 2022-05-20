a08b4089ee568b59d139477ea76a5c81489acc1f
David May <david.leo.may@gmail.com>
2022-05-20 21:44:22 +0200

    Removed unused conversion code

--- src/shared/services/conversion.service.ts
+++ src/shared/services/conversion.service.ts
@@ -5,7 +5,6 @@ import { HttpService } from './http.service';
 @Injectable()
 export class ConversionService {
   private readonly fiatUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1';
-  private readonly cryptoUrl = 'https://api.coingecko.com/api/v3/simple/price';
 
   constructor(private readonly http: HttpService) {}
 
@@ -28,18 +27,6 @@ export class ConversionService {
 
     const result = await this.callApi<{ [currency: string]: number }>(url);
     return result[toCurrency.toLowerCase()];
-  }
-
-  // --- CRYPTO --- //
-  public async toDfi(amount: number, currency: string): Promise<number> {
-    const rate = await this.getDfiRate(currency);
-    return amount / rate;
-  }
-
-  public async getDfiRate(currency: string): Promise<number> {
-    const url = `${this.cryptoUrl}?ids=defichain&vs_currencies=${currency.toLowerCase()}`;
-    const result = await this.http.get(url);
-    return result['defichain'][currency.toLowerCase()];
   }
 
   // --- HELPERS --- //
