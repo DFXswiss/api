295b7da2d6a29b3650ac256b5b17cdd29f4c80d5
David May <david.leo.may@gmail.com>
2022-07-14 22:17:57 +0200

    [DEV-369] Minor improvements

--- infrastructure/scripts/btc/start.sh
+++ infrastructure/scripts/btc/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 /home/dfx/.bitcoin/bitcoin-22.0/bin/bitcoind -daemonwait \
-                                             -pid=/run/bitcoind/bitcoind.pid \
                                              -datadir=/home/dfx/.bitcoin \
                                              -conf=/home/dfx/.bitcoin/bitcoin.conf
