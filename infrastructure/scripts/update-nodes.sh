2d77f6da5fd16b90e2b64310fa2520654e3ac78f
David May <david.leo.may@gmail.com>
2022-07-15 11:00:10 +0200

    DEV-369 Updated scripts

--- infrastructure/scripts/update-nodes.sh
+++ infrastructure/scripts/update-nodes.sh
@@ -3,11 +3,9 @@ az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-nod
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-out-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml --slot stg
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-int-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml --slot stg
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-ref-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml --slot stg
-az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-btc-inp-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/btc-node-$1.yml --slot stg
 
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-inp-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-dex-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-out-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-int-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml
 az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-ref-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/defi-node-$1.yml
-az webapp config container set --resource-group rg-dfx-api-$1 --name app-dfx-node-btc-inp-$1 --multicontainer-config-type compose --multicontainer-config-file infrastructure/docker/btc-node-$1.yml
