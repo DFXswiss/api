5c40f24e057cad22c5305b4f66dc5bce6fae327e
Yannick1712 <nickbusch1998@gmail.com>
2022-07-16 17:11:32 +0200

    [DEV-329] buyCrypto: deactivate update allFields, change monitoring

--- src/payment/models/buy-crypto/services/buy-crypto.service.ts
+++ src/payment/models/buy-crypto/services/buy-crypto.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, ConflictException, Injectable, NotFoundException }
 import { BuyService } from '../../buy/buy.service';
 import { UserService } from 'src/user/models/user/user.service';
 import { BankTxRepository } from '../../bank-tx/bank-tx.repository';
-import { Between, In, IsNull } from 'typeorm';
+import { Between, In, IsNull, Not } from 'typeorm';
 import { UserStatus } from 'src/user/models/user/user.entity';
 import { BuyRepository } from '../../buy/buy.repository';
 import { Util } from 'src/shared/util';
@@ -72,8 +72,7 @@ export class BuyCryptoService {
 
     Util.removeNullFields(entity);
 
-    //TODO update aller Felder wieder deaktivieren
-    entity = await this.buyCryptoRepo.save({ ...entity, ...update });
+    entity = await this.buyCryptoRepo.save({ ...update, ...entity });
 
     // activate user
     if (entity.amlCheck === AmlCheck.PASS && entity.buy?.user?.status === UserStatus.NA) {
@@ -225,7 +224,7 @@ export class BuyCryptoService {
   // Monitoring
 
   async getIncompleteTransactions(): Promise<number> {
-    return await this.buyCryptoRepo.count({ mailSendDate: IsNull() });
+    return await this.buyCryptoRepo.count({ mailSendDate: IsNull(), amlCheck: Not(AmlCheck.FAIL) });
   }
 
   async getLastOutputDate(): Promise<Date> {
