e23c679c1966eaa65ac025037af5ea54d769d42b
Krysh <krysh.developer@gmail.com>
2022-07-21 18:34:25 +0200

    [DEV-201] remove user session guard from /country

--- src/shared/models/country/country.controller.ts
+++ src/shared/models/country/country.controller.ts
@@ -1,8 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { RoleGuard } from 'src/shared/auth/role.guard';
-import { UserRole } from 'src/shared/auth/user-role.enum';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Country } from './country.entity';
 import { CountryService } from './country.service';
 
@@ -12,8 +9,6 @@ export class CountryController {
   constructor(private readonly countryService: CountryService) {}
 
   @Get()
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard(), new RoleGuard(UserRole.USER))
   async getAllCountry(): Promise<Country[]> {
     return this.countryService.getAllCountry();
   }
