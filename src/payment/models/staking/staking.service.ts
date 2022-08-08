809948d34743ee79adff47f1eb774e082ee601da
Krysh <krysh.developer@gmail.com>
2022-08-08 14:14:30 +0200

    [DEV-586] added min deposit for staking routes

--- src/payment/models/staking/staking.service.ts
+++ src/payment/models/staking/staking.service.ts
@@ -282,6 +282,7 @@ export class StakingService {
       fee: fee,
       period: Config.staking.period,
       minInvestment: Config.staking.minInvestment,
+      minDeposit: Config.staking.minDeposit,
     };
   }
 
