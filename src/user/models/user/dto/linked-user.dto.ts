78bfa9a1ab28695ad954d69a7aef5e4fe769743b
Krysh <krysh.developer@gmail.com>
2022-08-18 13:59:13 +0200

    [DEV-634] fix broken import

--- src/user/models/user/dto/linked-user.dto.ts
+++ src/user/models/user/dto/linked-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
-import { Blockchain } from 'src/ain/node/node.service';
+import { Blockchain } from 'src/ain/services/crypto.service';
 import { GetConfig } from 'src/config/config';
 
 export class LinkedUserDto {
