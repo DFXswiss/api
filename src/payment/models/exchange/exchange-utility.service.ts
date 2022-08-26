433b04979d55f7f39e80823e19c097cc64d45340
Anton Semikin <tony.semikin@gmail.com>
2022-08-26 17:44:13 +0200

    [DEV-667] defined pricing interface

--- src/payment/models/exchange/exchange-utility.service.ts
+++ src/payment/models/exchange/exchange-utility.service.ts
@@ -1,11 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { Blockchain } from 'src/blockchain/shared/enums/blockchain.enum';
 import { BinanceService } from './binance.service';
 import { BitpandaService } from './bitpanda.service';
 import { BitstampService } from './bitstamp.service';
 import { Price } from './dto/price.dto';
 import { KrakenService } from './kraken.service';
 
-type PriceSource = string;
+// NOTE - Ideally from Asset to Asset where Fiat is also Asset.
+interface PriceRequest {
+  // TODO - if assets are same just return same amount, same applies to stable coins, USDC, USDT, maybe EUR....???
+  from: string;
+  to: string;
+  options?: PriceRequestOptions;
+}
+
+interface PriceRequestOptions {
+  includeReversePrice?: boolean;
+  // NOTE - potential extension
+  cached?: boolean;
+  fromBlockchain?: Blockchain;
+  toBlockchain?: Blockchain;
+  fromVolume?: number;
+}
+
+interface PriceResult {
+  path: PricePath;
+  price: Price;
+  reversePath?: PricePath;
+  reversePrice?: Price;
+}
+
+interface PricePath {
+  [key: number]: PriceStep;
+}
+
+interface PriceStep {
+  price: Price;
+  vendor: PriceVendor;
+  timestamp: Date;
+}
+
+// maybe upgrade to class with .execute() method
+interface PricePathExecutable {
+  [key: number]: PriceStepExecutable;
+}
+
+// this also have to be executable.
+interface PriceStepExecutable {
+  from: string;
+  to: string;
+  vendor: PriceVendor;
+}
+
+type PriceVendor = string;
 
 @Injectable()
 export class ExchangeUtilityService {
@@ -15,6 +62,13 @@ export class ExchangeUtilityService {
     private readonly bitstampService: BitstampService,
     private readonly bitpandaService: BitpandaService,
   ) {}
+
+  async getPrice(request: PriceRequest): Promise<PriceResult> {
+    const path = this.getPath(request);
+
+    // TODO - maybe separate classes, place behind interface
+    return this.executePath(path, request.options);
+  }
 
   async getMatchingPrice(fromCurrency: string, toCurrency: string, matchThreshold = 0.02): Promise<Price> {
     const mainPrice = await this.krakenService.getPrice(fromCurrency, toCurrency);
@@ -31,7 +85,7 @@ export class ExchangeUtilityService {
     return mainPrice;
   }
 
-  private async getReferencePrice(fromCurrency: string, toCurrency: string): Promise<[Price, PriceSource]> {
+  private async getReferencePrice(fromCurrency: string, toCurrency: string): Promise<[Price, PriceVendor]> {
     const referenceExchanges = [this.binanceService, this.bitstampService, this.bitpandaService];
 
     for (const exchange of referenceExchanges) {
@@ -43,5 +97,15 @@ export class ExchangeUtilityService {
     throw new Error(
       `Could not find reference price at Binance, Bitstamp and Bitpanda. From ${fromCurrency} to ${toCurrency}`,
     );
+  }
+
+  //*** HELPER METHODS ***//
+
+  private getPath(request: PriceRequest): PricePathExecutable {
+    return {};
+  }
+
+  private executePath(path: PricePathExecutable, options: PriceRequestOptions): PriceResult {
+    return null;
   }
 }
