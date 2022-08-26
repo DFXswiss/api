96f55bbf4afc739790b8b1ec9ff69c2ae92c2d03
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2022-08-26 15:19:26 +0200

    [DEV-596] Change migration

--- migration/1661519910674-BankTxChanges.js
+++ migration/1661519910674-BankTxChanges.js
@@ -1,11 +1,11 @@
 const { MigrationInterface, QueryRunner } = require("typeorm");
 
-module.exports = class AddAccountIban1661507831848 {
-    name = 'AddAccountIban1661507831848'
+module.exports = class BankTxChanges1661519910674 {
+    name = 'BankTxChanges1661519910674'
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "bank_tx" ADD "accountIban" nvarchar(256)`);
-        await queryRunner.query(`ALTER TABLE "bank_tx" ADD "txRaw" nvarchar(256)`);
+        await queryRunner.query(`ALTER TABLE "bank_tx" ADD "txRaw" nvarchar(MAX)`);
         await queryRunner.query(`ALTER TABLE "bank_tx" DROP CONSTRAINT "FK_feb41dc8ee54a46b69833ed05b2"`);
         await queryRunner.query(`ALTER TABLE "bank_tx" ALTER COLUMN "batchId" int`);
         await queryRunner.query(`ALTER TABLE "bank_tx" ADD CONSTRAINT "FK_feb41dc8ee54a46b69833ed05b2" FOREIGN KEY ("batchId") REFERENCES "bank_tx_batch"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
