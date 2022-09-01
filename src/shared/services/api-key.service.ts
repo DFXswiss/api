882be557839226406146f5580c7da934db42a0ec
Yannick1712 <nickbusch1998@gmail.com>
2022-09-01 11:51:56 +0200

    [FIX] add logging to apiSign timestamp

--- src/shared/services/api-key.service.ts
+++ src/shared/services/api-key.service.ts
@@ -31,6 +31,8 @@ export class ApiKeyService {
   public isValidSign(user: User, sign: string, timestamp: string): boolean {
     const userSign = this.getSign(user, timestamp);
 
+    console.log('CT timestamp: ' + timestamp);
+
     // TODO: temp code until CT changed its timestamp
     const [date, time] = timestamp.split('T');
     timestamp = `${date.split('.').reverse().join('-')}T${time}`;
