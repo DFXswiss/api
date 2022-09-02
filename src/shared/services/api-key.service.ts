061483ff7bb8900df5d1d31ad6621cc68f2e9cee
Yannick1712 <nickbusch1998@gmail.com>
2022-09-02 02:23:35 +0200

    [FIX] remove temp code

--- src/shared/services/api-key.service.ts
+++ src/shared/services/api-key.service.ts
@@ -31,12 +31,6 @@ export class ApiKeyService {
   public isValidSign(user: User, sign: string, timestamp: string): boolean {
     const userSign = this.getSign(user, timestamp);
 
-    console.log('CT timestamp: ' + timestamp);
-
-    // TODO: temp code until CT changed its timestamp
-    const [date, time] = timestamp.split('T');
-    timestamp = `${date.split('.').reverse().join('-')}T${time}`;
-
     return sign.toUpperCase() == userSign && Util.daysDiff(new Date(timestamp), new Date()) <= 1;
   }
 
