f413195800f1713950b32425aea90d7210f17b25
Krysh <krysh.developer@gmail.com>
2022-09-06 09:41:01 +0200

    [DEV-716] remove blockchain for user table from migration file

--- migration/1660729279828-blockchain-info.js
+++ migration/1660729279828-blockchain-info.js
@@ -5,12 +5,9 @@ module.exports = class blockchainInfo1660729279828 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "asset" ADD "blockchain" nvarchar(256) NOT NULL CONSTRAINT "DF_0e1dda4bf7f110acc1b1988dc81" DEFAULT 'DeFiChain'`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "blockchain" nvarchar(256) NOT NULL CONSTRAINT "DF_0759fc5718b185b6b6df7d98f90" DEFAULT 'DeFiChain'`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "DF_0759fc5718b185b6b6df7d98f90"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "blockchain"`);
         await queryRunner.query(`ALTER TABLE "asset" DROP CONSTRAINT "DF_0e1dda4bf7f110acc1b1988dc81"`);
         await queryRunner.query(`ALTER TABLE "asset" DROP COLUMN "blockchain"`);
     }
