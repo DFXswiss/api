521746114bc98a1eca6b3e6103666bb478195523
Anton Semikin <tony.semikin@gmail.com>
2022-09-06 13:49:56 +0200

    [DEV-667] declared basic integration tests scenarios

---
+++ src/payment/models/buy-crypto/__tests__/buy-crypto.integration.spec.ts
@@ -0,0 +1,57 @@
+describe('Buy Crypto Integration Tests', () => {
+  describe('Buy on Defichain', () => {
+    it('buys DFI coin', () => {
+      // TODO
+    });
+
+    it('buys USD stablecoin', () => {
+      // TODO
+    });
+
+    it('buys non-DFI Crypto coin', () => {
+      // TODO
+    });
+
+    it('buys Tokens', () => {
+      // TODO
+    });
+
+    it('buys Pool Pair', () => {
+      // TODO
+    });
+  });
+
+  describe('Buy on Bitcoin', () => {
+    it('buys BTC coin', () => {
+      // TODO
+    });
+  });
+
+  describe('Buy on Ethereum', () => {
+    it('buys ETH coin', () => {
+      // TODO
+    });
+  });
+
+  describe('Buy on Binance Smart Chain', () => {
+    it('buys BNB coin', () => {
+      // TODO
+    });
+  });
+
+  describe('Shared Scenarios', () => {
+    it('sends confirmation email to a User', () => {
+      // TODO
+    });
+
+    it('disallow to created new batches, when a batch for currency pair is in progress', () => {
+      // TODO
+    });
+  });
+
+  describe('General Failure Scenarios', () => {
+    it('', () => {
+      // TODO
+    });
+  });
+});
---
+++ src/payment/models/pricing/__tests__/pricing.integration.spec.ts
@@ -0,0 +1,39 @@
+describe('Pricing Module Integration Tests', () => {
+  it('MATCHING_ASSETS', () => {
+    // TODO
+  });
+
+  it('FIAT_TO_BTC', () => {
+    // TODO
+  });
+
+  it('ALTCOIN_TO_BTC', () => {
+    // TODO
+  });
+
+  it('FIAT_TO_ALTCOIN', () => {
+    // TODO
+  });
+
+  it('ALTCOIN_TO_ALTCOIN', () => {
+    // TODO
+  });
+
+  it('BTC_TO_ALTCOIN', () => {
+    // TODO
+  });
+
+  it('MATCHING_FIAT_TO_USD_STABLE_COIN', () => {
+    // TODO
+  });
+
+  it('NON_MATCHING_FIAT_TO_USD_STABLE_COIN', () => {
+    // TODO
+  });
+
+  describe('General Failure Scenarios', () => {
+    it('', () => {
+      // TODO
+    });
+  });
+});
