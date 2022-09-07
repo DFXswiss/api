3a858a8dda1b3a74ea85b5555400593dc05c4687
Krysh <krysh.developer@gmail.com>
2022-09-07 12:57:55 +0200

    [DEV-716] improved blockchain handling and added unique constraint for asset name & blockchain

--- migration/1660729279828-blockchain-info.js
+++ migration/1660729279828-blockchain-info.js
@@ -5,10 +5,13 @@ module.exports = class blockchainInfo1660729279828 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "asset" ADD "blockchain" nvarchar(256) NOT NULL CONSTRAINT "DF_0e1dda4bf7f110acc1b1988dc81" DEFAULT 'DeFiChain'`);
+        await queryRunner.query(`ALTER TABLE "asset" DROP CONSTRAINT "UQ_119b2d1c1bdccc42057c303c44f"`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "nameBlockchain" ON "asset" ("name", "blockchain") `);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "asset" DROP CONSTRAINT "DF_0e1dda4bf7f110acc1b1988dc81"`);
         await queryRunner.query(`ALTER TABLE "asset" DROP COLUMN "blockchain"`);
+        await queryRunner.query(`DROP INDEX "IDX_caebee0c1fa7bff3805c65fd01" ON "link_address"`);
     }
 }
--- migration/1662544933802-remove-asset-name-unique.js
+++
@@ -1,9 +0,0 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
-
-module.exports = class removeAssetNameUnique1662544933802 {
-    name = 'removeAssetNameUnique1662544933802'
-
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "asset" DROP CONSTRAINT "DF_0e1dda4bf7f110acc1b1988dc81"`);
-    }
-}
