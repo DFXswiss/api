31f142b2a98c4acf8929514b0023eab5360ccd2a
Anton Semikin <tony.semikin@gmail.com>
2022-09-08 15:50:03 +0200

    [DEV-667] fixed ETH liquidity purchase mail

--- src/payment/models/dex/strategies/purchase-liquidity/base/purchase-liquidity-evm.strategy.ts
+++ src/payment/models/dex/strategies/purchase-liquidity/base/purchase-liquidity-evm.strategy.ts
@@ -10,21 +10,24 @@ export class PurchaseLiquidityEvmStrategy extends PurchaseLiquidityStrategy {
 
   async purchaseLiquidity(request: LiquidityRequest): Promise<void> {
     const { referenceAsset, referenceAmount, context, correlationId } = request;
+    try {
+      // should always throw, even if there is amount, additional check is done for API consistency and sending mail
+      if (referenceAsset === this.dexEvmService._nativeCoin) {
+        const amount = await this.dexEvmService.checkCoinAvailability(referenceAmount);
 
-    // should always throw, even if there is amount, additional check is done for API consistency and sending mail
-    if (referenceAsset === this.dexEvmService._nativeCoin) {
-      const amount = await this.dexEvmService.checkCoinAvailability(referenceAmount);
-
-      if (amount) {
-        throw new Error(
-          `Requested ${referenceAsset} liquidity is already available on the wallet. No purchase required, retry checkLiquidity. Context: ${context}. CorrelationID: ${correlationId}`,
-        );
+        if (amount) {
+          throw new Error(
+            `Requested ${referenceAsset} liquidity is already available on the wallet. No purchase required, retry checkLiquidity. Context: ${context}. CorrelationID: ${correlationId}`,
+          );
+        }
       }
-    }
 
-    // throw by default, only native coin trading enabled
-    throw new Error(
-      `Only native coins are supported by EVM PurchaseLiquidity strategy. Provided reference asset: ${referenceAsset} Context: ${context}. CorrelationID: ${correlationId}`,
-    );
+      // throw by default, only native coin trading enabled
+      throw new Error(
+        `Only native coins are supported by EVM PurchaseLiquidity strategy. Provided reference asset: ${referenceAsset} Context: ${context}. CorrelationID: ${correlationId}`,
+      );
+    } catch (e) {
+      await this.handlePurchaseLiquidityError(e, request);
+    }
   }
 }
