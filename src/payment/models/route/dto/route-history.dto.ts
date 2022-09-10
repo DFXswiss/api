3d9576f728bc4e274ea7f1953552797be6b0db35
Yannick1712 <nickbusch1998@gmail.com>
2022-09-10 10:45:48 +0200

    [DEV-729] add swagger docu

--- src/payment/models/route/dto/route-history.dto.ts
+++ src/payment/models/route/dto/route-history.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { AmlCheck } from '../../buy-crypto/enums/aml-check.enum';
 
 export enum HistoryTransactionType {
@@ -7,16 +8,32 @@ export enum HistoryTransactionType {
 }
 
 export class RouteHistoryDto {
+  @ApiProperty()
   inputAmount: number;
+
+  @ApiProperty()
   inputAsset: string;
+
+  @ApiPropertyOptional()
   outputAmount: number;
+
+  @ApiPropertyOptional()
   outputAsset: string;
+
+  @ApiPropertyOptional()
   txId: string;
+
+  @ApiProperty()
   date: Date;
+
+  @ApiProperty()
   amlCheck: AmlCheck;
+
+  @ApiProperty()
   isComplete: boolean;
 }
 
 export class TypedRouteHistoryDto extends RouteHistoryDto {
+  @ApiProperty({ enum: HistoryTransactionType })
   type: HistoryTransactionType;
 }
