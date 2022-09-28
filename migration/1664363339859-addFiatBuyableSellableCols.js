c4c4fd223bdd39c59599296ea1c47f8f7dafc08d
Yannick1712 <nickbusch1998@gmail.com>
2022-09-28 16:55:41 +0200

    [DEV-824] change migration

--- migration/1664363339859-addFiatBuyableSellableCols.js
+++ migration/1664363339859-addFiatBuyableSellableCols.js
@@ -4,7 +4,7 @@ module.exports = class addFiatBuyableSellableCols1664363339859 {
     name = 'addFiatBuyableSellableCols1664363339859'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "dbo"."fiat" ADD "buyable" bit NOT NULL CONSTRAINT "DF_8e87e46c42a0570b4845ff73ab0" DEFAULT 1`);
+        await queryRunner.query(`ALTER TABLE "dbo"."fiat" ADD "buyable" bit NOT NULL CONSTRAINT "DF_8e87e46c42a0570b4845ff73ab0" DEFAULT 0`);
         await queryRunner.query(`ALTER TABLE "dbo"."fiat" ADD "sellable" bit NOT NULL CONSTRAINT "DF_41b20c12dda3b0838b3cc53a67c" DEFAULT 0`);
     }
 
