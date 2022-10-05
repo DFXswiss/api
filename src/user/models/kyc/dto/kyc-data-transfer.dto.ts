6c0aa85441782edc30ebd383f5556940445b1129
Yannick1712 <nickbusch1998@gmail.com>
2022-10-05 22:47:14 +0200

    [DEV-849] fix dto check

--- src/user/models/kyc/dto/kyc-data-transfer.dto.ts
+++ src/user/models/kyc/dto/kyc-data-transfer.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmptyObject, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class KycDataTransferDto {
   @ApiProperty()
-  @IsNotEmptyObject()
+  @IsNotEmpty()
   @IsString()
   walletName: string;
 }
