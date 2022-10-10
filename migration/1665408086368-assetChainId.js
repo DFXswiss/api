1793df652cc56b9a51573831c34f6e1932fc5d4a
Anton Semikin <tony.semikin@gmail.com>
2022-10-10 15:23:39 +0200

    [DEV-622] created migration

---
+++ migration/1665408086368-assetChainId.js
@@ -0,0 +1,15 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class assetChainId1665408086368 {
+    name = 'assetChainId1665408086368'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "asset" DROP COLUMN "chainId"`);
+        await queryRunner.query(`ALTER TABLE "asset" ADD "chainId" nvarchar(255)`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "asset" DROP COLUMN "chainId"`);
+        await queryRunner.query(`ALTER TABLE "asset" ADD "chainId" int`);
+    }
+}
