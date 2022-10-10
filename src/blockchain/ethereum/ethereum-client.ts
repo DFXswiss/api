e2b8118890e84f08ceb7659928611bd4b5d2e7af
Anton Semikin <tony.semikin@gmail.com>
2022-10-10 14:13:57 +0200

    [DEV-622] fixed evm client construction

--- src/blockchain/ethereum/ethereum-client.ts
+++ src/blockchain/ethereum/ethereum-client.ts
@@ -1,7 +1,13 @@
 import { EvmClient } from '../shared/evm/evm-client';
 
 export class EthereumClient extends EvmClient {
-  constructor(gatewayUrl: string, privateKey: string, dfxAddress: string, swapContractAddress: string) {
-    super(gatewayUrl, privateKey, dfxAddress, swapContractAddress);
+  constructor(
+    gatewayUrl: string,
+    privateKey: string,
+    dfxAddress: string,
+    swapContractAddress: string,
+    swapTokenAddress: string,
+  ) {
+    super(gatewayUrl, privateKey, dfxAddress, swapContractAddress, swapTokenAddress);
   }
 }
