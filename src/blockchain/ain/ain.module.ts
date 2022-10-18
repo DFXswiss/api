f91c50949d761eddd1ed894abd00e7ca23549592
David May <david.leo.may@gmail.com>
2022-10-18 17:19:24 +0200

    [DEV-893] Using recommended fee and RBF for BTC send

--- src/blockchain/ain/ain.module.ts
+++ src/blockchain/ain/ain.module.ts
@@ -5,11 +5,12 @@ import { NodeController } from './node/node.controller';
 import { NodeService } from './node/node.service';
 import { WhaleService } from './whale/whale.service';
 import { DeFiChainUtil } from './utils/defichain.util';
+import { BtcFeeService } from './services/btc-fee.service';
 
 @Module({
   imports: [SharedModule],
-  providers: [CryptoService, NodeService, WhaleService, DeFiChainUtil],
-  exports: [CryptoService, NodeService, WhaleService, DeFiChainUtil],
+  providers: [CryptoService, NodeService, WhaleService, DeFiChainUtil, BtcFeeService],
+  exports: [CryptoService, NodeService, WhaleService, DeFiChainUtil, BtcFeeService],
   controllers: [NodeController],
 })
 export class AinModule {}
