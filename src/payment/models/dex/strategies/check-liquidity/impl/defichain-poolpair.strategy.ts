596ebe1c59e8e49785a5c7df28784d504e365233
TonySemikin <tony.semikin@gmail.com>
2022-11-08 11:46:23 +0100

    [DEV-728] Max purchasable liquidity reference check for pool pairs (#497)
    * [DEV-728] implemented max purchasable liquidity reference check for pool pairs
    
    * [DEV-728] fixed unit test

--- src/payment/models/dex/strategies/check-liquidity/impl/defichain-poolpair.strategy.ts
+++ src/payment/models/dex/strategies/check-liquidity/impl/defichain-poolpair.strategy.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { Blockchain } from 'src/blockchain/shared/enums/blockchain.enum';
 import { Asset, AssetType } from 'src/shared/models/asset/asset.entity';
 import { AssetService } from 'src/shared/models/asset/asset.service';
+import { Util } from 'src/shared/util';
 import { CheckLiquidityResult, LiquidityRequest } from '../../../interfaces';
+import { DexDeFiChainService } from '../../../services/dex-defichain.service';
 import { CheckLiquidityUtil } from '../utils/check-liquidity.util';
 import { CheckLiquidityStrategy } from './base/check-liquidity.strategy';
 
 @Injectable()
 export class DeFiChainPoolPairStrategy extends CheckLiquidityStrategy {
-  constructor(protected readonly assetService: AssetService) {
+  constructor(
+    protected readonly assetService: AssetService,
+    private readonly dexDeFiChainService: DexDeFiChainService,
+  ) {
     super();
   }
 
@@ -17,7 +22,20 @@ export class DeFiChainPoolPairStrategy extends CheckLiquidityStrategy {
    * special case - availability check and target amount calculation is omitted
    */
   async checkLiquidity(request: LiquidityRequest): Promise<CheckLiquidityResult> {
-    return CheckLiquidityUtil.createNonPurchasableCheckLiquidityResult(request, 0, 0, await this.feeAsset());
+    const { referenceAsset, referenceAmount } = request;
+
+    const referenceMaxPurchasableAmount = await this.calculateReferenceMaxPurchasableAmount(
+      referenceAsset,
+      referenceAmount,
+    );
+
+    return CheckLiquidityUtil.createNonPurchasableCheckLiquidityResult(
+      request,
+      0,
+      0,
+      await this.feeAsset(),
+      referenceMaxPurchasableAmount,
+    );
   }
 
   protected getFeeAsset(): Promise<Asset> {
@@ -26,5 +44,30 @@ export class DeFiChainPoolPairStrategy extends CheckLiquidityStrategy {
       blockchain: Blockchain.DEFICHAIN,
       type: AssetType.COIN,
     });
+  }
+
+  //*** HELPER METHODS ***//
+
+  private async calculateReferenceMaxPurchasableAmount(
+    referenceAsset: Asset,
+    referenceAmount: number,
+  ): Promise<number> {
+    const dfiToken = await this.assetService.getAssetByQuery({
+      dexName: 'DFI',
+      blockchain: Blockchain.DEFICHAIN,
+      type: AssetType.TOKEN,
+    });
+
+    const dfiRequiredAmount = await this.dexDeFiChainService.getSwapAmountForPurchase(
+      referenceAsset,
+      referenceAmount,
+      null,
+      dfiToken,
+    );
+
+    const dfiAvailableAmount = await this.dexDeFiChainService.getAssetAvailability(dfiToken);
+
+    // approximate, indicative calculation
+    return Util.round((referenceAmount / dfiRequiredAmount) * dfiAvailableAmount, 8);
   }
 }
