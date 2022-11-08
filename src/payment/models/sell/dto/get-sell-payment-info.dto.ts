680f652b92b233c0da209058edfd4a9c27ccd99e
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2022-11-08 10:10:20 +0100

    [DEV-1013] set fee 0 for crypto asset (#496)
    * [DEV-1013] set fee 0 for crypto asset
    
    * [DEV-1013] Refactoring

--- src/payment/models/sell/dto/get-sell-payment-info.dto.ts
+++ src/payment/models/sell/dto/get-sell-payment-info.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsEnum, IsNotEmpty, IsNotEmptyObject, IsString, ValidateNested } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNotEmptyObject, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { Blockchain } from 'src/blockchain/shared/enums/blockchain.enum';
 import { EntityDto } from 'src/shared/dto/entity.dto';
+import { Asset } from 'src/shared/models/asset/asset.entity';
 import { Fiat } from 'src/shared/models/fiat/fiat.entity';
 
 export class GetSellPaymentInfoDto {
@@ -16,6 +17,13 @@ export class GetSellPaymentInfoDto {
   @ValidateNested()
   @Type(() => EntityDto)
   currency: Fiat;
+
+  @ApiProperty()
+  // TODO change to IsNotEmptyObject
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => EntityDto)
+  asset: Asset;
 
   @ApiProperty()
   @IsNotEmpty()
