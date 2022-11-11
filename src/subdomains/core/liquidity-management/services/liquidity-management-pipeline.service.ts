103149c7ec5b859fba74df088a35e71c0ef61d82
Anton Semikin <tony.semikin@gmail.com>
2022-11-11 12:26:33 +0100

    [DEV-704] added integration check for actions

--- src/subdomains/core/liquidity-management/services/liquidity-management-pipeline.service.ts
+++ src/subdomains/core/liquidity-management/services/liquidity-management-pipeline.service.ts
@@ -1,35 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { LiquidityManagementOrder } from '../entities/liquidity-management-order.entity';
-import { LiquidityManagementAction } from '../entities/liquidity-management-action.entity';
 import { LiquidityManagementOrderStatus, LiquidityManagementPipelineStatus } from '../enums';
 import { OrderNotProcessableException } from '../exceptions/order-not-processable.exception';
-import { LiquidityActionIntegrationFactory } from '../factories/liquidity-action.integration.factory';
-import { LiquidityActionIntegration } from '../interfaces';
 import { LiquidityManagementOrderRepository } from '../repositories/liquidity-management-order.repository';
 import { LiquidityManagementPipelineRepository } from '../repositories/liquidity-management-pipeline.repository';
 import { LiquidityManagementPipeline } from '../entities/liquidity-management-pipeline.entity';
+import { Lock } from 'src/shared/utils/lock';
+import { LiquidityManagementRuleService } from './liquidity-management-rule.service';
 
 @Injectable()
 export class LiquidityManagementPipelineService {
+  private readonly processPipelinesLock = new Lock(1800);
+  private readonly processOrdersLock = new Lock(1800);
+
   constructor(
     private readonly orderRepo: LiquidityManagementOrderRepository,
     private readonly pipelineRepo: LiquidityManagementPipelineRepository,
-    private readonly liquidityActionIntegrationFactory: LiquidityActionIntegrationFactory,
+    private readonly ruleService: LiquidityManagementRuleService,
   ) {}
 
   //*** JOBS ***//
 
   @Interval(60000)
   async processPipelines() {
-    await this.startNewPipelines();
-    await this.checkRunningPipelines();
+    if (!this.processPipelinesLock.acquire()) return;
+
+    try {
+      await this.startNewPipelines();
+      await this.checkRunningPipelines();
+    } catch (e) {
+    } finally {
+      this.processPipelinesLock.release();
+    }
   }
 
   @Interval(60000)
   async processOrders() {
-    await this.startNewOrders();
-    await this.checkRunningOrders();
+    if (!this.processOrdersLock.acquire()) return;
+
+    try {
+      await this.startNewOrders();
+      await this.checkRunningOrders();
+    } catch (e) {
+    } finally {
+      this.processOrdersLock.release();
+    }
   }
 
   //*** HELPER METHODS ***//
@@ -40,8 +56,13 @@ export class LiquidityManagementPipelineService {
     });
 
     for (const pipeline of newPipelines) {
-      pipeline.start();
-      await this.pipelineRepo.save(pipeline);
+      try {
+        pipeline.start();
+        await this.pipelineRepo.save(pipeline);
+      } catch (e) {
+        console.error(`Error in starting new liquidity pipeline. Pipeline ID: ${pipeline.id}`, e);
+        continue;
+      }
     }
   }
 
@@ -51,19 +72,24 @@ export class LiquidityManagementPipelineService {
     });
 
     for (const pipeline of runningPipelines) {
-      const order = await this.orderRepo.findOne({ pipeline, action: pipeline.currentAction });
+      try {
+        const order = await this.orderRepo.findOne({ pipeline, action: pipeline.currentAction });
 
-      if (!order) {
-        await this.placeLiquidityOrder(pipeline);
+        if (!order) {
+          await this.placeLiquidityOrder(pipeline);
+          continue;
+        }
+
+        if (
+          order.status === LiquidityManagementOrderStatus.COMPLETE ||
+          order.status === LiquidityManagementOrderStatus.FAILED
+        ) {
+          pipeline.continue(order.status);
+          await this.pipelineRepo.save(pipeline);
+        }
+      } catch (e) {
+        console.error(`Error in checking running liquidity pipeline. Pipeline ID: ${pipeline.id}`, e);
         continue;
-      }
-
-      if (
-        order.status === LiquidityManagementOrderStatus.COMPLETE ||
-        order.status === LiquidityManagementOrderStatus.FAILED
-      ) {
-        pipeline.continue(order.status);
-        await this.pipelineRepo.save(pipeline);
       }
     }
   }
@@ -85,22 +111,21 @@ export class LiquidityManagementPipelineService {
         if (e instanceof OrderNotProcessableException) {
           order.fail();
           await this.orderRepo.save(order);
+          continue;
         }
+
+        console.error(`Error in starting new liquidity order. Order ID: ${order.id}`, e);
       }
     }
   }
 
   private async executeOrder(order: LiquidityManagementOrder): Promise<void> {
-    const actionIntegration = await this.findLiquidityActionIntegration(order.action);
+    const actionIntegration = await this.ruleService.findLiquidityActionIntegration(order.action);
 
     await actionIntegration.runCommand(order.action.command);
     order.inProgress();
 
     await this.orderRepo.save(order);
-  }
-
-  private async findLiquidityActionIntegration(action: LiquidityManagementAction): Promise<LiquidityActionIntegration> {
-    return this.liquidityActionIntegrationFactory.getIntegration(action);
   }
 
   private async checkRunningOrders(): Promise<void> {
@@ -113,13 +138,16 @@ export class LiquidityManagementPipelineService {
         if (e instanceof OrderNotProcessableException) {
           order.fail();
           await this.orderRepo.save(order);
+          continue;
         }
+
+        console.error(`Error in checking running liquidity order. Order ID: ${order.id}`, e);
       }
     }
   }
 
   private async checkOrder(order: LiquidityManagementOrder): Promise<void> {
-    const actionIntegration = await this.findLiquidityActionIntegration(order.action);
+    const actionIntegration = await this.ruleService.findLiquidityActionIntegration(order.action);
     const isComplete = await actionIntegration.checkCompletion();
 
     if (isComplete) {
--- src/subdomains/core/liquidity-management/services/liquidity-management-rule.service.ts
+++ src/subdomains/core/liquidity-management/services/liquidity-management-rule.service.ts
@@ -12,6 +12,8 @@ import { LiquidityManagementActionRepository } from '../repositories/liquidity-m
 import { LiquidityManagementRuleOutputDto } from '../dto/output/liquidity-management-rule-output.dto';
 import { LiquidityManagementRuleOutputDtoMapper } from '../dto/output/mappers/liquidity-management-rule-output-dto.mapper';
 import { LiquidityManagementRuleCreationDto } from '../dto/input/liquidity-management-rule-creation.dto';
+import { LiquidityActionIntegration } from '../interfaces';
+import { LiquidityActionIntegrationFactory } from '../factories/liquidity-action.integration.factory';
 
 @Injectable()
 export class LiquidityManagementRuleService {
@@ -20,6 +22,7 @@ export class LiquidityManagementRuleService {
     private readonly actionRepo: LiquidityManagementActionRepository,
     private readonly assetService: AssetService,
     private readonly fiatService: FiatService,
+    private readonly liquidityActionIntegrationFactory: LiquidityActionIntegrationFactory,
   ) {}
 
   //*** PUBLIC API ***//
@@ -71,6 +74,10 @@ export class LiquidityManagementRuleService {
     rule.reactivate();
 
     return LiquidityManagementRuleOutputDtoMapper.entityToDto(await this.ruleRepo.save(rule));
+  }
+
+  findLiquidityActionIntegration(action: LiquidityManagementAction): LiquidityActionIntegration {
+    return this.liquidityActionIntegrationFactory.getIntegration(action);
   }
 
   //*** HELPER METHODS ***//
@@ -177,6 +184,14 @@ export class LiquidityManagementRuleService {
       actionOnSuccess,
       actionOnFail,
     );
+
+    const integration = this.findLiquidityActionIntegration(newAction);
+
+    if (!integration) {
+      throw new BadRequestException(
+        `No integration found for action. System: ${actionDto.system}, command: ${actionDto.command}`,
+      );
+    }
 
     return this.actionRepo.save(newAction);
   }
