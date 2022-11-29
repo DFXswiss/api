bda5dba7436c37a368e1fea6b635036435903b57
Anton Semikin <tony.semikin@gmail.com>
2022-11-29 12:46:50 +0100

    [DEV-1122] adjusted min and max rule check

--- src/subdomains/core/liquidity-management/specifications/liquidity-management-rule-init.specification.ts
+++ src/subdomains/core/liquidity-management/specifications/liquidity-management-rule-init.specification.ts
@@ -7,23 +7,27 @@ export class LiquidityManagementRuleInitSpecification {
       throw new BadRequestException('Cannot create rule. Provide targetAsset or targetFiat, not both.');
     }
 
-    if (!rule.minimal && !rule.maximal) {
+    if (!rule.targetAsset && !rule.targetFiat) {
+      throw new BadRequestException('Cannot create rule. Neither targetAsset or targetFiat was provided.');
+    }
+
+    if (rule.minimal == null && rule.maximal == null) {
       throw new BadRequestException('Cannot create rule. Neither minimal or maximal constraint was provided');
     }
 
-    if (rule.minimal && rule.minimal > rule.optimal) {
+    if (rule.minimal != null && rule.minimal > rule.optimal) {
       throw new BadRequestException('Cannot create rule. Minimal liquidity configuration must be lower than optimal');
     }
 
-    if (rule.maximal && rule.maximal < rule.optimal) {
+    if (rule.maximal != null && rule.maximal < rule.optimal) {
       throw new BadRequestException('Cannot create rule. Maximal liquidity configuration must be higher than optimal');
     }
 
-    if (rule.minimal && !rule.deficitStartAction) {
+    if (rule.minimal != null && !rule.deficitStartAction) {
       throw new BadRequestException('Cannot create rule. Deficit actions must be provided for minimal liquidity');
     }
 
-    if (rule.maximal && !rule.redundancyStartAction) {
+    if (rule.maximal != null && !rule.redundancyStartAction) {
       throw new BadRequestException('Cannot create rule. Redundancy actions must be provided for maximal liquidity');
     }
 
