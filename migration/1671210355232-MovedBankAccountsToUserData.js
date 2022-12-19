c56098698eee83014a06f1ace1f4807f285420c9
David May <david.leo.may@gmail.com>
2022-12-19 17:56:44 +0100

    [DEV-1203] Fixed migration

--- migration/1671210355232-MovedBankAccountsToUserData.js
+++ migration/1671210355232-MovedBankAccountsToUserData.js
@@ -7,8 +7,9 @@ module.exports = class MovedBankAccountsToUserData1671210355232 {
         await queryRunner.query(`ALTER TABLE "dbo"."bank_account" DROP CONSTRAINT "FK_c2ba1381682b0291238cbc7a65d"`);
         await queryRunner.query(`DROP INDEX "ibanUser" ON "dbo"."bank_account"`);
 
-        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ADD "userDataId" int NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ADD "userDataId" int`);
         await queryRunner.query(`UPDATE ba SET ba.userDataId = u.userDataId FROM dbo.bank_account ba INNER JOIN dbo.[user] u ON ba.userId = u.id`);
+        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ALTER COLUMN "userDataId" int NOT NULL`);
         await queryRunner.query(`ALTER TABLE "dbo"."bank_account" DROP COLUMN "userId"`);
         
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_a7ac3599aae96c391aeb374088" ON "dbo"."bank_account" ("iban", "userDataId") `);
@@ -19,8 +20,9 @@ module.exports = class MovedBankAccountsToUserData1671210355232 {
         await queryRunner.query(`ALTER TABLE "dbo"."bank_account" DROP CONSTRAINT "FK_166fa63a5cb0089c3268454f577"`);
         await queryRunner.query(`DROP INDEX "IDX_a7ac3599aae96c391aeb374088" ON "dbo"."bank_account"`);
 
-        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ADD "userId" int NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ADD "userId" int`);
         await queryRunner.query(`UPDATE ba SET ba.userId = u.id FROM dbo.bank_account ba INNER JOIN dbo.user_data ud ON ba.userDataId = ud.id INNER JOIN dbo.[user] u ON u.userDataId = ud.id`);
+        await queryRunner.query(`ALTER TABLE "dbo"."bank_account" ALTER COLUMN "userId" int NOT NULL`);
         await queryRunner.query(`ALTER TABLE "dbo"."bank_account" DROP COLUMN "userDataId"`);
 
         await queryRunner.query(`CREATE UNIQUE INDEX "ibanUser" ON "dbo"."bank_account" ("iban", "userId") `);
