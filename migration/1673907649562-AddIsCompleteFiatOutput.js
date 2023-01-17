23509e44192326742bf4d63a219fe9edae7ad41a
David May <david.leo.may@gmail.com>
2023-01-17 16:39:36 +0100

    [NO-TASK] Fixed migration

--- migration/1673907649562-AddIsCompleteFiatOutput.js
+++ migration/1673907649562-AddIsCompleteFiatOutput.js
@@ -5,13 +5,11 @@ module.exports = class AddIsCompleteFiatOutput1673907649562 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "fiat_output" ADD "isComplete" bit NOT NULL CONSTRAINT "DF_d7568fb14f2bd3138f69412d86c" DEFAULT 0`);
-        await queryRunner.query(`ALTER TABLE "fiat_output" DROP COLUMN "batchAmount"`);
-        await queryRunner.query(`ALTER TABLE "fiat_output" ADD "batchAmount" float`);
+        await queryRunner.query(`ALTER TABLE "fiat_output" ALTER COLUMN "batchAmount" float`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "fiat_output" DROP COLUMN "batchAmount"`);
-        await queryRunner.query(`ALTER TABLE "fiat_output" ADD "batchAmount" int`);
+        await queryRunner.query(`ALTER TABLE "fiat_output" ALTER COLUMN "batchAmount" int`);
         await queryRunner.query(`ALTER TABLE "fiat_output" DROP CONSTRAINT "DF_d7568fb14f2bd3138f69412d86c"`);
         await queryRunner.query(`ALTER TABLE "fiat_output" DROP COLUMN "isComplete"`);
     }
