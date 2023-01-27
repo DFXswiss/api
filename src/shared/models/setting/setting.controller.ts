c08d2704685da13aa3e763b4910a2a2b4e12eaa0
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-01-27 16:35:01 +0100

    [DEV-793] add setting route for cake bot (#628)
    * [DEV-793] add setting route for cake bot
    
    * [DEV-793] Refactoring

--- src/shared/models/setting/setting.controller.ts
+++ src/shared/models/setting/setting.controller.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiExcludeController, ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/shared/auth/role.guard';
 import { UserRole } from 'src/shared/auth/user-role.enum';
+import { CakeFlowDto, CakeSettings } from './dto/cake-flow.dto';
 import { CfpSettings } from 'src/subdomains/core/statistic/dto/cfp.dto';
 import { FrontendSettings } from './dto/frontend-settings.dto';
 import { Setting } from './setting.entity';
@@ -23,6 +24,11 @@ export class SettingController {
     return { cfpVotingOpen: cfpSettings.votingOpen };
   }
 
+  @Get('cake')
+  async getCakeSettings(): Promise<CakeSettings> {
+    return this.settingService.getObj<CakeSettings>('cake');
+  }
+
   // --- ADMIN --- //
   @Get()
   @ApiBearerAuth()
@@ -30,6 +36,14 @@ export class SettingController {
   @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
   async getSettings(): Promise<Setting[]> {
     return this.settingService.getAll();
+  }
+
+  @Put('cakeFlow')
+  @ApiBearerAuth()
+  @ApiExcludeEndpoint()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  async updateCakeFlowSetting(@Body() dto: CakeFlowDto): Promise<void> {
+    return this.settingService.setCakeFlow(dto);
   }
 
   @Put(':key')
