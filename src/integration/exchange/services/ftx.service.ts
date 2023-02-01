9ab3349e1c69181f73f58275f03ea0eb2d30f529
David May <david.leo.may@gmail.com>
2023-02-01 22:17:33 +0100

    [DEV-1294] Fixed exchange services

--- src/integration/exchange/services/ftx.service.ts
+++ src/integration/exchange/services/ftx.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { ftx } from 'ccxt';
 import { GetConfig } from 'src/config/config';
+import { SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class FtxService extends ExchangeService {
-  constructor() {
-    super(new ftx(GetConfig().exchange));
+  constructor(readonly scheduler: SchedulerRegistry) {
+    super(new ftx(GetConfig().exchange), scheduler);
   }
 }
