739c408184b3b81d7a69ac0c72fee6ef9c938ab4
TonySemikin <tony.semikin@gmail.com>
2023-03-01 15:41:55 +0100

    [DEV-1212] Google sheet EVM api (#663)
    * [DEV-1212] WIP
    
    * [DEV-1212] implemented EVM low level api for GS
    
    * [DEV-1212] added thunder requests and after test fixes
    
    * [DEV-1212] after cherry-pick fixes
    
    * [DEV-1212] changed controller path for EVM api

---
+++ src/subdomains/generic/gs/gs-evm.service.ts
@@ -0,0 +1,75 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import { EvmCoinTransactionDto } from 'src/integration/blockchain/shared/evm/dto/evm-coin-transaction.dto';
+import { EvmTokenTransactionDto } from 'src/integration/blockchain/shared/evm/dto/evm-token-transaction.dto';
+import { EvmRegistryService } from 'src/integration/blockchain/shared/evm/evm-registry.service';
+import { AssetService } from 'src/shared/models/asset/asset.service';
+import { BlockchainAddress } from 'src/shared/models/blockchain-address';
+import { DepositService } from 'src/subdomains/supporting/address-pool/deposit/deposit.service';
+import { EvmRawTransactionDto } from '../../../integration/blockchain/shared/evm/dto/evm-raw-transaction.dto';
+
+@Injectable()
+export class GsEvmService {
+  constructor(
+    private readonly evmRegistryService: EvmRegistryService,
+    private readonly depositService: DepositService,
+    private readonly assetService: AssetService,
+  ) {}
+
+  async sendRawTransaction({
+    request,
+    blockchain,
+  }: EvmRawTransactionDto): Promise<ethers.providers.TransactionResponse> {
+    const client = this.evmRegistryService.getClient(blockchain);
+
+    const depositPrivateKey = await this.depositService.getDepositKey(
+      BlockchainAddress.create(request.from, blockchain),
+    );
+
+    if (depositPrivateKey) {
+      return client.sendRawTransactionFromAddress(depositPrivateKey, request);
+    } else if (request.from === client._dfxAddress) {
+      return client.sendRawTransactionFromDex(request);
+    }
+
+    throw new Error('Provided source address is not known');
+  }
+
+  async sendTokenTransaction(dto: EvmTokenTransactionDto): Promise<string> {
+    const { fromAddress, toAddress, assetId, amount, feeLimit, blockchain } = dto;
+    const token = await this.assetService.getAssetById(assetId);
+
+    if (!token) throw new BadRequestException(`Asset with id ${assetId} not found`);
+
+    const client = this.evmRegistryService.getClient(blockchain);
+
+    const depositPrivateKey = await this.depositService.getDepositKey(
+      BlockchainAddress.create(fromAddress, blockchain),
+    );
+
+    if (depositPrivateKey) {
+      return client.sendTokenFromAddress(fromAddress, depositPrivateKey, toAddress, token, amount, feeLimit);
+    } else if (fromAddress === client._dfxAddress) {
+      return client.sendTokenFromDex(toAddress, token, amount, feeLimit);
+    }
+
+    throw new Error('Provided source address is not known');
+  }
+
+  async sendCoinTransaction(dto: EvmCoinTransactionDto): Promise<string> {
+    const { fromAddress, toAddress, amount, feeLimit, blockchain } = dto;
+    const client = this.evmRegistryService.getClient(blockchain);
+
+    const depositPrivateKey = await this.depositService.getDepositKey(
+      BlockchainAddress.create(fromAddress, blockchain),
+    );
+
+    if (depositPrivateKey) {
+      return client.sendNativeCoinFromAddress(fromAddress, depositPrivateKey, toAddress, amount, feeLimit);
+    } else if (fromAddress === client._dfxAddress) {
+      return client.sendNativeCoinFromDex(toAddress, amount, feeLimit);
+    }
+
+    throw new Error('Provided source address is not known');
+  }
+}
--- src/subdomains/generic/gs/gs.module.ts
+++ src/subdomains/generic/gs/gs.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
+import { BlockchainModule } from 'src/integration/blockchain/blockchain.module';
 import { LetterModule } from 'src/integration/letter/letter.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { BuyCryptoModule } from 'src/subdomains/core/buy-crypto/buy-crypto.module';
 import { ReferralModule } from 'src/subdomains/core/referral/referral.module';
 import { SellCryptoModule } from 'src/subdomains/core/sell-crypto/sell-crypto.module';
 import { StakingModule } from 'src/subdomains/core/staking/staking.module';
+import { AddressPoolModule } from 'src/subdomains/supporting/address-pool/address-pool.module';
 import { BankModule } from 'src/subdomains/supporting/bank/bank.module';
 import { NotificationModule } from 'src/subdomains/supporting/notification/notification.module';
 import { PayInModule } from 'src/subdomains/supporting/payin/payin.module';
 import { UserModule } from '../user/user.module';
+import { GsEvmController } from './gs-evm.controller';
+import { GsEvmService } from './gs-evm.service';
 import { GsController } from './gs.controller';
 import { GsService } from './gs.service';
 
 @Module({
   imports: [
     SharedModule,
+    BlockchainModule,
+    AddressPoolModule,
     ReferralModule,
     BuyCryptoModule,
     SellCryptoModule,
@@ -25,8 +31,8 @@ import { GsService } from './gs.service';
     BankModule,
     PayInModule,
   ],
-  controllers: [GsController],
-  providers: [GsService],
+  controllers: [GsController, GsEvmController],
+  providers: [GsService, GsEvmService],
   exports: [],
 })
 export class GsModule {}
