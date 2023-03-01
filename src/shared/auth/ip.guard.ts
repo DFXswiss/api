ed0f7402f4c4ca7c0bee30d1cf38eb6c6295960d
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2023-03-01 10:56:59 +0100

    [DEV-1362] Refactoring

--- src/shared/auth/ip.guard.ts
+++ src/shared/auth/ip.guard.ts
@@ -1,39 +1,20 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
-import { Config } from 'src/config/config';
-import { GeoLocationService } from 'src/integration/geolocation/geo-location.service';
-import { CountryService } from '../models/country/country.service';
+import { CanActivate, ExecutionContext, ForbiddenException, HttpException, Injectable } from '@nestjs/common';
 import { getClientIp } from '@supercharge/request-ip';
 import { IpLogService } from '../models/ip-log/ip-log.service';
 
 @Injectable()
 export class IpGuard implements CanActivate {
-  constructor(
-    private geoLocationService: GeoLocationService,
-    private countryService: CountryService,
-    private ipLogService: IpLogService,
-  ) {}
+  constructor(private ipLogService: IpLogService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const ip = getClientIp(req);
 
-    await this.ipLogService.checkRateLimit(req.url, ip);
+    const checkRateLimit = await this.ipLogService.checkRateLimit(req.url, ip);
+    if (!checkRateLimit) throw new HttpException(`Too many requests on endpoint ${req.url}`, 429);
 
-    const { country, result } = await this.checkIpCountry(ip);
-    const address = req.body.address;
+    const ipLog = await this.ipLogService.create(ip, req.url, req.body.address);
+    if (!ipLog.result) throw new ForbiddenException('The country of IP address is not allowed');
 
-    await this.ipLogService.create(ip, country, result, req.url, address);
-
-    if (!result) throw new ForbiddenException('The country of IP address is not allowed');
-    return result;
-  }
-
-  private async checkIpCountry(userIp: string): Promise<{ country: string; result: boolean }> {
-    if (Config.environment === 'loc' || userIp?.includes(Config.azureIpSubstring))
-      return { country: 'INTERN', result: true };
-    const country = await this.geoLocationService.getCountry(userIp);
-    if (!country) return { country, result: false };
-    const countryObject = await this.countryService.getCountryWithSymbol(country);
-
-    return { country, result: countryObject?.ipEnable };
+    return ipLog.result;
   }
 }
