bd432a328547b1f5e3c23ccf319c64232a20b382
David May <david.leo.may@gmail.com>
2023-03-17 17:29:22 +0100

    [DEV-1293] Using UserRepo

--- src/shared/models/ip-log/ip-log.service.ts
+++ src/shared/models/ip-log/ip-log.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { Config } from 'src/config/config';
 import { GeoLocationService } from 'src/integration/geolocation/geo-location.service';
 import { UserRole } from 'src/shared/auth/user-role.enum';
-import { UserService } from 'src/subdomains/generic/user/models/user/user.service';
+import { UserRepository } from 'src/subdomains/generic/user/models/user/user.repository';
+import { EntityManager } from 'typeorm';
 import { CountryService } from '../country/country.service';
 import { IpLog } from './ip-log.entity';
 import { IpLogRepository } from './ip-log.repository';
 
 @Injectable()
 export class IpLogService {
+  private readonly userRepo: UserRepository;
+
   constructor(
-    private geoLocationService: GeoLocationService,
-    private countryService: CountryService,
-    private ipLogRepo: IpLogRepository,
-    private userService: UserService,
-  ) {}
+    private readonly geoLocationService: GeoLocationService,
+    private readonly countryService: CountryService,
+    private readonly ipLogRepo: IpLogRepository,
+    entityManager: EntityManager,
+  ) {
+    this.userRepo = entityManager.getCustomRepository(UserRepository);
+  }
 
   async create(ip: string, url: string, address: string): Promise<IpLog> {
     const { country, result } = await this.checkIpCountry(ip, address);
@@ -32,11 +37,12 @@ export class IpLogService {
   private async checkIpCountry(userIp: string, address: string): Promise<{ country: string; result: boolean }> {
     if (Config.environment === 'loc' || userIp?.includes(Config.azureIpSubstring))
       return { country: 'INTERN', result: true };
-    const country = await this.geoLocationService.getCountry(userIp);
-    const user = await this.userService.getUserByAddress(address);
 
-    if (!country || user?.role != UserRole.USER) return { country, result: true };
+    const country = await this.geoLocationService.getCountry(userIp);
     const countryObject = await this.countryService.getCountryWithSymbol(country);
+
+    const user = await this.userRepo.findOne({ address });
+    if (!countryObject || user?.role != UserRole.USER) return { country, result: true };
 
     return { country, result: countryObject?.ipEnable };
   }
