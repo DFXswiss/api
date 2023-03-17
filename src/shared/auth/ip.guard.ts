c31a82538c896a4508772bff853d269457e6102c
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2023-03-17 17:06:33 +0100

    [DEV-1393] Refactoring

--- src/shared/auth/ip.guard.ts
+++ src/shared/auth/ip.guard.ts
@@ -1,17 +1,15 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { getClientIp } from '@supercharge/request-ip';
-import { UserService } from 'src/subdomains/generic/user/models/user/user.service';
 import { IpLogService } from '../models/ip-log/ip-log.service';
 
 @Injectable()
 export class IpGuard implements CanActivate {
-  constructor(private ipLogService: IpLogService, private userService: UserService) {}
+  constructor(private ipLogService: IpLogService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const ip = getClientIp(req);
 
     const ipLog = await this.ipLogService.create(ip, req.url, req.body.address);
-
     if (!ipLog.result) throw new ForbiddenException('The country of IP address is not allowed');
 
     return ipLog.result;
