240e60bf23b4feae2061db95bfc2fa1a7e7deb59
David May <david.leo.may@gmail.com>
2023-04-04 10:00:14 +0200

    [NOT-ASK] Fixed build

--- src/subdomains/core/statistic/statistic.module.ts
+++ src/subdomains/core/statistic/statistic.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AinModule } from 'src/integration/blockchain/ain/ain.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { UserModule } from 'src/subdomains/generic/user/user.module';
-import { MasternodeModule } from 'src/subdomains/supporting/masternode/masternode.module';
 import { BuyCryptoModule } from '../buy-crypto/buy-crypto.module';
 import { ReferralModule } from '../referral/referral.module';
 import { SellCryptoModule } from '../sell-crypto/sell-crypto.module';
@@ -11,7 +10,7 @@ import { StatisticController } from './statistic.controller';
 import { StatisticService } from './statistic.service';
 
 @Module({
-  imports: [SharedModule, BuyCryptoModule, SellCryptoModule, ReferralModule, UserModule, AinModule, MasternodeModule],
+  imports: [SharedModule, BuyCryptoModule, SellCryptoModule, ReferralModule, UserModule, AinModule],
   controllers: [StatisticController],
   providers: [StatisticService, CfpService],
   exports: [],
