644e0eadcc5031ee1db13bd614a59cf9052ee130
David May <david.leo.may@gmail.com>
2023-05-16 17:36:08 +0200

    [NO-TASK] Fiat API anonymous

--- src/shared/models/fiat/fiat.controller.ts
+++ src/shared/models/fiat/fiat.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { RoleGuard } from 'src/shared/auth/role.guard';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { FiatService } from './fiat.service';
-import { AuthGuard } from '@nestjs/passport';
-import { UserRole } from 'src/shared/auth/user-role.enum';
 import { FiatDto } from './dto/fiat.dto';
 import { FiatDtoMapper } from './dto/fiat-dto.mapper';
 
@@ -13,8 +10,6 @@ export class FiatController {
   constructor(private readonly fiatService: FiatService) {}
 
   @Get()
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard(), new RoleGuard(UserRole.USER))
   @ApiOkResponse({ type: FiatDto, isArray: true })
   async getAllFiat(): Promise<FiatDto[]> {
     return this.fiatService.getAllFiat().then(FiatDtoMapper.entitiesToDto);
