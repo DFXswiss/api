b7b89220b9025d35b4f6d35880cbe4d396121ab3
David May <david.leo.may@gmail.com>
2023-05-24 11:10:58 +0200

    [DEV-1523] Re-enabled asset prices

--- src/subdomains/supporting/pricing/services/asset-prices.service.ts
+++ src/subdomains/supporting/pricing/services/asset-prices.service.ts
@@ -6,7 +6,6 @@ import { DfxLogger } from 'src/shared/services/dfx-logger';
 import { PriceProviderService } from './price-provider.service';
 import { Lock } from 'src/shared/utils/lock';
 import { FiatService } from 'src/shared/models/fiat/fiat.service';
-import { Blockchain } from 'src/integration/blockchain/shared/enums/blockchain.enum';
 
 @Injectable()
 export class AssetPricesService {
@@ -27,8 +26,7 @@ export class AssetPricesService {
     const usd = await this.fiatService.getFiatByName('USD');
     const assets = await this.assetService.getAllAsset([]);
 
-    // TODO: fix for all chains
-    const assetsToUpdate = assets.filter((a) => a.blockchain === Blockchain.DEFICHAIN && !a.dexName.includes('BURN'));
+    const assetsToUpdate = assets.filter((a) => a.dexName.includes('BURN'));
 
     for (const asset of assetsToUpdate) {
       try {
