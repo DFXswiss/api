1d9b2e8c652cd2ad4210aa2bef7d8dc275e358d3
bernd2022 <bernd.nospam.2018@t-online.de>
2023-05-26 12:08:02 +0200

    [DEV-1521]: added constructor parameters

--- src/subdomains/supporting/payin/strategies/send/impl/lightning.strategy.ts
+++ src/subdomains/supporting/payin/strategies/send/impl/lightning.strategy.ts
@@ -4,13 +4,21 @@ import { DfxLogger } from 'src/shared/services/dfx-logger';
 import { BlockchainAddress } from 'src/shared/models/blockchain-address';
 import { CryptoInput } from '../../../entities/crypto-input.entity';
 import { PayInRepository } from '../../../repositories/payin.repository';
+import { PriceProviderService } from 'src/subdomains/supporting/pricing/services/price-provider.service';
+import { PayoutService } from 'src/subdomains/supporting/payout/services/payout.service';
+import { TransactionHelper } from 'src/shared/payment/services/transaction-helper';
 
 @Injectable()
 export class LightningStrategy extends SendStrategy {
   protected readonly logger = new DfxLogger(LightningStrategy);
 
-  constructor(private readonly payInRepo: PayInRepository) {
-    super();
+  constructor(
+    private readonly payInRepo: PayInRepository,
+    priceProvider: PriceProviderService,
+    payoutService: PayoutService,
+    transactionHelper: TransactionHelper,
+  ) {
+    super(priceProvider, payoutService, transactionHelper);
   }
 
   async doSend(payIns: CryptoInput[], _: SendType): Promise<void> {
