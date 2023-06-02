a82992a5a25a9976d856e354fbd7368c53139042
bernd2022 <bernd.nospam.2018@t-online.de>
2023-06-02 11:21:03 +0200

    Added httpsAgent with Certificate

--- src/integration/lightning/lnurl-forward.service.ts
+++ src/integration/lightning/lnurl-forward.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { HttpService } from 'src/shared/services/http.service';
+import { Agent } from 'https';
+import { Config } from 'src/config/config';
+import { HttpRequestConfig, HttpService } from 'src/shared/services/http.service';
+import { LnurlPayCallbackDto } from './dto/lnurlp-pay-callback.dto';
 import { LnurlPayRequestDto } from './dto/lnurlp-pay-request.dto';
 import { LightningHelper } from './lightning-helper';
-import { LnurlPayCallbackDto } from './dto/lnurlp-pay-callback.dto';
 
 @Injectable()
 export class LnUrlForwardService {
@@ -10,7 +12,7 @@ export class LnUrlForwardService {
 
   async lnurlpForward(id: string): Promise<LnurlPayRequestDto> {
     const lnBitsUrl = `${LightningHelper.lnurlpLnBitsBasePath}/${id}`;
-    const payRequest = await this.http.get<LnurlPayRequestDto>(lnBitsUrl);
+    const payRequest = await this.http.get<LnurlPayRequestDto>(lnBitsUrl, this.getHttpLnBitsConfig());
 
     payRequest.callback = `${LightningHelper.lnurlpCallbackDfxApiBasePath}/${id}`;
     payRequest.metadata = '[["text/plain", "DFX Deposit Address"]]';
@@ -20,6 +22,16 @@ export class LnUrlForwardService {
 
   async lnurlpCallbackForward(id: string, params: any): Promise<LnurlPayCallbackDto> {
     const lnBitsCallbackUrl = `${LightningHelper.lnurlpCallbackLnBitsBasePath}/${id}`;
-    return this.http.get<LnurlPayCallbackDto>(lnBitsCallbackUrl, { params });
+    return this.http.get<LnurlPayCallbackDto>(lnBitsCallbackUrl, this.getHttpLnBitsConfig(params));
+  }
+
+  private getHttpLnBitsConfig(params?: any): HttpRequestConfig {
+    return {
+      httpsAgent: new Agent({
+        ca: Config.blockchain.lightning.certificate,
+      }),
+
+      params,
+    };
   }
 }
