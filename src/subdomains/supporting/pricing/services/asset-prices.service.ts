2c5152775c942900ae60df8ffe877e18692a86fa
David May <david.leo.may@gmail.com>
2023-06-09 09:12:00 +0200

    [NO-TASK] No pricing for custom tokens

--- src/subdomains/supporting/pricing/services/asset-prices.service.ts
+++ src/subdomains/supporting/pricing/services/asset-prices.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Config, Process } from 'src/config/config';
+import { AssetType } from 'src/shared/models/asset/asset.entity';
 import { AssetService } from 'src/shared/models/asset/asset.service';
-import { DfxLogger } from 'src/shared/services/dfx-logger';
-import { PriceProviderService } from './price-provider.service';
-import { Lock } from 'src/shared/utils/lock';
 import { FiatService } from 'src/shared/models/fiat/fiat.service';
+import { DfxLogger } from 'src/shared/services/dfx-logger';
+import { Lock } from 'src/shared/utils/lock';
+import { PriceProviderService } from './price-provider.service';
 
 @Injectable()
 export class AssetPricesService {
@@ -26,7 +27,7 @@ export class AssetPricesService {
     const usd = await this.fiatService.getFiatByName('USD');
     const assets = await this.assetService.getAllAsset([]);
 
-    const assetsToUpdate = assets.filter((a) => !a.dexName.includes('BURN'));
+    const assetsToUpdate = assets.filter((a) => a.type !== AssetType.CUSTOM);
 
     for (const asset of assetsToUpdate) {
       try {
