7aac17b132f2958b78703edf0114fb69d496e185
David May <david.leo.may@gmail.com>
2023-06-14 09:32:45 +0200

    [NO-TASK] Added Bitcoin/DeFiChain deposit generation

--- src/subdomains/supporting/address-pool/deposit/deposit.service.ts
+++ src/subdomains/supporting/address-pool/deposit/deposit.service.ts
@@ -1,5 +1,8 @@
+import { AddressType } from '@defichain/jellyfish-api-core/dist/category/wallet';
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Config } from 'src/config/config';
+import { NodeClient } from 'src/integration/blockchain/ain/node/node-client';
+import { NodeService, NodeType } from 'src/integration/blockchain/ain/node/node.service';
 import { Blockchain } from 'src/integration/blockchain/shared/enums/blockchain.enum';
 import { EvmUtil } from 'src/integration/blockchain/shared/evm/evm.util';
 import { CryptoService } from 'src/integration/blockchain/shared/services/crypto.service';
@@ -7,6 +10,7 @@ import { LightningClient } from 'src/integration/lightning/lightning-client';
 import { LightningHelper } from 'src/integration/lightning/lightning-helper';
 import { LightningService } from 'src/integration/lightning/services/lightning.service';
 import { BlockchainAddress } from 'src/shared/models/blockchain-address';
+import { Util } from 'src/shared/utils/util';
 import { DepositRepository } from 'src/subdomains/supporting/address-pool/deposit/deposit.repository';
 import { In } from 'typeorm';
 import { Deposit } from './deposit.entity';
@@ -14,14 +18,19 @@ import { CreateDepositDto } from './dto/create-deposit.dto';
 
 @Injectable()
 export class DepositService {
+  private inpClient: NodeClient;
+  private btcInpClient: NodeClient;
   private readonly lightningClient: LightningClient;
 
   constructor(
     private readonly depositRepo: DepositRepository,
     private readonly cryptoService: CryptoService,
+    nodeService: NodeService,
     lightningService: LightningService,
   ) {
     this.lightningClient = lightningService.getDefaultClient();
+    nodeService.getConnectedNode(NodeType.INPUT).subscribe((c) => (this.inpClient = c));
+    nodeService.getConnectedNode(NodeType.BTC_INPUT).subscribe((c) => (this.btcInpClient = c));
   }
 
   async getDeposit(id: number): Promise<Deposit> {
@@ -49,13 +58,27 @@ export class DepositService {
   }
 
   async createDeposits({ blockchain, count }: CreateDepositDto): Promise<void> {
-    if (this.cryptoService.EthereumBasedChains.includes(blockchain)) {
+    if ([Blockchain.DEFICHAIN, Blockchain.BITCOIN].includes(blockchain)) {
+      return this.createJellyfishDeposits(blockchain, count);
+    } else if (this.cryptoService.EthereumBasedChains.includes(blockchain)) {
       return this.createEvmDeposits(blockchain, count);
     } else if (blockchain === Blockchain.LIGHTNING) {
       return this.createLightningDeposits(blockchain, count);
     }
 
     throw new BadRequestException(`Deposit creation for ${blockchain} not possible.`);
+  }
+
+  private async createJellyfishDeposits(blockchain: Blockchain, count: number) {
+    const client = blockchain === Blockchain.DEFICHAIN ? this.inpClient : this.btcInpClient;
+    const label = Util.isoDate(new Date());
+    const type = blockchain === Blockchain.DEFICHAIN ? AddressType.BECH32 : AddressType.P2SH_SEGWIT;
+
+    for (let i = 0; i < count; i++) {
+      const address = await client.createAddress(label, type);
+      const deposit = Deposit.create(address, blockchain);
+      await this.depositRepo.save(deposit);
+    }
   }
 
   private async createEvmDeposits(blockchain: Blockchain, count: number) {
