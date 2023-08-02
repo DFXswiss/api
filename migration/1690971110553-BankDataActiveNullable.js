24b861fdf588d5e783701217c71ae5d6f284cbef
David May <david.leo.may@gmail.com>
2023-08-02 12:15:51 +0200

    [DEV-1668] Bank data active nullable

---
+++ migration/1690971110553-BankDataActiveNullable.js
@@ -0,0 +1,17 @@
+const { MigrationInterface, QueryRunner } = require('typeorm');
+
+module.exports = class BankDataActiveNullable1690971110553 {
+  name = 'BankDataActiveNullable1690971110553';
+
+  async up(queryRunner) {
+    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit`);
+    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" DROP CONSTRAINT "DF_dfe568e239bb915e3f4d75b198e"`);
+  }
+
+  async down(queryRunner) {
+    await queryRunner.query(
+      `ALTER TABLE "dbo"."bank_data" ADD CONSTRAINT "DF_dfe568e239bb915e3f4d75b198e" DEFAULT 1 FOR "active"`,
+    );
+    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit NOT NULL`);
+  }
+};
