8e4520dde724b9bb1dff51658b11dfe09fb2ebb2
David May <david.leo.may@gmail.com>
2023-08-02 12:30:20 +0200

    [DEV-1668] Fixed migration

--- migration/1690971110553-BankDataActiveNullable.js
+++ migration/1690971110553-BankDataActiveNullable.js
@@ -4,14 +4,14 @@ module.exports = class BankDataActiveNullable1690971110553 {
   name = 'BankDataActiveNullable1690971110553';
 
   async up(queryRunner) {
-    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit`);
     await queryRunner.query(`ALTER TABLE "dbo"."bank_data" DROP CONSTRAINT "DF_dfe568e239bb915e3f4d75b198e"`);
+    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit`);
   }
 
   async down(queryRunner) {
+    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit NOT NULL`);
     await queryRunner.query(
       `ALTER TABLE "dbo"."bank_data" ADD CONSTRAINT "DF_dfe568e239bb915e3f4d75b198e" DEFAULT 1 FOR "active"`,
     );
-    await queryRunner.query(`ALTER TABLE "dbo"."bank_data" ALTER COLUMN "active" bit NOT NULL`);
   }
 };
