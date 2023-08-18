2be0d4cb3a412846ce02bb5e54498b509c4bfe74
David May <david.leo.may@gmail.com>
2023-08-18 15:34:23 +0200

    [NO-TASK] Increased price mismatch threshold

--- src/subdomains/supporting/pricing/utils/price-step.ts
+++ src/subdomains/supporting/pricing/utils/price-step.ts
@@ -1,12 +1,12 @@
 import { cloneDeep } from 'lodash';
+import { DfxLogger } from 'src/shared/services/dfx-logger';
+import { Util } from 'src/shared/utils/util';
 import { Price } from '../domain/entities/price';
-import { PriceMismatchException } from '../domain/exceptions/price-mismatch.exception';
 import { Fiat } from '../domain/enums';
+import { PriceMismatchException } from '../domain/exceptions/price-mismatch.exception';
 import { PriceStepResult, PricingProvider, PricingProviderName } from '../domain/interfaces';
 import { PriceStepInitSpecification } from '../domain/specifications/price-step-init.specification';
 import { PricingUtil } from './pricing.util';
-import { DfxLogger } from 'src/shared/services/dfx-logger';
-import { Util } from 'src/shared/utils/util';
 
 export interface PriceStepOptions {
   from?: string | 'input';
@@ -135,7 +135,7 @@ export class PriceStep {
       return 0.005;
     }
 
-    return 0.02;
+    return 0.04;
   }
 
   private async getPrice(
