cfce67fd76a2a830cda53487ab7862d39e23c6d7
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-08-30 16:39:12 +0200

    [DEV-1748] bankTx editable cols (#869)

--- src/subdomains/supporting/bank/bank-tx/dto/update-bank-tx.dto.ts
+++ src/subdomains/supporting/bank/bank-tx/dto/update-bank-tx.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, ValidateIf } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, ValidateIf } from 'class-validator';
 import { BankTxType } from '../bank-tx.entity';
 
 export class UpdateBankTxDto {
@@ -29,6 +29,46 @@ export class UpdateBankTxDto {
   @IsOptional()
   @IsNumber()
   accountingAmountAfterFeeChf?: number;
+
+  @IsOptional()
+  @IsString()
+  iban?: string;
+
+  @IsOptional()
+  @IsString()
+  bic?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  addressLine1?: string;
+
+  @IsOptional()
+  @IsString()
+  addressLine2?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  ultimateName?: string;
+
+  @IsOptional()
+  @IsString()
+  ultimateAddressLine1?: string;
+
+  @IsOptional()
+  @IsString()
+  ultimateAddressLine2?: string;
+
+  @IsOptional()
+  @IsString()
+  ultimateCountry?: string;
 
   @IsNotEmpty()
   @IsInt()
