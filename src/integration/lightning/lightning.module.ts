dd2ac47b1c7b0402ae4f1c79afa6a66ca815309c
David May <david.leo.may@gmail.com>
2023-09-08 08:19:38 +0200

    [DEV-1770] Removed unused code

--- src/integration/lightning/lightning.module.ts
+++ src/integration/lightning/lightning.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from 'src/shared/shared.module';
-import { AlbyController } from './controllers/alby.controller';
 import { LnUrlPForwardController } from './controllers/lnurlp-forward.controller';
 import { LnUrlWForwardController } from './controllers/lnurlw-forward.controller';
 import { LightningService } from './services/lightning.service';
@@ -8,7 +7,7 @@ import { LnUrlForwardService } from './services/lnurl-forward.service';
 
 @Module({
   imports: [SharedModule],
-  controllers: [LnUrlPForwardController, LnUrlWForwardController, AlbyController],
+  controllers: [LnUrlPForwardController, LnUrlWForwardController],
   providers: [LightningService, LnUrlForwardService],
   exports: [LightningService, LnUrlForwardService],
 })
