e581ce2f63f71027f3321754fdb68bed2548f12b
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-09-12 00:33:23 +0200

    [DEV-1622] personal mail refactoring (#871)
    * [DEV-1622] add personal mail refactoring
    
    * [DEV-1622] Refactoring
    
    * [DEV-1622] add html list

--- src/subdomains/supporting/notification/entities/mail/personal-mail.ts
+++ src/subdomains/supporting/notification/entities/mail/personal-mail.ts
@@ -1,4 +1,5 @@
 import { UserData } from 'src/subdomains/generic/user/models/user-data/user-data.entity';
+import { MailAffix } from '../../interfaces';
 import { NotificationMetadata, NotificationOptions } from '../notification.entity';
 import { Mail } from './base/mail';
 
@@ -23,8 +24,25 @@ export interface PersonalMailParams {
   options?: NotificationOptions;
 }
 
+export interface PersonalMailParamsNew {
+  to: string;
+  subject: string;
+  prefix: MailAffix[];
+  banner: string;
+  from?: string;
+  displayName?: string;
+  metadata?: NotificationMetadata;
+  options?: NotificationOptions;
+}
+
 export class PersonalMail extends Mail {
   constructor(params: PersonalMailParams) {
     super({ ...params, template: 'personal', templateParams: params });
+  }
+}
+
+export class PersonalMailNew extends Mail {
+  constructor(params: PersonalMailParamsNew) {
+    super({ ...params, template: 'personalNew', templateParams: params });
   }
 }
