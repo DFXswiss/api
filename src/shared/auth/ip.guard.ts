8ee14164caf0ae52673cebf01243cfbd4dbd4eba
David May <david.leo.may@gmail.com>
2023-10-30 21:58:33 +0100

    [NO-TASK] Disabled IP blocking

--- src/shared/auth/ip.guard.ts
+++ src/shared/auth/ip.guard.ts
@@ -1,13 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { getClientIp } from '@supercharge/request-ip';
+import { DfxLogger } from '../services/dfx-logger';
 
 @Injectable()
 export class IpGuard implements CanActivate {
+  private readonly logger = new DfxLogger(IpGuard);
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const ip = getClientIp(req);
 
-    if (ip !== req.user?.ip) throw new UnauthorizedException('IP is not matching token IP');
+    // TODO: switch back
+    if (ip !== req.user?.ip) this.logger.verbose(`IP mismatch for user ${req.user?.id}`);
+    // if (ip !== req.user?.ip) throw new UnauthorizedException('IP is not matching token IP');
 
     return true;
   }
