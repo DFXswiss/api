908e52f813921fc4d6ef4b66d4a6954cd4223c8d
Yannick1712 <nickbusch1998@gmail.com>
2023-11-08 18:36:06 +0100

    [DEV-1948] add migration

---
+++ migration/1699464913349-addAddtionalFee.js
@@ -0,0 +1,13 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class addAddtionalFee1699464913349 {
+    name = 'addAddtionalFee1699464913349'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "dbo"."fee" ADD "additionalFee" float`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "dbo"."fee" DROP COLUMN "additionalFee"`);
+    }
+}
