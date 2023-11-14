536fdfadc3f7df8aa2edef3a6cc00b5327039008
David May <david.leo.may@gmail.com>
2023-11-14 12:01:20 +0100

    [DEV-1940] Addressed TODO

--- src/subdomains/generic/kyc/services/document-storage.service.ts
+++ src/subdomains/generic/kyc/services/document-storage.service.ts
@@ -1,29 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AzureStorageService, BlobContent } from 'src/integration/infrastructure/azure-storage.service';
-
-// TODO: move to separate file
-export enum KycFileType {
-  NAME_CHECK = 'NameCheck',
-  USER_INFORMATION = 'UserInformation',
-  IDENTIFICATION = 'Identification',
-  USER_NOTES = 'UserNotes',
-  TRANSACTION_NOTES = 'TransactionNotes',
-}
-
-export enum KycContentType {
-  TEXT = 'text/plain',
-  // TODO
-}
-
-export interface KycFile {
-  type: KycFileType;
-  name: string;
-  url: string;
-  contentType: KycContentType;
-  created: Date;
-  updated: Date;
-  metadata: Record<string, string>;
-}
+import { KycContentType, KycFile, KycFileType } from './kyc-file.dto';
 
 @Injectable()
 export class DocumentStorageService {
