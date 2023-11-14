0cb38dbbd65e7ef866255160f2520534ebbd9256
David May <david.leo.may@gmail.com>
2023-11-14 09:52:32 +0100

    [NO-TASK] Updated DeFiChain nodes

--- infrastructure/scripts/nodes.sh
+++ infrastructure/scripts/nodes.sh
@@ -1,11 +1,5 @@
 az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-inp-$2 --slot stg
 az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-dex-$2 --slot stg
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-out-$2 --slot stg
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-int-$2 --slot stg
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-ref-$2 --slot stg
 
 az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-inp-$2
 az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-dex-$2
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-out-$2
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-int-$2
-az webapp $1 --resource-group rg-dfx-api-$2 --name app-dfx-node-ref-$2
