9dcef35bd1ac6861a2e4ab86dccf4a52eca907bf
David May <david.leo.may@gmail.com>
2023-11-18 20:15:43 +0100

    [NO-TASK] Removed KYC step

--- src/subdomains/generic/kyc/entities/kyc-step.entity.ts
+++ src/subdomains/generic/kyc/entities/kyc-step.entity.ts
@@ -1,42 +1,37 @@
-import { IEntity } from 'src/shared/models/entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
-import { UserData } from '../../user/models/user-data/user-data.entity';
-import { KycStepName, KycStepStatus } from '../enums/kyc.enum';
+// @Entity()
+// export class KycStep extends IEntity {
+//   @ManyToOne(() => UserData, (userData) => userData.kycSteps, { nullable: false })
+//   userData: UserData;
 
-@Entity()
-export class KycStep extends IEntity {
-  @ManyToOne(() => UserData, (userData) => userData.kycSteps, { nullable: false })
-  userData: UserData;
+//   @Column()
+//   name: KycStepName;
 
-  @Column()
-  name: KycStepName;
+//   @Column()
+//   status: KycStepStatus;
 
-  @Column()
-  status: KycStepStatus;
+//   @Column({ nullable: true })
+//   sessionId?: string;
 
-  @Column({ nullable: true })
-  sessionId?: string;
+//   // --- FACTORY --- //
+//   static create(name: KycStepName, userData: UserData, sessionId?: string): KycStep {
+//     return Object.assign(new KycStep(), {
+//       name,
+//       userData,
+//       status: KycStepStatus.IN_PROGRESS,
+//       sessionId,
+//     });
+//   }
 
-  // --- FACTORY --- //
-  static create(name: KycStepName, userData: UserData, sessionId?: string): KycStep {
-    return Object.assign(new KycStep(), {
-      name,
-      userData,
-      status: KycStepStatus.IN_PROGRESS,
-      sessionId,
-    });
-  }
+//   // --- KYC PROCESS --- //
+//   complete(): this {
+//     this.status = KycStepStatus.COMPLETED;
 
-  // --- KYC PROCESS --- //
-  complete(): this {
-    this.status = KycStepStatus.COMPLETED;
+//     return this;
+//   }
 
-    return this;
-  }
+//   fail(): this {
+//     this.status = KycStepStatus.FAILED;
 
-  fail(): this {
-    this.status = KycStepStatus.FAILED;
-
-    return this;
-  }
-}
+//     return this;
+//   }
+// }
