445f346f0b755a3cb1479546559f8591b453ba84
Kolibri <daniel@dfx.swiss>
2023-12-07 12:57:10 +0100

    [DEV-1992] update migration

--- migration/1701717309599-kycLog.js
+++ migration/1701717309599-kycLog.js
@@ -6,7 +6,7 @@ module.exports = class kycLog1701717309599 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "dbo"."kyc_log" ADD "status" nvarchar(255)`);
         await queryRunner.query(`ALTER TABLE "dbo"."kyc_log" ADD "kycStepId" int`);
-        await queryRunner.query(`ALTER TABLE "dbo"."kyc_log" ADD CONSTRAINT "FK_0b0568192de90bc31d61a3cf364" FOREIGN KEY ("kycStepId") REFERENCES "sqldb-dfx-api-dev".."kyc_step"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "dbo"."kyc_log" ADD CONSTRAINT "FK_0b0568192de90bc31d61a3cf364" FOREIGN KEY ("kycStepId") REFERENCES "kyc_step"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     async down(queryRunner) {
