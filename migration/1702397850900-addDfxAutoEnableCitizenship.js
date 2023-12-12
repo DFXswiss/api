14a976f29f0eef0638b4fed67a4aa62369580407
Yannick1712 <nickbusch1998@gmail.com>
2023-12-12 17:18:30 +0100

    [DEV-2027] add migration

---
+++ migration/1702397850900-addDfxAutoEnableCitizenship.js
@@ -0,0 +1,14 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class addDfxAutoEnableCitizenship1702397850900 {
+    name = 'addDfxAutoEnableCitizenship1702397850900'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "dbo"."country" ADD "dfxAutoEnableCitizenship" bit NOT NULL CONSTRAINT "DF_c07dc76f654a325cc6cfb9c2553" DEFAULT 1`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "dbo"."country" DROP CONSTRAINT "DF_c07dc76f654a325cc6cfb9c2553"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."country" DROP COLUMN "dfxAutoEnableCitizenship"`);
+    }
+}
