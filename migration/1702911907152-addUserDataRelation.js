729ef429e2b09e7c5ebbaacdc2464bf5c757bc29
Yannick1712 <nickbusch1998@gmail.com>
2023-12-18 16:09:19 +0100

    [DEV-2041] change migration

--- migration/1702911907152-addUserDataRelation.js
+++ migration/1702911907152-addUserDataRelation.js
@@ -1,10 +1,10 @@
 const { MigrationInterface, QueryRunner } = require("typeorm");
 
-module.exports = class addUserDataRelation1702657883484 {
-    name = 'addUserDataRelation1702657883484'
+module.exports = class addUserDataRelation1702911907152 {
+    name = 'addUserDataRelation1702911907152'
 
     async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "user_data_relation" ("id" int NOT NULL IDENTITY(1,1), "updated" datetime2 NOT NULL CONSTRAINT "DF_7c114d731acdf975ea3bef258e6" DEFAULT getdate(), "created" datetime2 NOT NULL CONSTRAINT "DF_2051f287b9f8bda8830f8c8d3e9" DEFAULT getdate(), "relation" nvarchar(256), "signatory" nvarchar(256), "accountId" int NOT NULL, "relatedAccountId" int NOT NULL, CONSTRAINT "PK_ff56997cdfa55669c94fde48735" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user_data_relation" ("id" int NOT NULL IDENTITY(1,1), "updated" datetime2 NOT NULL CONSTRAINT "DF_7c114d731acdf975ea3bef258e6" DEFAULT getdate(), "created" datetime2 NOT NULL CONSTRAINT "DF_2051f287b9f8bda8830f8c8d3e9" DEFAULT getdate(), "relation" nvarchar(256) NOT NULL, "signatory" nvarchar(256) NOT NULL, "accountId" int NOT NULL, "relatedAccountId" int NOT NULL, CONSTRAINT "PK_ff56997cdfa55669c94fde48735" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "user_data_relation" ADD CONSTRAINT "FK_56f39477fa929bd1a8c93ad66d2" FOREIGN KEY ("accountId") REFERENCES "user_data"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_data_relation" ADD CONSTRAINT "FK_02a41115677a4916bb523c427d1" FOREIGN KEY ("relatedAccountId") REFERENCES "user_data"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
