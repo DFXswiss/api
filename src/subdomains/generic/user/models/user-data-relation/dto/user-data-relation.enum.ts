00823b88ce3e0a7514f52d93e801d07410f41f1e
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-12-19 14:08:55 +0100

    [NOTASK] add ControlHolder to enum

--- src/subdomains/generic/user/models/user-data-relation/dto/user-data-relation.enum.ts
+++ src/subdomains/generic/user/models/user-data-relation/dto/user-data-relation.enum.ts
@@ -2,6 +2,7 @@ export enum UserDataRelationState {
   CONVERSION_PARTNER = 'ConversionPartner',
   BENEFICIAL_OWNER = 'BeneficialOwner',
   CONTRACTING_PARTNER = 'ContractingPartner',
+  CONTROL_HOLDER = 'ControlHolder',
   WIFE = 'Wife',
   HUSBAND = 'Husband',
 }
