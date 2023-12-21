d2bc31cd9a88767b6731272db481090c20d2e2a2
David May <david.leo.may@gmail.com>
2023-12-21 18:20:55 +0100

    [DEV-1907] Fix auth

--- src/integration/alchemy/alchemy.module.ts
+++ src/integration/alchemy/alchemy.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SharedModule } from 'src/shared/shared.module';
 import { AlchemyController } from './controllers/alchemy.controller';
 import { AlchemyWebhookService } from './services/alchemy-webhook.service';
 import { AlchemyService } from './services/alchemy.service';
 
 @Module({
-  imports: [],
+  imports: [SharedModule],
   controllers: [AlchemyController],
   providers: [AlchemyService, AlchemyWebhookService],
   exports: [AlchemyService, AlchemyWebhookService],
