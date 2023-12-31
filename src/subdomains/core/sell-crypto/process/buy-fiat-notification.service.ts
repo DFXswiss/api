9630a7c0fecf574bbe25aba080c258397d31bb8c
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2023-12-31 10:57:27 +0100

    [DEV-2072] fix pending mails - missing url

--- src/subdomains/core/sell-crypto/process/buy-fiat-notification.service.ts
+++ src/subdomains/core/sell-crypto/process/buy-fiat-notification.service.ts
@@ -266,8 +266,14 @@ export class BuyFiatNotificationService {
               },
               suffix: [
                 { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line1` },
-                { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line2` },
-                { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line3` },
+                {
+                  key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line2`,
+                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
+                },
+                {
+                  key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line3`,
+                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
+                },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line4`,
                   params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
