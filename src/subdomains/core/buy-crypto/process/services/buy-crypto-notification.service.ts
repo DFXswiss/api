a1aa61f5be2e498f2cde8c79fc2c020201491cc9
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-01-03 13:39:59 +0100

    [DEV-2072] fix to service url

--- src/subdomains/core/buy-crypto/process/services/buy-crypto-notification.service.ts
+++ src/subdomains/core/buy-crypto/process/services/buy-crypto-notification.service.ts
@@ -266,15 +266,15 @@ export class BuyCryptoNotificationService {
                 { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line1` },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line2`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.user.userData.kycHash}` },
                 },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line3`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.user.userData.kycHash}` },
                 },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line4`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.user.userData.kycHash}` },
                 },
                 { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line5` },
                 { key: MailKey.SPACE, params: { value: '1' } },
--- src/subdomains/core/sell-crypto/process/buy-fiat-notification.service.ts
+++ src/subdomains/core/sell-crypto/process/buy-fiat-notification.service.ts
@@ -268,15 +268,15 @@ export class BuyFiatNotificationService {
                 { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line1` },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line2`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.sell.user.userData.kycHash}` },
                 },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line3`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.sell.user.userData.kycHash}` },
                 },
                 {
                   key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line4`,
-                  params: { url: `${Config.frontend.payment}/kyc?code=${entity.sell.user.userData.kycHash}` },
+                  params: { url: `${Config.frontend.services}/kyc?code=${entity.sell.user.userData.kycHash}` },
                 },
                 { key: `${MailFactory.parseMailKey(MailTranslationKey.PENDING, entity.amlReason)}.line5` },
                 { key: MailKey.SPACE, params: { value: '1' } },
