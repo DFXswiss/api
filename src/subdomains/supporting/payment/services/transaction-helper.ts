11ddb5d75af5938f021b645626a66d36eedc027c
David May <david.leo.may@gmail.com>
2024-02-17 10:18:50 +0100

    [NO-TASK] Fixed max volume for quotes

--- src/subdomains/supporting/payment/services/transaction-helper.ts
+++ src/subdomains/supporting/payment/services/transaction-helper.ts
@@ -191,10 +191,9 @@ export class TransactionHelper implements OnModuleInit {
     const extendedSpecs = {
       ...specs,
       minFee: fee.blockchain,
-      maxVolume:
-        user?.userData.availableTradingLimit ?? [paymentMethodIn, paymentMethodOut].includes(FiatPaymentMethod.CARD)
-          ? Config.defaultCardTradingLimit
-          : Config.defaultTradingLimit,
+      maxVolume: [paymentMethodIn, paymentMethodOut].includes(FiatPaymentMethod.CARD)
+        ? Math.min(user?.userData.availableTradingLimit ?? Infinity, Config.defaultCardTradingLimit)
+        : user?.userData.availableTradingLimit ?? Config.defaultTradingLimit,
       fixedFee: fee.fixed,
     };
 
