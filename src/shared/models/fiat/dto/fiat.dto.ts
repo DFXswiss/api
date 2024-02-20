8461801a637a42039bfe2795cb7c2cefe1789d0e
David May <85513542+davidleomay@users.noreply.github.com>
2024-02-20 15:29:51 +0100

    [DEV-2236] Improved payment webhook, asset and fiat endpoint (#1103)
    * [DEV-2236] Added asset IDs and payment methods to transaction DTO
    
    * [DEV-2236] Fixed unit tests
    
    * [DEV-2236] Added volumes to assets/fiats
    
    * [DEV-2236] Removed useless unit test
    
    * [DEV-2236] Refactored fiat/asset limits
    
    * [DEV-2236] Added asset CHF price, refactoring

--- src/shared/models/fiat/dto/fiat.dto.ts
+++ src/shared/models/fiat/dto/fiat.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { FiatPaymentMethod } from 'src/subdomains/supporting/payment/dto/payment-method.enum';
 
 export class FiatDto {
   @ApiProperty()
@@ -24,4 +25,28 @@ export class FiatDto {
 
   @ApiProperty()
   instantSellable: boolean;
+}
+
+export class VolumeLimitDto {
+  @ApiProperty({ description: 'Minimum transaction volume (in fiat)' })
+  minVolume: number;
+
+  @ApiProperty({ description: 'Maximum transaction volume (in fiat)' })
+  maxVolume: number;
+}
+
+export class FiatLimitsDto {
+  @ApiProperty()
+  [FiatPaymentMethod.BANK]: VolumeLimitDto;
+
+  @ApiProperty()
+  [FiatPaymentMethod.INSTANT]: VolumeLimitDto;
+
+  @ApiProperty()
+  [FiatPaymentMethod.CARD]: VolumeLimitDto;
+}
+
+export class FiatDetailDto extends FiatDto {
+  @ApiProperty()
+  limits: FiatLimitsDto;
 }
