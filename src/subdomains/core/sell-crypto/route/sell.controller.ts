a05a0624286be51c4b3e53e68a10617d015ac525
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2024-02-21 08:47:57 +0100

    [DEV-1664] Price guarantee (#1089)
    * [DEV-1664] add transaction request table
    
    * [DEV-1664] Refactoring, fix tests and add migration
    
    * [DEV-1664] Adapted fields
    
    * [DEV-1664] Removed useless code
    
    * [DEV-1664] Do not wait for transaction request creation
    
    ---------
    
    Co-authored-by: Kolibri <daniel@dfx.swiss>
    Co-authored-by: David May <david.leo.may@gmail.com>

--- src/subdomains/core/sell-crypto/route/sell.controller.ts
+++ src/subdomains/core/sell-crypto/route/sell.controller.ts
@@ -15,7 +15,9 @@ import { Util } from 'src/shared/utils/util';
 import { UserService } from 'src/subdomains/generic/user/models/user/user.service';
 import { DepositDtoMapper } from 'src/subdomains/supporting/address-pool/deposit/dto/deposit-dto.mapper';
 import { CryptoPaymentMethod, FiatPaymentMethod } from 'src/subdomains/supporting/payment/dto/payment-method.enum';
+import { TransactionRequestType } from 'src/subdomains/supporting/payment/entities/transaction-request.entity';
 import { TransactionHelper } from 'src/subdomains/supporting/payment/services/transaction-helper';
+import { TransactionRequestService } from 'src/subdomains/supporting/payment/services/transaction-request.service';
 import { BuyFiatService } from '../process/services/buy-fiat.service';
 import { CreateSellDto } from './dto/create-sell.dto';
 import { GetSellPaymentInfoDto } from './dto/get-sell-payment-info.dto';
@@ -38,6 +40,7 @@ export class SellController {
     private readonly paymentInfoService: PaymentInfoService,
     private readonly transactionHelper: TransactionHelper,
     private readonly cryptoService: CryptoService,
+    private readonly transactionRequestService: TransactionRequestService,
   ) {}
 
   @Get()
@@ -205,7 +208,7 @@ export class SellController {
       user,
     );
 
-    return {
+    const sellDto: SellPaymentInfoDto = {
       routeId: sell.id,
       fee: Util.round(fee.rate * 100, Config.defaultPercentageDecimal),
       depositAddress: sell.deposit.address,
@@ -227,5 +230,9 @@ export class SellController {
       isValid,
       error,
     };
+
+    void this.transactionRequestService.createTransactionRequest(TransactionRequestType.Sell, dto, sellDto);
+
+    return sellDto;
   }
 }
