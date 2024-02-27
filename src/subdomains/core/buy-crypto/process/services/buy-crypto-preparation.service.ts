8b621324d9442ec9d4720936617aa5ad9966c5c5
Kolibri <daniel@dfx.swiss>
2024-02-27 17:09:06 +0100

    [DEV-2269] remove unused line

--- src/subdomains/core/buy-crypto/process/services/buy-crypto-preparation.service.ts
+++ src/subdomains/core/buy-crypto/process/services/buy-crypto-preparation.service.ts
@@ -169,7 +169,6 @@ export class BuyCryptoPreparationService {
             fee.total,
             referenceChfPrice.convert(fee.total, 2),
             transactionRequest,
-            transactionRequest.externalTransactionId,
           ),
         );
 
