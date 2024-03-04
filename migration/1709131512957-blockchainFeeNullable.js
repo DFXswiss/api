6478f5ca342254323083a4327a201f9238dad6e4
Kolibri <daniel@dfx.swiss>
2024-03-04 12:40:27 +0100

    [DEV-1954] Fix migration

--- migration/1709131512957-blockchainFeeNullable.js
+++ migration/1709131512957-blockchainFeeNullable.js
@@ -4,14 +4,14 @@ module.exports = class blockchainFeeNullable1709131512957 {
     name = 'blockchainFeeNullable1709131512957'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE ""dbo"."buy_fiat" DROP COLUMN "outputReferenceAsset"`);
-        await queryRunner.query(`ALTER TABLE ""dbo"."buy_fiat" DROP COLUMN "outputAsset"`);
-        await queryRunner.query(`ALTER TABLE ""dbo"."blockchain_fee" ALTER COLUMN "amount" float`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" DROP COLUMN "outputReferenceAsset"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" DROP COLUMN "outputAsset"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."blockchain_fee" ALTER COLUMN "amount" float`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE ""dbo"."blockchain_fee" ALTER COLUMN "amount" float NOT NULL`);
-        await queryRunner.query(`ALTER TABLE ""dbo"."buy_fiat" ADD "outputAsset" nvarchar(256)`);
-        await queryRunner.query(`ALTER TABLE ""dbo"."buy_fiat" ADD "outputReferenceAsset" nvarchar(256)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."blockchain_fee" ALTER COLUMN "amount" float NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ADD "outputAsset" nvarchar(256)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ADD "outputReferenceAsset" nvarchar(256)`);
     }
 }
