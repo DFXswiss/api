90623a7a564a0fa3dd163d586ffca4d6b6359524
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-03-15 14:48:47 +0100

    [DEV-2360] add wallet amlRule (#1182)
    * [DEV-2360] add wallet amlRule
    
    * [DEV-2360] add migration

--- src/subdomains/supporting/payment/dto/transaction-error.enum.ts
+++ src/subdomains/supporting/payment/dto/transaction-error.enum.ts
@@ -2,5 +2,6 @@ export enum TransactionError {
   AMOUNT_TOO_LOW = 'AmountTooLow',
   AMOUNT_TOO_HIGH = 'AmountTooHigh',
   BANK_TRANSACTION_MISSING = 'BankTransactionMissing',
+  KYC_REQUIRED_INSTANT = 'KycRequiredInstant',
   KYC_REQUIRED = 'KycRequired',
 }
