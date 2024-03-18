051a06d4d45d3b4e6ff364e6002a07a8475a3c0b
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-03-18 11:32:27 +0100

    [DEV-2303] dev merge refactoring

--- src/subdomains/core/aml/aml.service.ts
+++ src/subdomains/core/aml/aml.service.ts
@@ -12,6 +12,7 @@ import { SpecialExternalBankAccount } from 'src/subdomains/supporting/bank/speci
 import { SpecialExternalBankAccountService } from 'src/subdomains/supporting/bank/special-external-bank-account/special-external-bank-account.service';
 import { BuyCrypto } from '../buy-crypto/process/entities/buy-crypto.entity';
 import { BuyFiat } from '../sell-crypto/process/buy-fiat.entity';
+import { AmlError } from './enums/aml-error.enum';
 import { CheckStatus } from './enums/check-status.enum';
 
 @Injectable()
@@ -31,83 +32,85 @@ export class AmlService {
     bankDataUserDataId: number,
     blacklist: SpecialExternalBankAccount[],
     instantBanks?: Bank[],
-  ): string[] {
+  ): AmlError[] {
     const errors = [];
 
-    if (entity.inputReferenceAmount < minVolume * 0.9) errors.push('MinVolumeNotReached');
-    if (!entity.user.isPaymentStatusEnabled) errors.push('InvalidUserStatus');
-    if (!entity.userData.isPaymentStatusEnabled) errors.push('InvalidUserDataStatus');
-    if (!entity.userData.isPaymentKycStatusEnabled) errors.push('InvalidKycStatus');
-    if (entity.userData.kycType !== KycType.DFX) errors.push('InvalidKycType');
-    if (!entity.userData.verifiedName) errors.push('NoVerifiedName');
-    if (!entity.userData.verifiedCountry) errors.push('NoVerifiedCountry');
-    if (!entity.userData.lastNameCheckDate) errors.push('NoNameCheck');
+    if (entity.inputReferenceAmount < minVolume * 0.9) errors.push(AmlError.MIN_VOLUME_NOT_REACHED);
+    if (!entity.user.isPaymentStatusEnabled) errors.push(AmlError.INVALID_USER_STATUS);
+    if (!entity.userData.isPaymentStatusEnabled) errors.push(AmlError.INVALID_USER_DATA_STATUS);
+    if (!entity.userData.isPaymentKycStatusEnabled) errors.push(AmlError.INVALID_KYC_STATUS);
+    if (entity.userData.kycType !== KycType.DFX) errors.push(AmlError.INVALID_KYC_TYPE);
+    if (!entity.userData.verifiedName) errors.push(AmlError.NO_VERIFIED_NAME);
+    if (!entity.userData.verifiedCountry) errors.push(AmlError.NO_VERIFIED_COUNTRY);
+    if (!entity.userData.lastNameCheckDate) errors.push(AmlError.NO_NAME_CHECK);
     if (Util.daysDiff(entity.userData.lastNameCheckDate) > Config.amlCheckLastNameCheckValidity)
-      errors.push('OutdatedNameCheck');
-    if (last30dVolume > Config.tradingLimits.monthlyDefault) errors.push('MonthlyLimitReached');
+      errors.push(AmlError.OUTDATED_NAME_CHECK);
+    if (last30dVolume > Config.tradingLimits.monthlyDefault) errors.push(AmlError.MONTHLY_LIMIT_REACHED);
     if (last24hVolume > Config.tradingLimits.dailyDefault) {
       // KYC required
-      if (entity.userData.kycLevel < KycLevel.LEVEL_50) errors.push('KycLevelTooLow');
-      if (!entity.userData.hasBankTxVerification) errors.push('NoBankTxVerification');
-      if (!entity.userData.letterSentDate) errors.push('NoLetter');
-      if (!entity.userData.amlListAddedDate) errors.push('NoAmlList');
-      if (!entity.userData.kycFileId) errors.push('NoKycFileId');
+      if (entity.userData.kycLevel < KycLevel.LEVEL_50) errors.push(AmlError.KYC_LEVEL_TOO_LOW);
+      if (!entity.userData.hasBankTxVerification) errors.push(AmlError.NO_BANK_TX_VERIFICATION);
+      if (!entity.userData.letterSentDate) errors.push(AmlError.NO_LETTER);
+      if (!entity.userData.amlListAddedDate) errors.push(AmlError.NO_AML_LIST);
+      if (!entity.userData.kycFileId) errors.push(AmlError.NO_KYC_FILE_ID);
       if (entity.userData.annualBuyVolume + amountInChf > entity.userData.depositLimit)
-        errors.push('DepositLimitReached');
+        errors.push(AmlError.DEPOSIT_LIMIT_REACHED);
     }
 
     if (entity instanceof BuyFiat || !entity.cryptoInput) {
       if (!bankDataUserDataId) {
-        errors.push('BankDataMissing');
+        errors.push(AmlError.BANK_DATA_MISSING);
       } else if (entity.userData.id !== bankDataUserDataId) {
-        errors.push('BankDataUserMismatch');
+        errors.push(AmlError.BANK_DATA_USER_MISMATCH);
       }
     }
 
     if (entity.cryptoInput) {
       // crypto input
-      if (entity.cryptoInput.amlCheck !== CheckStatus.PASS) errors.push('InputAmlFailed');
-      if (!entity.cryptoInput.isConfirmed) errors.push('InputNotConfirmed');
-      if (entity instanceof BuyCrypto && !entity.userData.cryptoCryptoAllowed) errors.push('CryptoNotAllowed');
-    } else if (entity.user.status === UserStatus.NA && entity.userData.hasSuspiciousMail) errors.push('SuspiciousMail');
+      if (entity.cryptoInput.amlCheck !== CheckStatus.PASS) errors.push(AmlError.INPUT_AML_CHECK_FAILED);
+      if (!entity.cryptoInput.isConfirmed) errors.push(AmlError.INPUT_NOT_CONFIRMED);
+      if (entity instanceof BuyCrypto && !entity.userData.cryptoCryptoAllowed)
+        errors.push(AmlError.CRYPTO_CRYPTO_NOT_ALLOWED);
+    } else if (entity.user.status === UserStatus.NA && entity.userData.hasSuspiciousMail)
+      errors.push(AmlError.SUSPICIOUS_MAIL);
 
     if (entity instanceof BuyCrypto) {
       // buyCrypto
-      if (!entity.target.asset.buyable) errors.push('AssetNotBuyable');
+      if (!entity.target.asset.buyable) errors.push(AmlError.ASSET_NOT_BUYABLE);
 
       switch (entity.user.wallet.amlRule) {
         case AmlRule.DEFAULT:
           break;
         case AmlRule.RULE_1:
           if (entity.checkoutTx && entity.user.status === UserStatus.NA && entity.checkoutTx.ip !== entity.user.ip)
-            errors.push('IpMismatch');
+            errors.push(AmlError.IP_MISMATCH);
           break;
         case AmlRule.RULE_2:
-          if (entity.userData.kycLevel < KycLevel.LEVEL_30) errors.push('KycLevel30NotReached');
+          if (entity.userData.kycLevel < KycLevel.LEVEL_30) errors.push(AmlError.KYC_LEVEL_30_NOT_REACHED);
           break;
         case AmlRule.RULE_3:
-          if (entity.userData.kycLevel < KycLevel.LEVEL_50) errors.push('KycLevel50NotReached');
+          if (entity.userData.kycLevel < KycLevel.LEVEL_50) errors.push(AmlError.KYC_LEVEL_50_NOT_REACHED);
           break;
       }
 
       if (entity.bankTx) {
         // bank
-        if (blacklist.some((b) => b.bic && b.bic === entity.bankTx.bic)) errors.push('BicBlacklisted');
-        if (blacklist.some((b) => b.iban && b.iban === entity.bankTx.iban)) errors.push('IbanBlacklisted');
+        if (blacklist.some((b) => b.bic && b.bic === entity.bankTx.bic)) errors.push(AmlError.BIC_BLACKLISTED);
+        if (blacklist.some((b) => b.iban && b.iban === entity.bankTx.iban)) errors.push(AmlError.IBAN_BLACKLISTED);
         if (instantBanks?.some((b) => b.iban === entity.bankTx.accountIban)) {
-          if (!entity.userData.olkypayAllowed) errors.push('InstantNotAllowed');
-          if (!entity.target.asset.instantBuyable) errors.push('AssetNotInstantBuyable');
+          if (!entity.userData.olkypayAllowed) errors.push(AmlError.INSTANT_NOT_ALLOWED);
+          if (!entity.target.asset.instantBuyable) errors.push(AmlError.ASSET_NOT_INSTANT_BUYABLE);
         }
       } else if (entity.checkoutTx) {
         // checkout
-        if (!entity.target.asset.cardBuyable) errors.push('AssetNotCardBuyable');
+        if (!entity.target.asset.cardBuyable) errors.push(AmlError.ASSET_NOT_CARD_BUYABLE);
         if (blacklist.some((b) => b.iban && b.iban === entity.checkoutTx.cardFingerPrint))
-          errors.push('CardBlacklisted');
+          errors.push(AmlError.CARD_BLACKLISTED);
       }
     } else {
       // buyFiat
-      if (!entity.target.asset.sellable) errors.push('AssetNotSellable');
-      if (blacklist.some((b) => b.iban && b.iban === entity.sell.iban)) errors.push('IbanBlacklisted');
+      if (!entity.target.asset.sellable) errors.push(AmlError.ASSET_NOT_SELLABLE);
+      if (blacklist.some((b) => b.iban && b.iban === entity.sell.iban)) errors.push(AmlError.IBAN_BLACKLISTED);
     }
 
     return errors;
