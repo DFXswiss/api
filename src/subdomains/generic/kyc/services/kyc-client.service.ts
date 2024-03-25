bf94780bb853c70e4424ac88682363136fe4ba81
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-03-26 00:30:51 +0100

    [DEV-1791] kyc client user payments endpoint

--- src/subdomains/generic/kyc/services/kyc-client.service.ts
+++ src/subdomains/generic/kyc/services/kyc-client.service.ts
@@ -1,7 +1,11 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BuyCryptoWebhookService } from 'src/subdomains/core/buy-crypto/process/services/buy-crypto-webhook.service';
+import { BuyCryptoService } from 'src/subdomains/core/buy-crypto/process/services/buy-crypto.service';
+import { BuyFiatService } from 'src/subdomains/core/sell-crypto/process/services/buy-fiat.service';
 import { User } from '../../user/models/user/user.entity';
 import { UserService } from '../../user/models/user/user.service';
 import { WalletService } from '../../user/models/wallet/wallet.service';
+import { PaymentWebhookData } from '../../user/services/webhook/dto/payment-webhook.dto';
 import { WebhookDataMapper } from '../../user/services/webhook/mapper/webhook-data.mapper';
 import {
   KycClientDataDto,
@@ -19,6 +23,9 @@ export class KycClientService {
     private readonly storageService: DocumentStorageService,
     private readonly userService: UserService,
     private readonly walletService: WalletService,
+    private readonly buyCryptoService: BuyCryptoService,
+    private readonly buyCryptoWebhookService: BuyCryptoWebhookService,
+    private readonly buyFiatService: BuyFiatService,
   ) {}
 
   async getAllKycData(walletId: number): Promise<KycClientDataDto[]> {
@@ -26,6 +33,34 @@ export class KycClientService {
     if (!wallet) throw new NotFoundException('Wallet not found');
 
     return wallet.users.map((b) => this.toKycDataDto(b));
+  }
+
+  async getAllPayments(walletId: number, dateFrom: Date, dateTo: Date): Promise<PaymentWebhookData[]> {
+    const wallet = await this.walletService.getByIdOrName(walletId, undefined, { users: { userData: true } });
+    if (!wallet) throw new NotFoundException('Wallet not found');
+
+    const payments: PaymentWebhookData[] = [];
+
+    for (const user of wallet.users) {
+      payments.push(...(await this.getWebhookData(user.id, dateFrom, dateTo)));
+    }
+
+    return payments;
+  }
+
+  async getAllUserPayments(
+    walletId: number,
+    userAddress: string,
+    dateFrom: Date,
+    dateTo: Date,
+  ): Promise<PaymentWebhookData[]> {
+    const wallet = await this.walletService.getByIdOrName(walletId, undefined, { users: { userData: true } });
+    if (!wallet) throw new NotFoundException('Wallet not found');
+
+    const user = wallet.users.find((u) => u.address === userAddress);
+    if (!user) throw new NotFoundException('User not found');
+
+    return this.getWebhookData(user.id, dateFrom, dateTo);
   }
 
   async getKycFiles(userAddress: string, walletId: number): Promise<KycReportDto[]> {
@@ -53,6 +88,32 @@ export class KycClientService {
   }
 
   // --- HELPER METHODS --- //
+  private async getWebhookData(userId: number, dateFrom: Date, dateTo: Date): Promise<PaymentWebhookData[]> {
+    const payments: PaymentWebhookData[] = [];
+
+    const buyCryptos = await this.buyCryptoService.getUserTransactions(userId, dateFrom, dateTo);
+    for (const buyCrypto of buyCryptos) {
+      payments.push(
+        await this.buyCryptoWebhookService
+          .extendBuyCrypto(buyCrypto)
+          .then((b) =>
+            b.isCryptoCryptoTransaction
+              ? WebhookDataMapper.mapCryptoCryptoData(b)
+              : WebhookDataMapper.mapFiatCryptoData(b),
+          ),
+      );
+    }
+
+    const buyFiats = await this.buyFiatService.getUserTransactions(userId, dateFrom, dateTo);
+    for (const buyFiat of buyFiats) {
+      payments.push(
+        await this.buyFiatService.extendBuyFiat(buyFiat).then((b) => WebhookDataMapper.mapCryptoFiatData(b)),
+      );
+    }
+
+    return payments;
+  }
+
   private getFileFor(type: KycReportType, documents: KycFile[]): KycFile | undefined {
     switch (type) {
       case KycReportType.IDENTIFICATION:
