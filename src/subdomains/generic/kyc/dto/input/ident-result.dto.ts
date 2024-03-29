1202ff6f29cb309a8cb89235076a9916b76dd0d2
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-03-29 03:11:00 +0100

    [DEV-2383] describe ident error codes

--- src/subdomains/generic/kyc/dto/input/ident-result.dto.ts
+++ src/subdomains/generic/kyc/dto/input/ident-result.dto.ts
@@ -9,6 +9,19 @@ export enum IdentResult {
   FRAUD_SUSPICION_CONFIRMED = 'FRAUD_SUSPICION_CONFIRMED',
 }
 
+export enum IdentReason {
+  TECH_HOLOGRAM = 'TECH_HOLOGRAM',
+  TECH_PHOTO = 'TECH_PHOTO',
+  BAD_PHOTO_QUALITY = 'BAD_PHOTO_QUALITY',
+  USER_ID = 'USER_ID',
+  ID_DAMAGED = 'ID_DAMAGED',
+  ID_OTHER = 'ID_OTHER',
+  ID_NOT_SUPPORTED = 'ID_NOT_SUPPORTED',
+  USER_WRONG_PERSON = 'USER_WRONG_PERSON',
+  PHOTO_OTHER = 'PHOTO_OTHER',
+  OTHER_ERROR = 'OTHER_ERROR',
+}
+
 export enum IdentShortResult {
   CANCEL = 'Cancel',
   ABORT = 'Abort',
@@ -33,7 +46,7 @@ export interface IdentItem {
 export interface IdentResultDto {
   identificationprocess: {
     result: IdentResult;
-    reason: string;
+    reason: IdentReason;
     companyid: string;
     filename: string;
     agentname: string;
@@ -91,6 +104,27 @@ const IdentResultMap: Record<IdentResult, IdentShortResult> = {
   [IdentResult.FRAUD_SUSPICION_CONFIRMED]: IdentShortResult.FAIL,
 };
 
+const IdentReasonMap: Record<IdentReason, string> = {
+  [IdentReason.TECH_HOLOGRAM]: 'Security features on the ID document are not clearly visible',
+  [IdentReason.TECH_PHOTO]: 'Photo of the ID document is not clearly captured',
+  [IdentReason.ID_OTHER]:
+    "Miscellaneous reason related to the ID document used during the identification like 'Expired document'",
+  [IdentReason.ID_NOT_SUPPORTED]: 'ID document which is not supported by our system',
+  [IdentReason.BAD_PHOTO_QUALITY]:
+    'Photo of the person during selfie or liveness process and or the document is not clearly visible',
+  [IdentReason.USER_ID]:
+    'User used an ID document which has issues like front side of the document scanned in the back side section, wrong page of passport scanned and other miscellaneous reasons',
+  [IdentReason.ID_DAMAGED]: 'Document ID chosen in the process is damaged',
+  [IdentReason.USER_WRONG_PERSON]: 'User is not the person who should have performed the identification',
+  [IdentReason.PHOTO_OTHER]:
+    'Miscellaneous reason related to the photo quality of the document or of the person which do not fit in any of the categories',
+  [IdentReason.OTHER_ERROR]: 'Other miscellaneous reason which cannot be categorized',
+};
+
 export function getIdentResult(dto: IdentResultDto): IdentShortResult {
   return IdentResultMap[dto.identificationprocess.result];
+}
+
+export function getIdentReason(reason: IdentReason | string): string {
+  return IdentReasonMap[reason] ?? reason;
 }
