57af02a1f38a9ccdeb280d77d129add6b0472c48
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-03-30 10:16:04 +0100

    [DEV-2383] add new error codes

--- src/subdomains/generic/kyc/dto/input/ident-result.dto.ts
+++ src/subdomains/generic/kyc/dto/input/ident-result.dto.ts
@@ -14,12 +14,51 @@ export enum IdentReason {
   TECH_PHOTO = 'TECH_PHOTO',
   BAD_PHOTO_QUALITY = 'BAD_PHOTO_QUALITY',
   USER_ID = 'USER_ID',
-  ID_DAMAGED = 'ID_DAMAGED',
-  ID_OTHER = 'ID_OTHER',
-  ID_NOT_SUPPORTED = 'ID_NOT_SUPPORTED',
   USER_WRONG_PERSON = 'USER_WRONG_PERSON',
   PHOTO_OTHER = 'PHOTO_OTHER',
   OTHER_ERROR = 'OTHER_ERROR',
+  //New Reasons
+  ID_OTHER = 'ID_OTHER',
+  ID_DAMAGED = 'ID_DAMAGED',
+  ID_NOT_SUPPORTED = 'ID_NOT_SUPPORTED',
+  ID_BLURRY = 'ID_BLURRY',
+  ID_GLARE = 'ID_GLARE',
+  ID_DARKNESS = 'ID_DARKNESS',
+  ID_DATA_COVERED = 'ID_DATA_COVERED',
+  ID_PERSPECTIVE = 'ID_PERSPECTIVE',
+  ID_DATA = 'ID_DATA',
+  ID_DATA_OTHER = 'ID_DATA_OTHER',
+  ID_EXPIRED = 'ID_EXPIRED',
+  ID_WRONG_SIDE = 'ID_WRONG_SIDE',
+  ID_OUTWORN = 'ID_OUTWORN',
+  ID_HAS_STICKER = 'ID_HAS_STICKER',
+  ID_WRITTEN_ON = 'ID_WRITTEN_ON',
+  ID_BROKEN = 'ID_BROKEN',
+  ID_DAMAGED_OTHER = 'ID_DAMAGED_OTHER',
+  ID_SECURITY_FEATURE_NOT_VISIBLE_NOT_FRAUD = 'ID_SECURITY_FEATURE_NOT_VISIBLE_NOT_FRAUD	',
+  ID_SECURITY_FEATURE_VIDEO_SHORT = 'ID_SECURITY_FEATURE_VIDEO_SHORT',
+  ID_SECURITY_FEATURE_VIDEO_CANNOT_BE_PLAYED = 'ID_SECURITY_FEATURE_VIDEO_CANNOT_BE_PLAYED',
+  ID_SECURITY_FEATURE_OTHER = 'ID_SECURITY_FEATURE_OTHER',
+  ID_SECOND_DOCUMENT = 'ID_SECOND_DOCUMENT',
+  ID_SECOND_DOCUMENT_BAD_PHOTO_QUALITY = 'ID_SECOND_DOCUMENT_BAD_PHOTO_QUALITY',
+  ID_SECOND_DOCUMENT_DAMAGED = 'ID_SECOND_DOCUMENT_DAMAGED',
+  ID_SECOND_DOCUMENT_EXPIRED = 'ID_SECOND_DOCUMENT_EXPIRED',
+  ID_SECOND_DOCUMENT_OTHER = 'ID_SECOND_DOCUMENT_OTHER',
+  ID_NEED_ADDITIONAL_DOCUMENT = 'ID_NEED_ADDITIONAL_DOCUMENT',
+  USER_INVOICE_MISSING = 'USER_INVOICE_MISSING',
+  USER_OBSCURED = 'USER_OBSCURED',
+  SELFIE_BLURRY = 'SELFIE_BLURRY',
+  SELFIE_GLARE = 'SELFIE_GLARE',
+  SELFIE_DARKNESS = 'SELFIE_DARKNESS	',
+  SELFIE_PERSPECTIVE = 'SELFIE_PERSPECTIVE',
+  SELFIE_OTHER = 'SELFIE_OTHER',
+  IDENT_CANNOT_BE_COMPLETED = 'IDENT_CANNOT_BE_COMPLETED',
+  IDENT_DISPLAY_ERROR = 'IDENT_DISPLAY_ERROR',
+  IDENT_OTHER = 'IDENT_OTHER',
+  TSP_WRONG_CONFIRMATION_TOKEN = 'TSP_WRONG_CONFIRMATION_TOKEN',
+  TSP_SIGNING_FAILED = 'TSP_SIGNING_FAILED',
+  TSP_CERTIFICATE_EXPIRED = 'TSP_CERTIFICATE_EXPIRED',
+  ID_SECURITY_FEATURE_VIDEO_FILE_MISSING = 'ID_SECURITY_FEATURE_VIDEO_FILE_MISSING',
 }
 
 export enum IdentShortResult {
@@ -107,18 +146,78 @@ const IdentResultMap: Record<IdentResult, IdentShortResult> = {
 const IdentReasonMap: Record<IdentReason, string> = {
   [IdentReason.TECH_HOLOGRAM]: 'Security features on the ID document are not clearly visible',
   [IdentReason.TECH_PHOTO]: 'Photo of the ID document is not clearly captured',
-  [IdentReason.ID_OTHER]:
-    "Miscellaneous reason related to the ID document used during the identification like 'Expired document'",
-  [IdentReason.ID_NOT_SUPPORTED]: 'ID document which is not supported by our system',
   [IdentReason.BAD_PHOTO_QUALITY]:
     'Photo of the person during selfie or liveness process and or the document is not clearly visible',
   [IdentReason.USER_ID]:
-    'User used an ID document which has issues like front side of the document scanned in the back side section, wrong page of passport scanned and other miscellaneous reasons',
-  [IdentReason.ID_DAMAGED]: 'Document ID chosen in the process is damaged',
-  [IdentReason.USER_WRONG_PERSON]: 'User is not the person who should have performed the identification',
+    'ID document has issues like front side of the document scanned in the back side section, wrong page of passport scanned and other miscellaneous reasons',
+  [IdentReason.USER_WRONG_PERSON]: 'You are not the person who should have performed the identification',
   [IdentReason.PHOTO_OTHER]:
     'Miscellaneous reason related to the photo quality of the document or of the person which do not fit in any of the categories',
   [IdentReason.OTHER_ERROR]: 'Other miscellaneous reason which cannot be categorized',
+  // new reasons
+  [IdentReason.ID_OTHER]:
+    "Miscellaneous reason related to the ID document used during the identification like 'Expired document'", // TODO change with new System to 'Other issues with the document used in the identification process'
+  [IdentReason.ID_DAMAGED]: 'Document used during identification is a damaged document',
+  [IdentReason.ID_NOT_SUPPORTED]: 'Document used during the identification is not supported for the customers use case',
+  [IdentReason.ID_BLURRY]: 'Document is blurry and mandatory data cannot be read',
+  [IdentReason.ID_GLARE]: 'Document has glare and mandatory data cannot be read',
+  [IdentReason.ID_DARKNESS]:
+    'Pictures of the document are dark and it is not possible to read the mandatory data or verify the authenticity of the document',
+  [IdentReason.ID_DATA_COVERED]: 'Mandatory data is covered while taking the picture',
+  [IdentReason.ID_PERSPECTIVE]:
+    'Document is positioned at such an angle that mandatory data cannot be read or document cannot be verified',
+  [IdentReason.ID_DATA]: 'Mandatory data cannot be read on the document',
+  [IdentReason.ID_DATA_OTHER]: 'Other reason due to which mandatory data cannot be read',
+  [IdentReason.ID_EXPIRED]: 'Document used during the identification is expired',
+  [IdentReason.ID_WRONG_SIDE]: 'Wrong side of the document is scanned during the process',
+  [IdentReason.ID_OUTWORN]: 'Document is worn out. Either data cannot be read out or the document cannot be verified',
+  [IdentReason.ID_HAS_STICKER]:
+    'Document has such stickers which are not acceptable and the document used is considered as damaged document',
+  [IdentReason.ID_WRITTEN_ON]:
+    'Document has text written over it which makes the document not readable or not verifiable',
+  [IdentReason.ID_BROKEN]: 'Document used during the identification is broken',
+  [IdentReason.ID_DAMAGED_OTHER]: 'Other reason for a damaged document',
+  [IdentReason.ID_SECURITY_FEATURE_NOT_VISIBLE_NOT_FRAUD]:
+    'Security features of the document are not visible because you did not move the document correctly',
+  [IdentReason.ID_SECURITY_FEATURE_VIDEO_SHORT]:
+    'Security feature video is too short to detect if there are holograms in the document',
+  [IdentReason.ID_SECURITY_FEATURE_VIDEO_CANNOT_BE_PLAYED]:
+    'Security feature video cannot be played for the agent to review holograms',
+  [IdentReason.ID_SECURITY_FEATURE_OTHER]: 'Other issues with the security feature video',
+  [IdentReason.ID_SECOND_DOCUMENT]: 'Two documents are required for the identification process',
+  [IdentReason.ID_SECOND_DOCUMENT_BAD_PHOTO_QUALITY]:
+    'Photo quality of the additional document in the process is not acceptable',
+  [IdentReason.ID_SECOND_DOCUMENT_DAMAGED]:
+    'Additional document used in the identification process is severely outworn, written or drawn on, ripped or broken',
+  [IdentReason.ID_SECOND_DOCUMENT_EXPIRED]:
+    'Additional document used in the identification process is an expired document',
+  [IdentReason.ID_SECOND_DOCUMENT_OTHER]:
+    'Other issues with the additional document used in the identification process',
+  [IdentReason.ID_NEED_ADDITIONAL_DOCUMENT]:
+    'Additional document like Drivers License is missing in the identification process',
+  [IdentReason.USER_INVOICE_MISSING]: 'Proof of address is needed from you as the additional document',
+  [IdentReason.USER_OBSCURED]:
+    'You covered your face during the face comparison process unintentionally like wearing the face mask',
+  [IdentReason.SELFIE_BLURRY]:
+    'Selfie taken is blurry and cannot be used to compare the face with the identification document',
+  [IdentReason.SELFIE_GLARE]: 'Photo of you on the ID document has glares and selfie cannot be compared with it',
+  [IdentReason.SELFIE_DARKNESS]:
+    'Your selfie is too dark to compare the face of the person with the photo on the identification document',
+  [IdentReason.SELFIE_PERSPECTIVE]:
+    'Your selfie is on such an angle that it is not possible to compare it with the photo on the identification document',
+  [IdentReason.SELFIE_OTHER]: 'Other issues with the selfie',
+  [IdentReason.IDENT_CANNOT_BE_COMPLETED]:
+    'Due to a technical reason, ident specialist cannot finish the identity verification process',
+  [IdentReason.IDENT_DISPLAY_ERROR]:
+    'Due to a technical reason, ident specialist cannot see your data in the identification process',
+  [IdentReason.IDENT_OTHER]:
+    'Other reason due to which the identification process cannot be completed by the ident specialist',
+  [IdentReason.TSP_WRONG_CONFIRMATION_TOKEN]: 'You entered a wrong signing code during the signing step',
+  [IdentReason.TSP_SIGNING_FAILED]: 'The signing process failed due to any technical error',
+  [IdentReason.TSP_CERTIFICATE_EXPIRED]:
+    'Signing certificates are valid for an hour. The final signing step took more than an hour from the time of certificate generation',
+  [IdentReason.ID_SECURITY_FEATURE_VIDEO_FILE_MISSING]:
+    'Security Features (Hologram) video is not saved due to any technical error',
 };
 
 export function getIdentResult(dto: IdentResultDto): IdentShortResult {
