875030de8bd6a3e810fe58646ad637eb8b9c14e3
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-05-13 15:35:59 +0200

    [DEV-2517] add service in module

--- src/subdomains/supporting/support-issue/support-issue.module.ts
+++ src/subdomains/supporting/support-issue/support-issue.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { KycModule } from 'src/subdomains/generic/kyc/kyc.module';
 import { UserModule } from 'src/subdomains/generic/user/user.module';
 import { SharedModule } from '../../../shared/shared.module';
+import { NotificationModule } from '../notification/notification.module';
 import { TransactionModule } from '../payment/transaction.module';
 import { SupportIssue } from './entities/support-issue.entity';
 import { SupportMessage } from './entities/support-message.entity';
 import { SupportIssueRepository } from './repositories/support-issue.repository';
 import { SupportMessageRepository } from './repositories/support-message.repository';
+import { SupportIssueNotificationService } from './services/support-issue-notification.service';
 import { SupportIssueService } from './services/support-issue.service';
 import { SupportIssueController } from './support-issue.controller';
 
@@ -18,9 +20,10 @@ import { SupportIssueController } from './support-issue.controller';
     TransactionModule,
     KycModule,
     UserModule,
+    NotificationModule,
   ],
   controllers: [SupportIssueController],
-  providers: [SupportIssueRepository, SupportIssueService, SupportMessageRepository],
+  providers: [SupportIssueRepository, SupportIssueService, SupportMessageRepository, SupportIssueNotificationService],
   exports: [SupportIssueService],
 })
 export class SupportIssueModule {}
