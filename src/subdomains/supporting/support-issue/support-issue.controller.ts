b68242f7a5be1f290fb4af0562fc03ae17677023
David May <david.leo.may@gmail.com>
2024-05-17 12:26:08 +0200

    [DEV-2449] After merge fixes

--- src/subdomains/supporting/support-issue/support-issue.controller.ts
+++ src/subdomains/supporting/support-issue/support-issue.controller.ts
@@ -30,7 +30,7 @@ export class SupportIssueController {
 
   @Post(':id/message')
   @ApiBearerAuth()
-  @UseGuards(AuthGuard(), new RoleGuard(UserRole.USER))
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ACCOUNT))
   async createSupportMessage(
     @GetJwt() jwt: JwtPayload,
     @Param('id') id: string,
@@ -39,7 +39,7 @@ export class SupportIssueController {
     return this.supportIssueService.createSupportMessage(
       +id,
       [UserRole.SUPPORT, UserRole.ADMIN].includes(jwt.role) ? dto : { ...dto, author: CustomerAuthor },
-      jwt.id,
+      jwt.account,
     );
   }
 
