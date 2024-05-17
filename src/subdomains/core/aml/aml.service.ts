73cf72d66a0fcfa455ee73d4afd6c977d0ca4c03
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-05-17 13:17:18 +0200

    [DEV-2565] auto merge only with iban (#1296)
    * [DEV-2565] auto merge only with iban
    
    * [DEV-2565] Refactoring
    
    * [DEV-2566] change auto merge
    
    * [DEV-2566] Refactoring 2

--- src/subdomains/core/aml/aml.service.ts
+++ src/subdomains/core/aml/aml.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import * as IbanTools from 'ibantools';
 import { DfxLogger } from 'src/shared/services/dfx-logger';
+import { Util } from 'src/shared/utils/util';
 import { NameCheckService } from 'src/subdomains/generic/kyc/services/name-check.service';
 import { BankData } from 'src/subdomains/generic/user/models/bank-data/bank-data.entity';
 import { BankDataService } from 'src/subdomains/generic/user/models/bank-data/bank-data.service';
@@ -31,7 +33,15 @@ export class AmlService {
 
     if (bankData) {
       if (!entity.userData.hasValidNameCheckDate) await this.checkNameCheck(entity, bankData);
-      if (bankData.active && bankData.userData.id !== entity.userData.id) {
+
+      if (
+        bankData.active &&
+        bankData.userData.id !== entity.userData.id &&
+        entity instanceof BuyCrypto &&
+        (Util.isSameName(entity.bankTx.name, entity.userData.verifiedName) ||
+          Util.isSameName(entity.bankTx.ultimateName, entity.userData.verifiedName)) &&
+        IbanTools.validateIBAN(bankData.iban.split(';')[0]).valid
+      ) {
         try {
           const [master, slave] =
             bankData.userData.kycLevel < entity.userData.kycLevel
