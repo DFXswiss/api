2bf6fa8112eba616ccee41984a2bfbbe78753931
David May <david.leo.may@gmail.com>
2024-05-22 15:39:42 +0200

    [DEV-2560] Implemented LM exchange transfers

--- src/subdomains/core/liquidity-management/adapters/actions/binance.adapter.ts
+++ src/subdomains/core/liquidity-management/adapters/actions/binance.adapter.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Blockchain } from 'src/integration/blockchain/shared/enums/blockchain.enum';
 import { BinanceService } from 'src/integration/exchange/services/binance.service';
+import { ExchangeRegistryService } from 'src/integration/exchange/services/exchange-registry.service';
 import { DexService } from 'src/subdomains/supporting/dex/services/dex.service';
 import { LiquidityManagementSystem } from '../../enums';
 import { LiquidityManagementOrderRepository } from '../../repositories/liquidity-management-order.repository';
@@ -27,10 +28,11 @@ export class BinanceAdapter extends CcxtExchangeAdapter {
 
   constructor(
     binanceService: BinanceService,
+    exchangeRegistry: ExchangeRegistryService,
     dexService: DexService,
     liquidityOrderRepo: LiquidityManagementOrderRepository,
   ) {
-    super(LiquidityManagementSystem.BINANCE, binanceService, dexService, liquidityOrderRepo);
+    super(LiquidityManagementSystem.BINANCE, binanceService, exchangeRegistry, dexService, liquidityOrderRepo);
   }
 
   protected mapBlockchainToCcxtNetwork(blockchain: Blockchain): string {
