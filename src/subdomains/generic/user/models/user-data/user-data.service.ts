4da432b331629e13ec4afb28c53bddecbc92174b
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-06-03 15:25:28 +0200

    [DEV-2603] remove unused ?

--- src/subdomains/generic/user/models/user-data/user-data.service.ts
+++ src/subdomains/generic/user/models/user-data/user-data.service.ts
@@ -544,7 +544,7 @@ export class UserDataService {
     master.accountRelations = master.accountRelations.concat(slave.accountRelations);
     master.relatedAccountRelations = master.relatedAccountRelations.concat(slave.relatedAccountRelations);
     slave.individualFeeList?.forEach((fee) => !master.individualFeeList?.includes(fee) && master.addFee(fee));
-    slave.kycClientList?.forEach((kc) => !master.kycClientList?.includes(kc) && master.addKycClient(kc));
+    slave.kycClientList.forEach((kc) => !master.kycClientList.includes(kc) && master.addKycClient(kc));
 
     if (master.status === UserDataStatus.KYC_ONLY) master.status = slave.status;
     if (!master.amlListAddedDate && slave.amlListAddedDate) {
