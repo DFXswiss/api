0348e75a466e17a95087a66abd43bd353dad6d09
Kolibri <daniel@dfx.swiss>
2024-06-13 22:53:30 +0200

    [DEV-2518] remove db names

--- migration/1718311906077-addSiftResponse.js
+++ migration/1718311906077-addSiftResponse.js
@@ -4,14 +4,14 @@ module.exports = class addSiftResponse1718311906077 {
     name = 'addSiftResponse1718311906077'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."checkout_tx" ADD "authStatusReason" nvarchar(255)`);
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."transaction_request" ADD "siftResponse" nvarchar(MAX)`);
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."buy_crypto" ADD "siftResponse" nvarchar(MAX)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."checkout_tx" ADD "authStatusReason" nvarchar(255)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" ADD "siftResponse" nvarchar(MAX)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD "siftResponse" nvarchar(MAX)`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."buy_crypto" DROP COLUMN "siftResponse"`);
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."transaction_request" DROP COLUMN "siftResponse"`);
-        await queryRunner.query(`ALTER TABLE "sqldb-dfx-api-dev"."dbo"."checkout_tx" DROP COLUMN "authStatusReason"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP COLUMN "siftResponse"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" DROP COLUMN "siftResponse"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."checkout_tx" DROP COLUMN "authStatusReason"`);
     }
 }
