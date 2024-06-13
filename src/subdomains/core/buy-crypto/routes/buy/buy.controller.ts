ca8cbb23d8a388d9bee9caa9aff368852d4b361d
lapatric <42653152+lapatric@users.noreply.github.com>
2024-06-13 18:52:30 +0200

    move reference data to additionalInformation

--- src/subdomains/core/buy-crypto/routes/buy/buy.controller.ts
+++ src/subdomains/core/buy-crypto/routes/buy/buy.controller.ts
@@ -374,7 +374,7 @@ ${buy.bankUsage}
     return {
       amount,
       currency,
-      reference,
+      additionalInformation: reference,
       creditor: {
         account: bankInfo.iban,
         address: bankInfo.street,
