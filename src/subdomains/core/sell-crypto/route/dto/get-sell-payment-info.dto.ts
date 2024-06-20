9c4c4d67809a13bf5f419d442e8020195f4692de
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-06-20 13:34:45 +0200

    [DEV-2695] fix sell paymentInfo bug

--- src/subdomains/core/sell-crypto/route/dto/get-sell-payment-info.dto.ts
+++ src/subdomains/core/sell-crypto/route/dto/get-sell-payment-info.dto.ts
@@ -7,12 +7,15 @@ import {
   IsNumber,
   IsOptional,
   IsString,
+  Validate,
+  ValidateIf,
   ValidateNested,
 } from 'class-validator';
 import { EntityDto } from 'src/shared/dto/entity.dto';
 import { Asset } from 'src/shared/models/asset/asset.entity';
 import { Fiat } from 'src/shared/models/fiat/fiat.entity';
 import { Util } from 'src/shared/utils/util';
+import { XOR } from 'src/shared/validators/xor.validator';
 import { IbanType, IsDfxIban } from 'src/subdomains/supporting/bank/bank-account/is-dfx-iban.validator';
 
 export class GetSellPaymentInfoDto {
@@ -35,20 +38,18 @@ export class GetSellPaymentInfoDto {
   @Type(() => EntityDto)
   currency: Fiat;
 
-  // TODO: add Validation Checks if App is updated
   //eslint-disable-next-line @typescript-eslint/no-inferrable-types
   @ApiPropertyOptional({ description: 'Amount in source asset' })
   @IsNotEmpty()
-  //@ValidateIf((b: GetSellPaymentInfoDto) => Boolean(b.amount || !b.targetAmount))
-  //@Validate(XOR, ['targetAmount'])
+  @ValidateIf((b: GetSellPaymentInfoDto) => Boolean(b.amount || !b.targetAmount))
+  @Validate(XOR, ['targetAmount'])
   @IsNumber()
-  amount: number = 0;
+  amount: number;
 
-  // TODO: add Validation Checks if App is updated
   @ApiPropertyOptional({ description: 'Amount in target currency' })
   @IsOptional()
-  //@ValidateIf((b: GetSellPaymentInfoDto) => Boolean(b.targetAmount || !b.amount))
-  //@Validate(XOR, ['amount'])
+  @ValidateIf((b: GetSellPaymentInfoDto) => Boolean(b.targetAmount || !b.amount))
+  @Validate(XOR, ['amount'])
   @IsNumber()
   targetAmount: number;
 
