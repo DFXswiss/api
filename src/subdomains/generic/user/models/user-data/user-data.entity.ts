0dc4a1511427d482a51e2b12db96252ba25da9bc
David May <85513542+davidleomay@users.noreply.github.com>
2024-06-20 08:49:47 +0200

    DB improvements (#1386)
    * [NO-TASK] Improved pay-in registration
    
    * [NO-TASK] Added index on liquidity order
    
    * [NO-TASK] KYC steps not eager

--- src/subdomains/generic/user/models/user-data/user-data.entity.ts
+++ src/subdomains/generic/user/models/user-data/user-data.entity.ts
@@ -210,7 +210,7 @@ export class UserData extends IEntity {
   @Column({ length: 256, nullable: true })
   kycType: KycType;
 
-  @OneToMany(() => KycStep, (step) => step.userData, { eager: true, cascade: true })
+  @OneToMany(() => KycStep, (step) => step.userData, { cascade: true })
   kycSteps: KycStep[];
 
   @Column({ type: 'float', nullable: true })
