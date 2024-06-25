fef19d5359c31485ed1b0a003a2213d78d44c3a1
David May <david.leo.may@gmail.com>
2024-06-25 16:10:46 +0200

    [DEV-2682] Fixed migration

--- migration/1719239340197-SetTransactionNullableFalse.js
+++ migration/1719239340197-SetTransactionNullableFalse.js
@@ -5,19 +5,29 @@ module.exports = class SetTransactionNullableFalse1719239340197 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" DROP CONSTRAINT "FK_f1be1be83e5cd35bb34b5d4c58c"`);
+        await queryRunner.query(`DROP INDEX "REL_f1be1be83e5cd35bb34b5d4c58" ON "dbo"."buy_fiat"`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ALTER COLUMN "transactionId" int NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP CONSTRAINT "FK_27bc248ef08405dbdce47daccf6"`);
-        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ALTER COLUMN "transactionId" int NOT NULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "REL_f1be1be83e5cd35bb34b5d4c58" ON "dbo"."buy_fiat" ("transactionId") WHERE "transactionId" IS NOT NULL`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ADD CONSTRAINT "FK_f1be1be83e5cd35bb34b5d4c58c" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP CONSTRAINT "FK_27bc248ef08405dbdce47daccf6"`);
+        await queryRunner.query(`DROP INDEX "REL_27bc248ef08405dbdce47daccf" ON "dbo"."buy_crypto"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ALTER COLUMN "transactionId" int NOT NULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "REL_27bc248ef08405dbdce47daccf" ON "dbo"."buy_crypto" ("transactionId") WHERE "transactionId" IS NOT NULL`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD CONSTRAINT "FK_27bc248ef08405dbdce47daccf6" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP CONSTRAINT "FK_27bc248ef08405dbdce47daccf6"`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" DROP CONSTRAINT "FK_f1be1be83e5cd35bb34b5d4c58c"`);
-        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ALTER COLUMN "transactionId" int`);
-        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD CONSTRAINT "FK_27bc248ef08405dbdce47daccf6" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`DROP INDEX "REL_f1be1be83e5cd35bb34b5d4c58" ON "dbo"."buy_fiat"`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ALTER COLUMN "transactionId" int`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "REL_f1be1be83e5cd35bb34b5d4c58" ON "dbo"."buy_fiat" ("transactionId") WHERE "transactionId" IS NOT NULL`);
         await queryRunner.query(`ALTER TABLE "dbo"."buy_fiat" ADD CONSTRAINT "FK_f1be1be83e5cd35bb34b5d4c58c" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" DROP CONSTRAINT "FK_27bc248ef08405dbdce47daccf6"`);
+        await queryRunner.query(`DROP INDEX "REL_27bc248ef08405dbdce47daccf" ON "dbo"."buy_crypto"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ALTER COLUMN "transactionId" int`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "REL_27bc248ef08405dbdce47daccf" ON "dbo"."buy_crypto" ("transactionId") WHERE "transactionId" IS NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "dbo"."buy_crypto" ADD CONSTRAINT "FK_27bc248ef08405dbdce47daccf6" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 }
