18993e606f2a4f0672f57cac03689d6204dd2888
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-07-08 23:07:39 +0200

    [DEV-2771] nameCheck improvement (#1430)

--- src/subdomains/generic/kyc/services/name-check.service.ts
+++ src/subdomains/generic/kyc/services/name-check.service.ts
@@ -4,7 +4,7 @@ import { BankData } from '../../user/models/bank-data/bank-data.entity';
 import { UserData } from '../../user/models/user-data/user-data.entity';
 import { UserDataService } from '../../user/models/user-data/user-data.service';
 import { UpdateNameCheckLogDto } from '../dto/input/update-name-check-log.dto';
-import { NameCheckLog, RiskStatus } from '../entities/name-check-log.entity';
+import { NameCheckLog, RiskEvaluation, RiskStatus } from '../entities/name-check-log.entity';
 import { KycLogType } from '../enums/kyc.enum';
 import { NameCheckLogRepository } from '../repositories/name-check-log.repository';
 import { DilisenseService } from './integration/dilisense.service';
@@ -62,6 +62,23 @@ export class NameCheckService implements OnModuleInit {
     }
 
     return RiskStatus.SANCTIONED;
+  }
+
+  async closeAndRefreshRiskStatus(bankData: BankData): Promise<void> {
+    const openNameChecks = await this.nameCheckLogRepo.find({
+      where: { userData: { id: bankData.userData.id }, riskEvaluation: IsNull(), riskStatus: RiskStatus.SANCTIONED },
+      relations: { userData: true },
+    });
+
+    for (const nameCheck of openNameChecks) {
+      await this.nameCheckLogRepo.update(nameCheck.id, {
+        riskEvaluation: RiskEvaluation.CANCELED,
+        riskEvaluationDate: new Date(),
+        comment: 'Ident refresh',
+      });
+    }
+
+    await this.refreshRiskStatus(bankData);
   }
 
   async hasOpenNameChecks(userData: UserData): Promise<boolean> {
