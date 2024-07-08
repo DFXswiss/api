e8a60d9ad36cfa176ece7d23624c26bd58c307c9
Kolibri <daniel@dfx.swiss>
2024-07-09 01:06:34 +0200

    [DEV-2785] Update payment info module

--- src/subdomains/core/payment-llink/payment-link.module.ts
+++ src/subdomains/core/payment-llink/payment-link.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentLinkPayment } from './entities/payment-link-payment.entity';
+import { PaymentLink } from './entities/payment-link.entity';
+import { PaymentLinkController } from './payment-link.controller';
+import { PaymentLinkPaymentService } from './services/payment-link-payment.services';
+import { PaymentLinkService } from './services/payment-link.services';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([PaymentLink, PaymentLinkPayment])],
+  controllers: [PaymentLinkController],
+  providers: [PaymentLinkService, PaymentLinkPaymentService],
   exports: [],
 })
 export class PaymentLinkModule {}
