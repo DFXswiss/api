85bb91b7ca6b25e26d26ca49415be315d45f07af
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-07-15 16:37:33 +0200

    [DEV-2797] Refactoring 5

--- src/subdomains/generic/kyc/services/kyc-admin.service.ts
+++ src/subdomains/generic/kyc/services/kyc-admin.service.ts
@@ -50,7 +50,7 @@ export class KycAdminService {
 
   async resetKyc(userData: UserData): Promise<void> {
     for (const kycStep of userData.kycSteps) {
-      if ([KycStepName.FINANCIAL_DATA, KycStepName.IDENT].includes(kycStep.name) && kycStep.isDone)
+      if ([KycStepName.FINANCIAL_DATA, KycStepName.IDENT].includes(kycStep.name) && !kycStep.isFailed)
         await this.kycStepRepo.update(kycStep.id, { status: KycStepStatus.CANCELED });
     }
   }
