470aab9bb5086498183a78a4fc5bd0edbf9cd035
lapatric <42653152+lapatric@users.noreply.github.com>
2024-07-23 14:05:01 +0200

    add "dbo"

--- migration/1721730420883-AddressLabel.js
+++ migration/1721730420883-AddressLabel.js
@@ -4,12 +4,12 @@ module.exports = class AddressLabel1721730420883 {
     name = 'AddressLabel1721730420883'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "wallet" ADD "displayName" nvarchar(256)`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "label" nvarchar(256)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."wallet" ADD "displayName" nvarchar(256)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."user" ADD "label" nvarchar(256)`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "label"`);
-        await queryRunner.query(`ALTER TABLE "wallet" DROP COLUMN "displayName"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."user" DROP COLUMN "label"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."wallet" DROP COLUMN "displayName"`);
     }
 }
