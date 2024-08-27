bcec67ab3be43269a29b09d223e94aa12b449c88
David May <david.leo.may@gmail.com>
2024-08-27 10:08:21 +0200

    [DEV-3032] Implemented lightning forwarding

--- src/subdomains/supporting/payout/services/payout-lightning.service.ts
+++ src/subdomains/supporting/payout/services/payout-lightning.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { LndPaymentDto, LndPaymentStatus, LndSendPaymentResponseDto } from 'src/integration/lightning/dto/lnd.dto';
 import { LightningClient } from 'src/integration/lightning/lightning-client';
-import { LightningAddressType, LightningHelper } from 'src/integration/lightning/lightning-helper';
+import { LightningHelper } from 'src/integration/lightning/lightning-helper';
 import { LightningService } from 'src/integration/lightning/services/lightning.service';
-import { Util } from 'src/shared/utils/util';
 
 @Injectable()
 export class PayoutLightningService {
-  private static MAX_HOURS = 24;
-
   private readonly client: LightningClient;
 
   constructor(private lightningService: LightningService) {
@@ -16,13 +12,7 @@ export class PayoutLightningService {
   }
 
   async isHealthy(): Promise<boolean> {
-    try {
-      const info = await this.client.getLndInfo();
-
-      return 0 < info.num_active_channels && info.synced_to_chain;
-    } catch {
-      return false;
-    }
+    return this.client.isHealthy();
   }
 
   async getEstimatedFee(address: string, amount: number): Promise<number> {
@@ -36,68 +26,10 @@ export class PayoutLightningService {
   }
 
   async sendPayment(address: string, amount: number): Promise<string> {
-    const addressType = LightningHelper.getAddressType(address);
-
-    let paymentResponse: LndSendPaymentResponseDto;
-
-    switch (addressType) {
-      case LightningAddressType.LN_URL: {
-        const invoice = await this.lightningService.getInvoiceByLnurlp(address, amount);
-        paymentResponse = await this.client.sendPaymentByInvoice(invoice);
-        break;
-      }
-
-      case LightningAddressType.LN_NID: {
-        const publicKey = await this.lightningService.getPublicKeyOfAddress(address);
-        paymentResponse = await this.client.sendPaymentByPublicKey(publicKey, amount);
-        break;
-      }
-
-      case LightningAddressType.LND_HUB: {
-        const invoice = await this.lightningService.getInvoiceByLndhub(address, amount);
-        paymentResponse = await this.client.sendPaymentByInvoice(invoice);
-        break;
-      }
-
-      default:
-        throw new Error(`Unknown address type ${addressType} in send payment`);
-    }
-
-    if (paymentResponse.payment_error) {
-      throw new Error(`Error while sending payment by LNURL ${address}: ${paymentResponse.payment_error}`);
-    }
-
-    return Buffer.from(paymentResponse.payment_hash, 'base64').toString('hex');
+    return this.lightningService.sendTransfer(address, amount);
   }
 
   async getPayoutCompletionData(payoutTxId: string): Promise<[boolean, number]> {
-    const foundPayment = await this.findPayment(new Date(), payoutTxId);
-
-    let isComplete = false;
-    let payoutFee = 0;
-
-    if (foundPayment) {
-      isComplete = foundPayment.status === LndPaymentStatus.SUCCEEDED;
-      payoutFee = LightningHelper.satToBtc(foundPayment.fee_sat);
-    }
-
-    return [isComplete, payoutFee];
-  }
-
-  private async findPayment(toDate: Date, payoutTxId: string): Promise<LndPaymentDto> {
-    let loopToDate = toDate;
-    let loopFromDate = Util.hoursBefore(1, loopToDate);
-
-    for (let hour = 0; hour < PayoutLightningService.MAX_HOURS; hour++) {
-      const payments = await this.client.listPayments(loopFromDate, loopToDate);
-      const foundPayment = payments.find((p) => p.payment_hash === payoutTxId);
-
-      if (foundPayment) return foundPayment;
-
-      loopToDate = Util.hoursBefore(1, loopToDate);
-      loopFromDate = Util.hoursBefore(1, loopToDate);
-    }
-
-    return null;
+    return this.lightningService.getTransferCompletionData(payoutTxId);
   }
 }
