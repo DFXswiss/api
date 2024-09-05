15cb2e26fb268926ad1082972eb24583b49b3e59
David May <david.leo.may@gmail.com>
2024-09-05 14:47:49 +0200

    [NO-TASK] Renaming

--- src/subdomains/supporting/support-issue/services/support-document.service.ts
+++ src/subdomains/supporting/support-issue/services/support-document.service.ts
@@ -4,7 +4,7 @@ import { ContentType } from 'src/subdomains/generic/kyc/dto/kyc-file.dto';
 
 export interface SupportFile extends Blob {
   userDataId: number;
-  ticketId: number;
+  issueId: number;
   contentType: ContentType;
 }
 
@@ -19,10 +19,10 @@ export class SupportDocumentService {
   async listFilesByPrefix(prefix: string): Promise<SupportFile[]> {
     const blobs = await this.storageService.listBlobs(prefix);
     return blobs.map((b) => {
-      const [userDataId, ticketId, name] = this.fromFileId(b.name);
+      const [userDataId, issueId, name] = this.fromFileId(b.name);
       return {
         userDataId,
-        ticketId,
+        issueId,
         name,
         url: b.url,
         contentType: b.contentType as ContentType,
@@ -35,26 +35,26 @@ export class SupportDocumentService {
 
   async uploadFile(
     userDataId: number,
-    ticketId: number,
+    issueId: number,
     name: string,
     data: Buffer,
     contentType: ContentType,
     metadata?: Record<string, string>,
   ): Promise<string> {
-    return this.storageService.uploadBlob(this.toFileId(userDataId, ticketId, name), data, contentType, metadata);
+    return this.storageService.uploadBlob(this.toFileId(userDataId, issueId, name), data, contentType, metadata);
   }
 
-  async downloadFile(userDataId: number, ticketId: number, name: string): Promise<BlobContent> {
-    return this.storageService.getBlob(this.toFileId(userDataId, ticketId, name));
+  async downloadFile(userDataId: number, issueId: number, name: string): Promise<BlobContent> {
+    return this.storageService.getBlob(this.toFileId(userDataId, issueId, name));
   }
 
   // --- HELPER METHODS --- //
-  private toFileId(userDataId: number, ticketId: number, name: string): string {
-    return `user/${userDataId}/issues/${ticketId}/${name}`;
+  private toFileId(userDataId: number, issueId: number, name: string): string {
+    return `user/${userDataId}/issues/${issueId}/${name}`;
   }
 
   private fromFileId(fileId: string): [number, number, string] {
-    const [_u, userDataId, _i, ticketId, name] = fileId.split('/');
-    return [+userDataId, +ticketId, name];
+    const [_u, userDataId, _i, issueId, name] = fileId.split('/');
+    return [+userDataId, +issueId, name];
   }
 }
