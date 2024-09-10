78041908cb5451bbf66cf2053c13bfac2be9d8e1
David May <david.leo.may@gmail.com>
2024-09-10 22:09:15 +0200

    [DEV-3104] Refactoring

--- src/subdomains/core/sell-crypto/process/services/buy-fiat-preparation.service.ts
+++ src/subdomains/core/sell-crypto/process/services/buy-fiat-preparation.service.ts
@@ -68,7 +68,7 @@ export class BuyFiatPreparationService {
 
         const inputReferenceCurrency = entity.cryptoInput.asset;
 
-        const isPayment = Boolean(entity.cryptoInput?.isPayment);
+        const isPayment = entity.cryptoInput.isPayment;
         const minVolume = await this.transactionHelper.getMinVolumeIn(
           entity.cryptoInput.asset,
           entity.cryptoInput.asset,
