34db5f22b234818a35b719712339c37b110e052f
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-10-23 11:01:08 +0200

    [DEV-3063] Refactoring 2

--- src/subdomains/supporting/bank/bank-account/dto/create-bank-account.dto.ts
+++ src/subdomains/supporting/bank/bank-account/dto/create-bank-account.dto.ts
@@ -1,29 +1,17 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Transform, Type } from 'class-transformer';
-import { IsBoolean, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
-import { EntityDto } from 'src/shared/dto/entity.dto';
-import { Fiat } from 'src/shared/models/fiat/fiat.entity';
+import { Transform } from 'class-transformer';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Util } from 'src/shared/utils/util';
+import { UpdateBankAccountDto } from 'src/subdomains/generic/user/models/bank-data/dto/update-bank-data.dto';
 import { IbanType, IsDfxIban } from '../is-dfx-iban.validator';
 
-export class CreateBankAccountDto {
+export class CreateBankAccountDto extends UpdateBankAccountDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @IsDfxIban(IbanType.BOTH)
   @Transform(Util.trimAll)
   iban: string;
-
-  @ApiPropertyOptional()
-  @IsOptional()
-  @IsString()
-  label: string;
-
-  @ApiPropertyOptional({ type: EntityDto })
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => EntityDto)
-  preferredCurrency: Fiat;
 
   @ApiPropertyOptional()
   @IsOptional()
