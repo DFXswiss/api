2bf860106f8f3094de54eb9424facdc5cf2fbf92
David May <david.leo.may@gmail.com>
2024-10-23 15:02:32 +0200

    [DEV-3063] Refactoring	1

---
+++ src/subdomains/supporting/bank/bank-account/dto/update-bank-account.dto.ts
@@ -0,0 +1,18 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsOptional, IsString, ValidateNested } from 'class-validator';
+import { EntityDto } from 'src/shared/dto/entity.dto';
+import { Fiat } from 'src/shared/models/fiat/fiat.entity';
+
+export class UpdateBankAccountDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  label?: string;
+
+  @ApiPropertyOptional({ type: EntityDto })
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => EntityDto)
+  preferredCurrency?: Fiat;
+}
