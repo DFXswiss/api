93775800b8f1476a6a6ce2faf242f6b555b27211
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-11-29 12:04:27 +0100

    [DEV-3154] fix code

--- src/subdomains/generic/user/models/user-data/user-data.entity.ts
+++ src/subdomains/generic/user/models/user-data/user-data.entity.ts
@@ -12,14 +12,13 @@ import { KycStep } from 'src/subdomains/generic/kyc/entities/kyc-step.entity';
 import { KycStepName, KycStepType } from 'src/subdomains/generic/kyc/enums/kyc.enum';
 import { BankData } from 'src/subdomains/generic/user/models/bank-data/bank-data.entity';
 import { User, UserStatus } from 'src/subdomains/generic/user/models/user/user.entity';
+import { BankTxReturn } from 'src/subdomains/supporting/bank-tx/bank-tx-return/bank-tx-return.entity';
 import { BankAccount } from 'src/subdomains/supporting/bank/bank-account/bank-account.entity';
 import { SupportIssue } from 'src/subdomains/supporting/support-issue/entities/support-issue.entity';
 import { Column, Entity, Generated, Index, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { UserDataRelation } from '../user-data-relation/user-data-relation.entity';
 import { TradingLimit } from '../user/dto/user.dto';
 import { AccountType } from './account-type.enum';
-import { BankTxReturn } from 'src/subdomains/supporting/bank-tx/bank-tx-return/bank-tx-return.entity';
-import { SupportIssue } from 'src/subdomains/supporting/support-issue/entities/support-issue.entity';
 
 export enum KycStatus {
   NA = 'NA',
