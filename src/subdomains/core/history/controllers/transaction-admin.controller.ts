374d6138c1fdbd4bdc552de05f20cbe1a9d0eabe
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-11-30 22:41:22 +0100

    [DEV-3397] transaction cols

---
+++ src/subdomains/core/history/controllers/transaction-admin.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiExcludeController, ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
+import { RoleGuard } from 'src/shared/auth/role.guard';
+import { UserRole } from 'src/shared/auth/user-role.enum';
+import { Transaction } from 'src/subdomains/supporting/payment/entities/transaction.entity';
+import { TransactionService } from 'src/subdomains/supporting/payment/services/transaction.service';
+import { UpdateTransactionDto } from '../dto/update-transaction.dto';
+
+@ApiTags('TransactionAdmin')
+@Controller('transactionAdmin')
+@ApiExcludeController()
+export class TransactionAdminController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Put(':id')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), new RoleGuard(UserRole.ADMIN))
+  @ApiExcludeEndpoint()
+  async updateTransaction(@Param('id') id: string, @Body() dto: UpdateTransactionDto): Promise<Transaction> {
+    return this.transactionService.update(+id, dto);
+  }
+}
