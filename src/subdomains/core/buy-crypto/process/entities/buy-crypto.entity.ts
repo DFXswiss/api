9261b501d633a2cc6042f5fbc6ed86fe98c64372
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2024-12-11 15:45:27 +0100

    [NOTASK] fix buyCrypto status reset

--- src/subdomains/core/buy-crypto/process/entities/buy-crypto.entity.ts
+++ src/subdomains/core/buy-crypto/process/entities/buy-crypto.entity.ts
@@ -459,7 +459,7 @@ export class BuyCrypto extends IEntity {
             refFactor: !fee.payoutRefBonus || usedRef === '000-000' ? 0 : 1,
             usedFees: fee.fees?.map((fee) => fee.id).join(';'),
             networkStartFeeAmount: fee.networkStart,
-            status: this.status === BuyCryptoStatus.WAITING_FOR_LOWER_FEE ? null : undefined,
+            status: this.status === BuyCryptoStatus.WAITING_FOR_LOWER_FEE ? BuyCryptoStatus.CREATED : undefined,
           };
 
     Object.assign(this, update);
