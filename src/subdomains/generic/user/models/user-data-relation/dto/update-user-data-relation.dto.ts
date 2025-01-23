2cd043af14e47e03b0f92bded042beccc3f9a10b
Yannick <52333989+Yannick1712@users.noreply.github.com>
2025-01-23 16:13:30 +0100

    [DEV-3567] userDataRelation optional signatory (#1913)
    * [DEV-3567] userDataRelation optional signatory
    
    * [DEV-3567] add migration
    
    * [DEV-3567] remove ApiExcludeEndpoint

---
+++ src/subdomains/generic/user/models/user-data-relation/dto/update-user-data-relation.dto.ts
@@ -0,0 +1,8 @@
+import { IsEnum, IsOptional } from 'class-validator';
+import { SignatoryState } from './user-data-relation.enum';
+
+export class UpdateUserDataRelationDto {
+  @IsOptional()
+  @IsEnum(SignatoryState)
+  signatory: SignatoryState;
+}
