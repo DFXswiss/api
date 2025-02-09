a13cb9f870f87d2f687d8c4333e67ed3a4cb37b5
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2025-02-09 20:18:40 +0100

    [DEV-3524] get exchange observer with volume 30 days

--- src/subdomains/core/monitoring/observers/exchange.observer.ts
+++ src/subdomains/core/monitoring/observers/exchange.observer.ts
@@ -6,12 +6,14 @@ import { RepositoryFactory } from 'src/shared/repositories/repository.factory';
 import { DfxLogger } from 'src/shared/services/dfx-logger';
 import { DisabledProcess, Process } from 'src/shared/services/process.service';
 import { Lock } from 'src/shared/utils/lock';
+import { Util } from 'src/shared/utils/util';
 import { MetricObserver } from 'src/subdomains/core/monitoring/metric.observer';
 import { MonitoringService } from 'src/subdomains/core/monitoring/monitoring.service';
+import { MoreThan } from 'typeorm';
 
 interface ExchangeData {
   name: string;
-  volume30: number;
+  volume30: number; //volume of the last 30 days
 }
 
 @Injectable()
@@ -29,6 +31,7 @@ export class ExchangeObserver extends MetricObserver<ExchangeData[]> {
 
     const data = [];
     data.concat(await this.getKraken());
+    data.concat(await this.getBinance());
     this.emit(data);
 
     return data;
@@ -38,14 +41,25 @@ export class ExchangeObserver extends MetricObserver<ExchangeData[]> {
 
   private async getKraken(): Promise<ExchangeData> {
     const volume30 = await this.getVolume30(ExchangeName.KRAKEN);
-
     return {
       name: 'Kraken',
       volume30,
     };
   }
 
+  private async getBinance(): Promise<ExchangeData> {
+    const volume30 = await this.getVolume30(ExchangeName.BINANCE);
+    return {
+      name: 'Binance',
+      volume30,
+    };
+  }
+
   private async getVolume30(exchange: ExchangeName): Promise<number> {
-    return this.repos.exchangeTx.sum('vol', { exchange, type: ExchangeTxType.TRADE });
+    return this.repos.exchangeTx.sum('vol', {
+      exchange,
+      type: ExchangeTxType.TRADE,
+      created: MoreThan(Util.daysBefore(30)),
+    });
   }
 }
