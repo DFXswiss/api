0638041a324260814e17e7013aab5f675e300173
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2025-02-09 19:29:18 +0100

    [DEV-3601] Add syncDate to checkout observer

--- src/subdomains/core/monitoring/observers/checkout.observer.ts
+++ src/subdomains/core/monitoring/observers/checkout.observer.ts
@@ -6,19 +6,25 @@ import { DisabledProcess, Process } from 'src/shared/services/process.service';
 import { Lock } from 'src/shared/utils/lock';
 import { MetricObserver } from 'src/subdomains/core/monitoring/metric.observer';
 import { MonitoringService } from 'src/subdomains/core/monitoring/monitoring.service';
+import { CheckoutTxService } from 'src/subdomains/supporting/fiat-payin/services/checkout-tx.service';
 
 interface CheckoutData {
   name: string;
   currency: string;
   balance: CheckoutBalances;
   description: string;
+  syncDate: Date;
 }
 
 @Injectable()
 export class CheckoutObserver extends MetricObserver<CheckoutData[]> {
   protected readonly logger = new DfxLogger(CheckoutObserver);
 
-  constructor(monitoringService: MonitoringService, private readonly checkoutService: CheckoutService) {
+  constructor(
+    monitoringService: MonitoringService,
+    private readonly checkoutService: CheckoutService,
+    private readonly checkoutTxService: CheckoutTxService,
+  ) {
     super(monitoringService, 'checkout', 'balance');
   }
 
@@ -38,12 +44,14 @@ export class CheckoutObserver extends MetricObserver<CheckoutData[]> {
 
   private async getCheckout(): Promise<CheckoutData[]> {
     const balances = await this.checkoutService.getBalances();
+    const syncDate = await this.checkoutTxService.getSyncDate();
 
     return balances.map((b) => ({
       name: 'Checkout',
       currency: b.holding_currency,
       balance: b.balances,
       description: b.descriptor,
+      syncDate,
     }));
   }
 }
