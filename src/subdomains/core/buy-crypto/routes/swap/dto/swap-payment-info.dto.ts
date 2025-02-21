052f7c1e7f3f21f92fdc14cdc58f157c84e86f4e
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-02-21 09:55:56 +0100

    [DEV-3663] Change dto descriptions

--- src/subdomains/core/buy-crypto/routes/swap/dto/swap-payment-info.dto.ts
+++ src/subdomains/core/buy-crypto/routes/swap/dto/swap-payment-info.dto.ts
@@ -7,10 +7,10 @@ import { QuoteError } from 'src/subdomains/supporting/payment/dto/transaction-he
 import { PriceStep } from 'src/subdomains/supporting/pricing/domain/entities/price';
 
 export class SwapPaymentInfoDto {
-  @ApiProperty({ description: 'Transaction request ID' })
+  @ApiProperty({ description: 'Transaction order ID' })
   id: number;
 
-  @ApiProperty({ description: 'UID of the transaction request' })
+  @ApiProperty({ description: 'UID of the transaction order' })
   uid?: string;
 
   @ApiProperty({ description: 'Price timestamp' })
