0e08b17cee77cc5abc635a48c4086dd2ccb83bb9
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2025-02-25 22:49:28 +0100

    [DEV-3685] Refactoring

--- src/subdomains/core/custody/custody.module.ts
+++ src/subdomains/core/custody/custody.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { SiftModule } from 'src/integration/sift/sift.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { UserModule } from 'src/subdomains/generic/user/user.module';
 import { ReferralModule } from '../referral/referral.module';
@@ -7,7 +6,7 @@ import { CustodyController } from './controllers/custody.controller';
 import { CustodyService } from './services/custody-service';
 
 @Module({
-  imports: [UserModule, ReferralModule, SiftModule, SharedModule],
+  imports: [UserModule, ReferralModule, SharedModule],
   controllers: [CustodyController],
   providers: [CustodyService],
   exports: [CustodyService],
