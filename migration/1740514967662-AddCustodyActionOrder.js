9afc331c20f2735fa43f56201aeb5584a7c89312
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2025-02-25 21:23:17 +0100

    [DEV-3696] Add custody action order table

---
+++ migration/1740514967662-AddCustodyActionOrder.js
@@ -0,0 +1,21 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class AddCustodyActionOrder1740514967662 {
+    name = 'AddCustodyActionOrder1740514967662'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE TABLE "custody_action_order" ("id" int NOT NULL IDENTITY(1,1), "updated" datetime2 NOT NULL CONSTRAINT "DF_23891f76140b50512faa5c57b47" DEFAULT getdate(), "created" datetime2 NOT NULL CONSTRAINT "DF_2a57d45fb064c48bfc1279a3a64" DEFAULT getdate(), "type" nvarchar(255) NOT NULL, "userConfirmation" bit NOT NULL CONSTRAINT "DF_486735f08a1cacb1ec0f7a3e4ef" DEFAULT 0, "internalConfirmation" bit NOT NULL CONSTRAINT "DF_e93a163da9c9754efc33ec1533d" DEFAULT 0, "userId" int NOT NULL, CONSTRAINT "PK_6376f890b7362db01f58d9a57b4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "custodyAddressIndex" int`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "custodyAddressType" nvarchar(255)`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_6a93cd1ec23d0a32f74cd6a0da" ON "user" ("custodyAddressIndex") WHERE custodyAddressIndex IS NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "custody_action_order" ADD CONSTRAINT "FK_be6219b026c0320b0556c3bf957" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "custody_action_order" DROP CONSTRAINT "FK_be6219b026c0320b0556c3bf957"`);
+        await queryRunner.query(`DROP INDEX "IDX_6a93cd1ec23d0a32f74cd6a0da" ON "user"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "custodyAddressType"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "custodyAddressIndex"`);
+        await queryRunner.query(`DROP TABLE "custody_action_order"`);
+    }
+}
--- src/subdomains/core/custody/entities/custofy-action-order.entity.ts
+++ src/subdomains/core/custody/entities/custofy-action-order.entity.ts
@@ -1,6 +1,7 @@
 import { IEntity, UpdateResult } from 'src/shared/models/entity';
 import { User } from 'src/subdomains/generic/user/models/user/user.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { TransactionRequest } from 'src/subdomains/supporting/payment/entities/transaction-request.entity';
+import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
 import { CustodyActionType } from '../enums/custody';
 
 @Entity()
@@ -8,13 +9,18 @@ export class CustodyActionOrder extends IEntity {
   @Column({ nullable: false })
   type: CustodyActionType;
 
-  @ManyToOne(() => User, (user) => user.custodyActionOrders)
+  @ManyToOne(() => User, (user) => user.custodyActionOrders, { nullable: false })
   user: User;
 
-  @Column({ nullable: true, default: false })
+  @OneToOne(() => TransactionRequest, (transactionRequest) => transactionRequest.custodyActionOrder, {
+    nullable: false,
+  })
+  transactionRequest: TransactionRequest;
+
+  @Column({ nullable: false, default: false })
   userConfirmation?: boolean;
 
-  @Column({ nullable: true, default: false })
+  @Column({ nullable: false, default: false })
   internalConfirmation?: boolean;
 
   userConfirm(): UpdateResult<CustodyActionOrder> {
