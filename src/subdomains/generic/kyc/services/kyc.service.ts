190da44bf1139bb870e2aad696b4383e2b21500b
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-03-07 14:19:29 +0100

    [DEV-3553] change release logic

--- src/subdomains/generic/kyc/services/kyc.service.ts
+++ src/subdomains/generic/kyc/services/kyc.service.ts
@@ -722,13 +722,13 @@ export class KycService {
         if (
           identSteps.some((i) => i.comment?.split(';').includes(KycError.USER_DATA_EXISTING)) ||
           ((identSteps.some((i) => i.comment?.split(';').includes(KycError.BLOCKED)) ||
-            identSteps.length > Config.kyc.maxIdentTries) &&
-            !identSteps.some((i) => i.comment?.split(';').includes(KycError.RELEASED))) ||
-          identSteps.some((i) =>
-            i
-              .getResult<SumsubResult>()
-              ?.webhook?.reviewResult?.rejectLabels?.some((l) => SumSubBlockLabels.includes(l)),
-          )
+            identSteps.length > Config.kyc.maxIdentTries ||
+            identSteps.some((i) =>
+              i
+                .getResult<SumsubResult>()
+                ?.webhook?.reviewResult?.rejectLabels?.some((l) => SumSubBlockLabels.includes(l)),
+            )) &&
+            !identSteps.some((i) => i.comment?.split(';').includes(KycError.RELEASED)))
         )
           return { nextStep: undefined };
 
