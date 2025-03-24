69f4097e725ba8291cd5ce748b8cfdd262cc385a
bernd2022 <104787072+bernd2022@users.noreply.github.com>
2025-03-24 12:06:31 +0100

    [DEV-3761] DEPS pricing service (#2042)

---
+++ src/subdomains/supporting/pricing/services/integration/pricing-deuro.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { DEuroService } from 'src/integration/blockchain/deuro/deuro.service';
+import { Util } from 'src/shared/utils/util';
+import { Price } from '../../domain/entities/price';
+import { PricingProvider } from './pricing-provider';
+
+@Injectable()
+export class PricingDeuroService extends PricingProvider implements OnModuleInit {
+  private static readonly DEURO = 'DEURO';
+  private static readonly DEPS = 'DEPS';
+  private static readonly ALLOWED_ASSETS = [PricingDeuroService.DEURO, PricingDeuroService.DEPS];
+  private static readonly CONTRACT_FEE = 0;
+
+  private deuroService: DEuroService;
+
+  constructor(private readonly moduleRef: ModuleRef) {
+    super();
+  }
+
+  async onModuleInit() {
+    this.deuroService = this.moduleRef.get(DEuroService, { strict: false });
+  }
+
+  async getPrice(from: string, to: string): Promise<Price> {
+    if (!PricingDeuroService.ALLOWED_ASSETS.includes(from)) throw new Error(`from asset ${from} is not allowed`);
+    if (!PricingDeuroService.ALLOWED_ASSETS.includes(to)) throw new Error(`to asset ${to} is not allowed`);
+
+    const contractPrice = await this.deuroService.getDEPSPrice();
+    const assetPrice = from === PricingDeuroService.DEURO ? contractPrice : 1 / contractPrice;
+
+    return Price.create(from, to, Util.round(assetPrice / (1 - PricingDeuroService.CONTRACT_FEE), 8));
+  }
+}
