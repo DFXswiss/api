617678eed61e5afa3a57f26497fd01f7dc7e309e
lapatric <42653152+lapatric@users.noreply.github.com>
2025-04-18 01:17:17 +0200

    [NO-TASK] fix tests 2

--- src/subdomains/core/sell-crypto/route/__tests__/sell.controller.spec.ts
+++ src/subdomains/core/sell-crypto/route/__tests__/sell.controller.spec.ts
@@ -2,12 +2,15 @@ import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from 'src/integration/blockchain/shared/services/crypto.service';
 import { AssetService } from 'src/shared/models/asset/asset.service';
+import { FiatService } from 'src/shared/models/fiat/fiat.service';
 import { PaymentInfoService } from 'src/shared/services/payment-info.service';
 import { TestSharedModule } from 'src/shared/utils/test.shared.module';
 import { TestUtil } from 'src/shared/utils/test.util';
 import { UserService } from 'src/subdomains/generic/user/models/user/user.service';
+import { SwissQRService } from 'src/subdomains/supporting/payment/services/swiss-qr.service';
 import { TransactionHelper } from 'src/subdomains/supporting/payment/services/transaction-helper';
 import { TransactionRequestService } from 'src/subdomains/supporting/payment/services/transaction-request.service';
+import { TransactionService } from 'src/subdomains/supporting/payment/services/transaction.service';
 import { BuyFiatService } from '../../process/services/buy-fiat.service';
 import { SellController } from '../sell.controller';
 import { SellService } from '../sell.service';
@@ -23,6 +26,9 @@ describe('SellController', () => {
   let cryptoService: CryptoService;
   let transactionRequestService: TransactionRequestService;
   let assetService: AssetService;
+  let transactionService: TransactionService;
+  let fiatService: FiatService;
+  let swissQrService: SwissQRService;
 
   beforeEach(async () => {
     sellService = createMock<SellService>();
@@ -33,6 +39,9 @@ describe('SellController', () => {
     cryptoService = createMock<CryptoService>();
     transactionRequestService = createMock<TransactionRequestService>();
     assetService = createMock<AssetService>();
+    transactionService = createMock<TransactionService>();
+    fiatService = createMock<FiatService>();
+    swissQrService = createMock<SwissQRService>();
 
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestSharedModule],
@@ -46,6 +55,9 @@ describe('SellController', () => {
         { provide: CryptoService, useValue: cryptoService },
         { provide: TransactionRequestService, useValue: transactionRequestService },
         { provide: AssetService, useValue: assetService },
+        { provide: TransactionService, useValue: transactionService },
+        { provide: FiatService, useValue: fiatService },
+        { provide: SwissQRService, useValue: swissQrService },
         TestUtil.provideConfig(),
       ],
     }).compile();
