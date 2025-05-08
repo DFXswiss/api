d9207c888028f0a57ea6235d058c500c8e3a1a03
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-05-08 15:44:29 +0200

    [DEV-3922] add LegalEntity GbR

--- src/subdomains/generic/user/models/user-data/user-data.entity.ts
+++ src/subdomains/generic/user/models/user-data/user-data.entity.ts
@@ -69,6 +69,7 @@ export enum LegalEntity {
   AG = 'AG',
   GMBH = 'GmbH',
   UG = 'UG',
+  GBR = 'GbR',
   ASSOCIATION = 'Association',
   FOUNDATION = 'Foundation',
   LIFE_INSURANCE = 'LifeInsurance',
