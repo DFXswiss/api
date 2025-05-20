bf4b78107ebcaaf7de9904af5109a6580a6bb2d4
David May <david.leo.may@gmail.com>
2025-05-20 18:09:40 +0200

    [DEV-3791] Added migration

---
+++ migration/1747757193317-AdaptiveLiquidityManagement.js
@@ -0,0 +1,19 @@
+module.exports = class AdaptiveLiquidityManagement1747757193317 {
+    name = 'AdaptiveLiquidityManagement1747757193317'
+
+    async up(queryRunner) {
+        await queryRunner.query(`EXEC sp_rename "liquidity_management_order.amount", "minAmount"`);
+        await queryRunner.query(`ALTER TABLE "liquidity_management_order" ADD "maxAmount" float`);
+
+        await queryRunner.query(`EXEC sp_rename "liquidity_management_pipeline.targetAmount", "minAmount"`);
+        await queryRunner.query(`ALTER TABLE "liquidity_management_pipeline" ADD "optAmount" float`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "liquidity_management_pipeline" DROP COLUMN "optAmount"`);
+        await queryRunner.query(`EXEC sp_rename "liquidity_management_pipeline.minAmount", "targetAmount"`);
+
+        await queryRunner.query(`ALTER TABLE "liquidity_management_order" DROP COLUMN "maxAmount"`);
+        await queryRunner.query(`EXEC sp_rename "liquidity_management_order.minAmount", "amount"`);
+    }
+}
