d6da2df9b226b36531a1c241dfa8be27d4fceb97
David May <david.leo.may@gmail.com>
2025-06-04 16:59:12 +0200

    [NO-TASK] Invert swap chain check

--- src/subdomains/core/buy-crypto/routes/swap/swap.entity.ts
+++ src/subdomains/core/buy-crypto/routes/swap/swap.entity.ts
@@ -9,16 +9,7 @@ import { ChildEntity, Column, JoinColumn, ManyToOne, OneToMany, OneToOne } from 
 import { Deposit } from '../../../../supporting/address-pool/deposit/deposit.entity';
 import { DepositRoute } from '../../../../supporting/address-pool/route/deposit-route.entity';
 
-export const SwapInputBlockchains: Blockchain[] = [
-  Blockchain.BITCOIN,
-  Blockchain.LIGHTNING,
-  Blockchain.ETHEREUM,
-  Blockchain.ARBITRUM,
-  Blockchain.OPTIMISM,
-  Blockchain.POLYGON,
-  Blockchain.BINANCE_SMART_CHAIN,
-  Blockchain.BASE,
-];
+export const NoSwapBlockchains: Blockchain[] = [Blockchain.MONERO];
 
 @ChildEntity('Crypto')
 export class Swap extends DepositRoute {
