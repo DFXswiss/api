bdecbb6d6af2dec218a1e5d8c8f22c132b049f97
OakHost Customer <customer@oh-23-88-91-208.client.oakhost-customer.net>
2025-06-26 21:00:29 +0000

    migration

---
+++ migration/1750971576155-AddModeToPaymentLinkTable.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ */
+
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
+module.exports = class AddModeToPaymentLinkTable1750971576155 {
+    name = 'AddModeToPaymentLinkTable1750971576155'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "payment_link" ADD "mode" nvarchar(256) NOT NULL CONSTRAINT "DF_a09516f7c0897254173b13a5db0" DEFAULT 'Multiple'`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "payment_link" DROP CONSTRAINT "DF_a09516f7c0897254173b13a5db0"`);
+        await queryRunner.query(`ALTER TABLE "payment_link" DROP COLUMN "mode"`);
+    }
+}
