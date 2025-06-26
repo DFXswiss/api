30128ee156c08cb2e53d451bf4461e226831a30a
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-06-26 15:16:44 +0200

    [DEV-2478] remove not injectable logger from module

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,12 +1,11 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { NotificationModule } from 'src/subdomains/supporting/notification/notification.module';
-import { DfxLogger } from './dfx-logger.service';
 import { LoggerFactory } from './logger.factory';
 
 @Module({
   imports: [forwardRef(() => NotificationModule)],
   controllers: [],
-  providers: [LoggerFactory, DfxLogger],
-  exports: [LoggerFactory, DfxLogger],
+  providers: [LoggerFactory],
+  exports: [LoggerFactory],
 })
 export class LoggerModule {}
