39c993aacb90b198bbc785a19ddfbfa5369fbffb
David May <david.leo.may@gmail.com>
2025-06-30 17:38:07 +0200

    [DEV-4096] Refactoring

--- src/config/config.ts
+++ src/config/config.ts
@@ -568,7 +568,7 @@ export class Configuration {
 
     binancePayPublic: process.env.BINANCEPAY_PUBLIC_KEY,
     binancePaySecret: process.env.BINANCEPAY_SECRET_KEY,
-    binancePayMerchantId: '218832235',
+    binancePayMerchantId: process.env.BINANCEPAY_MERCHANT_ID,
 
     checkbotSignTx: process.env.PAYMENT_CHECKBOT_SIGN_TX,
     checkbotPubKey: process.env.PAYMENT_CHECKBOT_PUB_KEY?.split('<br>').join('\n'),
