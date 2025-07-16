626c98f10a7d87a8019e75dcf7a1f415b1eae958
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-07-16 11:41:17 +0200

    Add Spark as an additional payment method

--- src/config/config.ts
+++ src/config/config.ts
@@ -563,7 +563,7 @@ export class Configuration {
     defaultQuoteTimeout: 300, // sec
     addressQuoteTimeout: 7200, // sec
 
-    manualMethods: ['KuCoinPay', 'TaprootAsset'],
+    manualMethods: ['KuCoinPay', 'TaprootAsset', 'Spark'],
 
     webhookPublicKey: process.env.PAYMENT_WEBHOOK_PUBLIC_KEY?.split('<br>').join('\n'),
     webhookPrivateKey: process.env.PAYMENT_WEBHOOK_PRIVATE_KEY?.split('<br>').join('\n'),
