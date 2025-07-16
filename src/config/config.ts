f3ca8ca9477a9b81aee1f9df717c2d4a802e2a8c
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-07-16 11:39:01 +0200

    rename TaprootAsset

--- src/config/config.ts
+++ src/config/config.ts
@@ -563,7 +563,7 @@ export class Configuration {
     defaultQuoteTimeout: 300, // sec
     addressQuoteTimeout: 7200, // sec
 
-    manualMethods: ['KuCoinPay', 'BitcoinOnChainTaprootAsset'],
+    manualMethods: ['KuCoinPay', 'TaprootAsset'],
 
     webhookPublicKey: process.env.PAYMENT_WEBHOOK_PUBLIC_KEY?.split('<br>').join('\n'),
     webhookPrivateKey: process.env.PAYMENT_WEBHOOK_PRIVATE_KEY?.split('<br>').join('\n'),
