8e992c8e255c8166cb987517dcfc63f89f9f0f02
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-08-05 22:48:32 +0200

    [NOTASK] change description property

--- src/subdomains/core/payment-link/controllers/payment-link.controller.ts
+++ src/subdomains/core/payment-link/controllers/payment-link.controller.ts
@@ -344,7 +344,7 @@ export class PaymentLinkController {
   @ApiQuery({ name: 'ids', description: 'Comma-separated payment link IDs', required: false })
   @ApiQuery({ name: 'type', description: 'Sticker type', required: false, enum: StickerType })
   @ApiQuery({ name: 'lang', description: 'Language code', required: false })
-  @ApiQuery({ name: 'mode', description: 'QR code mode (PaymentLink or Pos)', required: false, enum: StickerQrMode })
+  @ApiQuery({ name: 'mode', description: 'QR code mode', required: false, enum: StickerQrMode })
   async generateOcpStickers(
     @GetJwt() jwt: JwtPayload,
     @Query('route') route: string,
