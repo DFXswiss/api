68709b2662f43f4597af1e62f070c55b45b28443
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-08-22 14:12:38 +0200

    [NOTASK] fix tron as paymentAsset in financeLog

--- src/subdomains/supporting/log/log-job.service.ts
+++ src/subdomains/supporting/log/log-job.service.ts
@@ -223,6 +223,7 @@ export class LogJobService {
           Blockchain.ZANO,
           Blockchain.BINANCE_PAY,
           Blockchain.KUCOIN_PAY,
+          Blockchain.TRON,
         ].includes(a.blockchain),
     );
     const paymentAssetMap = Util.groupBy<Asset, Blockchain>(paymentAssets, 'blockchain');
