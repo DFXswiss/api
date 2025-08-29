1996f799512ff9cb58ced36325ebe37a69a049a0
Yannick <52333989+Yannick1712@users.noreply.github.com>
2025-08-29 15:11:55 +0200

    [DEV-4346] manual check phone (#2410)
    * [DEV-4346] manual check phone
    
    * [DEV-4346] Refactoring

--- src/subdomains/core/sell-crypto/process/buy-fiat.entity.ts
+++ src/subdomains/core/sell-crypto/process/buy-fiat.entity.ts
@@ -556,6 +556,7 @@ export const BuyFiatAmlReasonPendingStates = [
   AmlReason.VIDEO_IDENT_NEEDED,
   AmlReason.KYC_DATA_NEEDED,
   AmlReason.BANK_TX_NEEDED,
+  AmlReason.MANUAL_CHECK_PHONE,
 ];
 
 export const BuyFiatEditableAmlCheck = [CheckStatus.PENDING, CheckStatus.GSHEET, CheckStatus.FAIL];
