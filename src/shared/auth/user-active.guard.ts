1991074d631a0c20f38acda1d71f4b5c6b45cf71
Yannick <52333989+Yannick1712@users.noreply.github.com>
2025-09-09 00:52:07 +0200

    [DEV-4343] add riskStatus to ActiveGuard (#2406)

--- src/shared/auth/user-active.guard.ts
+++ src/shared/auth/user-active.guard.ts
@@ -1,28 +1,34 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { UserDataStatus } from 'src/subdomains/generic/user/models/user-data/user-data.enum';
+import { RiskStatus, UserDataStatus } from 'src/subdomains/generic/user/models/user-data/user-data.enum';
 import { UserStatus } from 'src/subdomains/generic/user/models/user/user.entity';
 
 class UserActiveGuardClass implements CanActivate {
   constructor(
-    private readonly blockedUserStatus?: UserStatus[],
-    private readonly blockedUserDataStatus?: UserDataStatus[],
+    private readonly blockedUserStatus: UserStatus[] = [],
+    private readonly blockedUserDataStatus: UserDataStatus[] = [],
+    private readonly blockedUserDataRiskStatus: RiskStatus[] = [],
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     const userStatus = req.user.userStatus;
     const accountStatus = req.user.accountStatus;
+    const riskStatus = req.user.riskStatus;
 
-    return this.blockedUserDataStatus || this.blockedUserStatus
-      ? !this.blockedUserStatus.includes(userStatus) && !this.blockedUserDataStatus.includes(accountStatus)
+    return this.blockedUserDataStatus.length || this.blockedUserStatus.length || this.blockedUserDataRiskStatus.length
+      ? !this.blockedUserStatus.includes(userStatus) &&
+          !this.blockedUserDataStatus.includes(accountStatus) &&
+          !this.blockedUserDataRiskStatus.includes(riskStatus)
       : ![UserStatus.BLOCKED, UserStatus.DELETED].includes(userStatus) &&
-          ![UserDataStatus.BLOCKED, UserDataStatus.DEACTIVATED].includes(accountStatus);
+          ![UserDataStatus.BLOCKED, UserDataStatus.DEACTIVATED].includes(accountStatus) &&
+          (!riskStatus || ![RiskStatus.BLOCKED, RiskStatus.SUSPICIOUS].includes(riskStatus));
   }
 }
 
 export function UserActiveGuard(
-  blockedUserStatus?: UserStatus[],
-  blockedUserDataStatus?: UserDataStatus[],
+  blockedUserStatus: UserStatus[] = [],
+  blockedUserDataStatus: UserDataStatus[] = [],
+  blockedUserDataRiskStatus: RiskStatus[] = [],
 ): UserActiveGuardClass {
-  return new UserActiveGuardClass(blockedUserStatus, blockedUserDataStatus);
+  return new UserActiveGuardClass(blockedUserStatus, blockedUserDataStatus, blockedUserDataRiskStatus);
 }
