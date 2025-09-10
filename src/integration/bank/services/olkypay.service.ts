b13f90de763f6ade5d9b20d8ad4cb967053270cf
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-09-11 00:56:22 +0200

    feat: Automatically set bankReleaseDate for Olkypay transactions (#2442)

--- src/integration/bank/services/olkypay.service.ts
+++ src/integration/bank/services/olkypay.service.ts
@@ -113,6 +113,7 @@ export class OlkypayService {
         remittanceInfo: tx.line2,
         accountIban: accountIban,
         type: tx.codeInterbancaireInterne === TransactionType.BILLING ? BankTxType.BANK_ACCOUNT_FEE : null,
+        bankReleaseDate: new Date(), // Automatically set for Olkypay transactions
       };
     } catch (e) {
       throw new Error(`Failed to parse transaction ${tx.idCtp}: ${e.message}`);
