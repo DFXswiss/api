9fcd43a0fe9d6f97bcd08ff7ec6e973be6abf425
David May <85513542+davidleomay@users.noreply.github.com>
2025-09-15 17:20:10 +0200

    [DEV-4330] TX risk assessment (#2409)
    * [DEV-4330] Added entity, service + controller
    
    * [DEV-4330] Added migration
    
    * [DEV-4330] Refactoring

--- src/subdomains/supporting/payment/transaction.module.ts
+++ src/subdomains/supporting/payment/transaction.module.ts
@@ -1,19 +1,32 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SharedModule } from 'src/shared/shared.module';
 import { UserModule } from 'src/subdomains/generic/user/user.module';
 import { NotificationModule } from '../notification/notification.module';
+import { TransactionAdminController } from './controllers/transaction-admin.controller';
+import { TransactionRiskAssessment } from './entities/transaction-risk-assessment.entity';
+import { Transaction } from './entities/transaction.entity';
 import { SpecialExternalAccountRepository } from './repositories/special-external-account.repository';
+import { TransactionRiskAssessmentRepository } from './repositories/transaction-risk-assessment.repository';
 import { TransactionRepository } from './repositories/transaction.repository';
 import { SpecialExternalAccountService } from './services/special-external-account.service';
 import { TransactionNotificationService } from './services/transaction-notification.service';
+import { TransactionRiskAssessmentService } from './services/transaction-risk-assessment.service';
 import { TransactionService } from './services/transaction.service';
 
 @Module({
-  imports: [NotificationModule, forwardRef(() => UserModule), SharedModule],
-  controllers: [],
+  imports: [
+    NotificationModule,
+    forwardRef(() => UserModule),
+    SharedModule,
+    TypeOrmModule.forFeature([Transaction, TransactionRiskAssessment]),
+  ],
+  controllers: [TransactionAdminController],
   providers: [
     TransactionRepository,
     TransactionService,
+    TransactionRiskAssessmentRepository,
+    TransactionRiskAssessmentService,
     SpecialExternalAccountService,
     SpecialExternalAccountRepository,
     TransactionNotificationService,
