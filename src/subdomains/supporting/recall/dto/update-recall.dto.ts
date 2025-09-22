0ae156e3431a835ac02b2852a03dceb875ad3ca4
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-09-22 10:59:01 +0200

    [DEV-4398] recall update endpoint

---
+++ src/subdomains/supporting/recall/dto/update-recall.dto.ts
@@ -0,0 +1,19 @@
+import { IsInt, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateRecallDto {
+  @IsOptional()
+  @IsInt()
+  sequence: number;
+
+  @IsOptional()
+  @IsInt()
+  userId?: number;
+
+  @IsOptional()
+  @IsString()
+  comment: string;
+
+  @IsOptional()
+  @IsNumber()
+  fee: number;
+}
