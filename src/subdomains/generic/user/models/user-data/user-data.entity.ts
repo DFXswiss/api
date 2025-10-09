0ecce187b68eece9e2f7458a16f2da7d76e58de0
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-10-09 10:16:40 +0200

    [DEV-4407] add recallAgreementAccepted to suppport update

--- src/subdomains/generic/user/models/user-data/user-data.entity.ts
+++ src/subdomains/generic/user/models/user-data/user-data.entity.ts
@@ -701,7 +701,7 @@ export class UserData extends IEntity {
 
 export const KycCompletedStates = [KycStatus.COMPLETED];
 
-export const UserDataSupportUpdateCols = ['status', 'riskStatus'];
+export const UserDataSupportUpdateCols = ['status', 'riskStatus', 'recallAgreementAccepted'];
 export const UserDataComplianceUpdateCols = ['kycStatus', 'depositLimit'];
 
 export function KycCompleted(kycStatus?: KycStatus): boolean {
