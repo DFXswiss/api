613604a0f54de391b20942fa7ca85886eed936de
bernd2022 <bernd.nospam.2018@t-online.de>
2025-10-24 15:48:11 +0200

    [DEV-4425] Added Citreasan info to infrastructure

---
+++ infrastructure/citrea/citreascan/scripts/setupEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~
+mkdir update
+
+mkdir -p volumes/bitcoin/testnet4/wallets
+
+mkdir -p volumes/citrea/testnet4
+mkdir -p volumes/blockscout/citrea/testnet
+
+mkdir -p volumes/nginx/www
---
+++ infrastructure/citrea/citreascan/scripts/update-blockscout.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+case "$1" in
+  frontend|backend)
+  ;;
+  *)
+    echo "Invalid parameter '$1'"
+    echo "Allowed values are: 'frontend' or 'backend'"
+    exit 1
+  ;;
+esac
+
+composefile=docker-compose-blockscout-citrea-testnet.yml
+service=$1
+
+cd /home/dfx
+
+docker compose -f ${composefile} down ${service}
+docker compose -f ${composefile} pull ${service}
+
+./update/replace-docker-compose-blockscout.sh
+
+docker compose -f ${composefile} up -d ${service}
