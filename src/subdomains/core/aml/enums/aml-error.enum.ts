4d2756695f23630f38159b61106dc9b33d0f1723
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-11-11 15:53:22 +0100

    [DEV-4482] amlCheck tradeApprovalDate

--- src/subdomains/core/aml/enums/aml-error.enum.ts
+++ src/subdomains/core/aml/enums/aml-error.enum.ts
@@ -59,6 +59,7 @@ export enum AmlError {
   IP_PHONE_VERIFICATION_NEEDED = 'IpPhoneVerificationNeeded',
   IP_BLACKLISTED_WITHOUT_KYC = 'IpBlacklistedWithoutKyc',
   BANK_RELEASE_DATE_MISSING = 'BankReleaseDateMissing',
+  TRADE_APPROVAL_DATE_MISSING = 'TradeApprovalDateMissing',
 }
 
 export const DelayResultError = [
@@ -275,5 +276,10 @@ export const AmlErrorResult: {
     type: AmlErrorType.CRUCIAL,
     amlCheck: CheckStatus.PENDING,
     amlReason: AmlReason.HIGH_RISK_KYC_NEEDED,
+  },
+  [AmlError.TRADE_APPROVAL_DATE_MISSING]: {
+    type: AmlErrorType.CRUCIAL,
+    amlCheck: CheckStatus.PENDING,
+    amlReason: AmlReason.MANUAL_CHECK_PHONE,
   },
 };
