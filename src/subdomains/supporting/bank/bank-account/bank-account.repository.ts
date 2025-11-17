ac144f8b18664882e455f128ef244bba645b2620
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-11-17 18:20:51 +0100

    [DEV-4489] add period parameter in CachedRepo

--- src/subdomains/supporting/bank/bank-account/bank-account.repository.ts
+++ src/subdomains/supporting/bank/bank-account/bank-account.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CachedRepository } from 'src/shared/repositories/cached.repository';
+import { CacheItemResetPeriod } from 'src/shared/utils/async-cache';
 import { EntityManager } from 'typeorm';
 import { BankAccount } from './bank-account.entity';
 
 @Injectable()
 export class BankAccountRepository extends CachedRepository<BankAccount> {
   constructor(manager: EntityManager) {
-    super(BankAccount, manager);
+    super(BankAccount, manager, CacheItemResetPeriod.EVERY_24_HOURS);
   }
 }
