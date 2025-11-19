4609d59f587f5d54bc5e28c3aed2889b1c6b5659
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2025-11-19 11:26:08 +0100

    [NOTASK] Add controller

--- src/subdomains/core/faucet-request/faucet-request.module.ts
+++ src/subdomains/core/faucet-request/faucet-request.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/shared/shared.module';
 import { UserModule } from 'src/subdomains/generic/user/user.module';
 import { PayoutModule } from 'src/subdomains/supporting/payout/payout.module';
 import { PricingModule } from 'src/subdomains/supporting/pricing/pricing.module';
+import { FaucetRequestController } from './controller/faucet-request.controller';
 import { FaucetRequest } from './entities/faucet-request.entity';
 import { FaucetRequestRepository } from './repositories/faucet-request.repository';
 import { FaucetRequestService } from './services/faucet-request.service';
@@ -18,7 +19,7 @@ import { FaucetRequestService } from './services/faucet-request.service';
     PricingModule,
     PayoutModule,
   ],
-  controllers: [],
+  controllers: [FaucetRequestController],
   providers: [FaucetRequestService, FaucetRequestRepository],
   exports: [],
 })
