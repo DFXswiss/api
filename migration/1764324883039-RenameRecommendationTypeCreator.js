c34ae116309f0c7925148256e14c1918d68555e0
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-11-28 15:41:06 +0100

    [NOTASK] fix migration

--- migration/1764324883039-RenameRecommendationTypeCreator.js
+++ migration/1764324883039-RenameRecommendationTypeCreator.js
@@ -10,12 +10,12 @@ module.exports = class RenameRecommendationTypeCreator1764324883039 {
     name = 'RenameRecommendationTypeCreator1764324883039'
 
     async up(queryRunner) {
-        await queryRunner.query(`EXEC sp_rename dbo.recommendation.type", "method"`);
-        await queryRunner.query(`EXEC sp_rename dbo.recommendation.creator", "type"`);
+        await queryRunner.query(`EXEC sp_rename "dbo.recommendation.type", "method"`);
+        await queryRunner.query(`EXEC sp_rename "dbo.recommendation.creator", "type"`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`EXEC sp_rename dbo.recommendation.type", "creator"`);
-        await queryRunner.query(`EXEC sp_rename dbo.recommendation.method", "type"`);
+        await queryRunner.query(`EXEC sp_rename "dbo.recommendation.type", "creator"`);
+        await queryRunner.query(`EXEC sp_rename "dbo.recommendation.method", "type"`);
     }
 }
