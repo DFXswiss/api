3b1166d76c0f43cfecacd43e079dfc7c38ec96b5
Yannick <52333989+Yannick1712@users.noreply.github.com>
2025-12-08 15:58:53 +0100

    [DEV-4513] bankTxRepeat chargeback cols (#2590)
    * [DEV-4513] bankTxRepeat chargeback cols
    
    * [DEV-4513] add fiatOutput logic
    
    * [DEV-4513] fix unit test
    
    * [DEV-4513] add migration
    
    * [DEV-4513] add relation

--- src/subdomains/supporting/bank-tx/bank-tx-repeat/bank-tx-repeat.entity.ts
+++ src/subdomains/supporting/bank-tx/bank-tx-repeat/bank-tx-repeat.entity.ts
@@ -3,6 +3,7 @@ import { IEntity } from 'src/shared/models/entity';
 import { User } from 'src/subdomains/generic/user/models/user/user.entity';
 import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { BankService } from '../../bank/bank/bank.service';
+import { FiatOutput } from '../../fiat-output/fiat-output.entity';
 import { Transaction } from '../../payment/entities/transaction.entity';
 import { BankTx } from '../bank-tx/entities/bank-tx.entity';
 
@@ -20,9 +21,16 @@ export class BankTxRepeat extends IEntity {
   @JoinColumn()
   chargebackBankTx?: BankTx;
 
+  @OneToOne(() => FiatOutput, { nullable: true })
+  @JoinColumn()
+  chargebackOutput?: FiatOutput;
+
   @OneToOne(() => Transaction, { eager: true, nullable: true })
   @JoinColumn()
   transaction?: Transaction;
+
+  @ManyToOne(() => User, { nullable: true })
+  user?: User;
 
   @Column({ length: 256, nullable: true })
   info?: string;
@@ -36,8 +44,26 @@ export class BankTxRepeat extends IEntity {
   @Column({ type: 'float', nullable: true })
   amountInUsd?: number;
 
-  @ManyToOne(() => User, { nullable: true })
-  user: User;
+  @Column({ type: 'datetime2', nullable: true })
+  chargebackDate?: Date;
+
+  @Column({ length: 256, nullable: true })
+  chargebackRemittanceInfo?: string;
+
+  @Column({ type: 'datetime2', nullable: true })
+  chargebackAllowedDate?: Date;
+
+  @Column({ type: 'datetime2', nullable: true })
+  chargebackAllowedDateUser?: Date;
+
+  @Column({ type: 'float', nullable: true })
+  chargebackAmount?: number;
+
+  @Column({ length: 256, nullable: true })
+  chargebackAllowedBy?: string;
+
+  @Column({ length: 256, nullable: true })
+  chargebackIban?: string;
 
   //*** METHODS ***//
 
