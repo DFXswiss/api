96fde9a687512c886fdb764749a7bb07aac1634b
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2025-12-11 00:19:55 +0100

    [NO-TASK] Add possibility to register with taproot address / signature (#2573)

--- src/integration/blockchain/shared/services/crypto.service.ts
+++ src/integration/blockchain/shared/services/crypto.service.ts
@@ -1,6 +1,7 @@
 import verifyCardanoSignature from '@cardano-foundation/cardano-verify-datasignature';
 import { MainNet } from '@defichain/jellyfish-network';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { Verifier } from 'bip322-js';
 import { verify } from 'bitcoinjs-message';
 import { isEthereumAddress } from 'class-validator';
 import { verifyMessage } from 'ethers/lib/utils';
@@ -257,11 +258,22 @@ export class CryptoService {
 
   private verifyBitcoinBased(message: string, address: string, signature: string, prefix: string | null): boolean {
     let isValid = false;
+
     try {
-      isValid = verify(message, address, signature, prefix, true);
+      isValid = Verifier.verifySignature(address, message, signature, true);
     } catch {}
 
-    if (!isValid) isValid = verify(message, address, signature, prefix);
+    if (!isValid) {
+      try {
+        isValid = verify(message, address, signature, prefix, true); // ← WICHTIG: electrum=true
+      } catch {}
+    }
+
+    if (!isValid) {
+      try {
+        isValid = verify(message, address, signature, prefix);
+      } catch {}
+    }
 
     return isValid;
   }
