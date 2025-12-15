cee6b2d2dd7621b9e57acbb950e27aca6ca212ee
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2025-12-15 02:31:21 +0100

    [NOTASK] remove userData wallet sync

--- src/subdomains/generic/user/models/user-data/user-data-job.service.ts
+++ src/subdomains/generic/user/models/user-data/user-data-job.service.ts
@@ -7,9 +7,9 @@ import { FileType } from 'src/subdomains/generic/kyc/dto/kyc-file.dto';
 import { KycStepName } from 'src/subdomains/generic/kyc/enums/kyc-step-name.enum';
 import { ReviewStatus } from 'src/subdomains/generic/kyc/enums/review-status.enum';
 import { KycService } from 'src/subdomains/generic/kyc/services/kyc.service';
-import { In, IsNull, Like, MoreThan, Not } from 'typeorm';
+import { IsNull, Like, MoreThan } from 'typeorm';
 import { AccountType } from './account-type.enum';
-import { KycLevel, SignatoryPower, UserDataStatus } from './user-data.enum';
+import { KycLevel, SignatoryPower } from './user-data.enum';
 import { UserDataRepository } from './user-data.repository';
 
 @Injectable()
@@ -20,27 +20,6 @@ export class UserDataJobService {
   async fillUserData() {
     await this.bankTxVerification();
     await this.setAccountOpener();
-  }
-
-  @DfxCron(CronExpression.EVERY_MINUTE, { process: Process.USER_DATA_WALLET_SYNC, timeout: 1800 })
-  async syncWallets() {
-    await this.walletSync();
-  }
-
-  private async walletSync(): Promise<void> {
-    const entities = await this.userDataRepo.find({
-      where: {
-        wallet: { id: IsNull() },
-        status: Not(In([UserDataStatus.KYC_ONLY, UserDataStatus.MERGED])),
-        users: { id: Not(IsNull()) },
-      },
-      take: 5000,
-      relations: { users: { wallet: true } },
-    });
-
-    for (const entity of entities) {
-      await this.userDataRepo.update(entity.id, { wallet: entity.users[0].wallet });
-    }
   }
 
   private async bankTxVerification(): Promise<void> {
