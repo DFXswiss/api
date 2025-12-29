599d7fbe53f86a27c2d5551bb0e95d6f286d6303
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-12-29 23:35:48 +0100

    Remove deprecated interface-name-prefix ESLint rule

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,6 @@ module.exports = tseslint.config(
       'no-console': ['warn'],
       '@typescript-eslint/return-await': ['warn', 'in-try-catch'],
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
