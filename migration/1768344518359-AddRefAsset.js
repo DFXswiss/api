1866072944c077e05e6f4b638a5ce1c3a0d1237f
Kolibri <66674482+Kolibri1990@users.noreply.github.com>
2026-01-13 23:49:10 +0100

    feat(DEV-3610): Add migration

--- migration/1768344518359-AddRefAsset.js
+++ migration/1768344518359-AddRefAsset.js
@@ -7,13 +7,14 @@
  * @class
  * @implements {MigrationInterface}
  */
-module.exports = class AddRefAsset1768344360851 {
-    name = 'AddRefAsset1768344360851'
+module.exports = class AddRefAsset1768344518359 {
+    name = 'AddRefAsset1768344518359'
 
     /**
      * @param {QueryRunner} queryRunner
      */
     async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "asset" ADD "refEnabled" bit NOT NULL CONSTRAINT "DF_d2c85e8cbdbff07a1dcd8d17797" DEFAULT 0`);
         await queryRunner.query(`ALTER TABLE "user" ADD "refAssetId" int`);
         await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_20e823fee19baff0c5090ab72df" FOREIGN KEY ("refAssetId") REFERENCES "asset"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -24,5 +25,7 @@ module.exports = class AddRefAsset1768344360851 {
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_20e823fee19baff0c5090ab72df"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "refAssetId"`);
+        await queryRunner.query(`ALTER TABLE "asset" DROP CONSTRAINT "DF_d2c85e8cbdbff07a1dcd8d17797"`);
+        await queryRunner.query(`ALTER TABLE "asset" DROP COLUMN "refEnabled"`);
     }
 }
