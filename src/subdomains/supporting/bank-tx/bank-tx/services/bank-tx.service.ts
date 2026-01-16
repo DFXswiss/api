b58830bd33cd5931d52721523cb07e7ba1d52b8b
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-01-16 18:41:20 +0100

    Add automatic SCB bank transaction detection
    Automatically assign BankTxType.SCB to transactions where
    the name contains "SCB AG".

--- src/subdomains/supporting/bank-tx/bank-tx/services/bank-tx.service.ts
+++ src/subdomains/supporting/bank-tx/bank-tx/services/bank-tx.service.ts
@@ -505,6 +505,10 @@ export class BankTxService implements OnModuleInit {
       return BankTxType.SCRYPT;
     }
 
+    if (tx.name?.includes('SCB AG')) {
+      return BankTxType.SCB;
+    }
+
     return null;
   }
 
