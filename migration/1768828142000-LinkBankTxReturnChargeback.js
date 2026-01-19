3b046b094cae8670ab596263b687d00ff12a9808
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-01-19 14:35:22 +0100

    Fix bank_tx and transaction type for chargeback 185618
    The chargeback bank_tx 185618 and its transaction 295791 incorrectly
    had type 'Internal' instead of 'BankTxReturn-Chargeback'.
    
    Changes:
    - bank_tx 185618: Internal -> BankTxReturn-Chargeback
    - transaction 295791: Internal -> BankTxReturn-Chargeback

--- migration/1768828142000-LinkBankTxReturnChargeback.js
+++ migration/1768828142000-LinkBankTxReturnChargeback.js
@@ -10,8 +10,11 @@
  * - Chargeback (bank_tx 185618): Outgoing refund to original sender (DBIT, 57399.75 EUR)
  * - Chargeback date: 2026-01-14 (booking date of bank_tx 185618)
  *
- * Note: The setFiatAmounts cron will NOT process this record because chargebackOutputId is NULL.
- * Therefore, we must set amountInEur/Chf/Usd directly in this migration.
+ * Fixes:
+ * 1. BankTxReturn 452: Set chargeback link and fiat amounts (cron won't process due to NULL chargebackOutputId)
+ * 2. bank_tx 185618: Fix type from 'Internal' to 'BankTxReturn-Chargeback'
+ * 3. transaction 295791: Fix type from 'Internal' to 'BankTxReturn-Chargeback'
+ *
  * Exchange rates from 2026-01-14: EUR/CHF=0.932, EUR/USD=1.161
  *
  * @class
@@ -59,6 +62,20 @@ module.exports = class LinkBankTxReturnChargeback1768828142000 {
     console.log(
       `Linked chargeback bank_tx 185618 to BankTxReturn 452 (bank_tx 186225): ${chargebackAmount} EUR, ${amountInChf} CHF, ${amountInUsd} USD`,
     );
+
+    // Fix bank_tx 185618 type: 'Internal' -> 'BankTxReturn-Chargeback'
+    await queryRunner.query(`UPDATE dbo.bank_tx SET type = @0 WHERE id = @1`, [
+      'BankTxReturn-Chargeback',
+      185618,
+    ]);
+    console.log('Fixed bank_tx 185618 type: Internal -> BankTxReturn-Chargeback');
+
+    // Fix transaction 295791 type: 'Internal' -> 'BankTxReturn-Chargeback'
+    await queryRunner.query(`UPDATE dbo.[transaction] SET type = @0 WHERE id = @1`, [
+      'BankTxReturn-Chargeback',
+      295791,
+    ]);
+    console.log('Fixed transaction 295791 type: Internal -> BankTxReturn-Chargeback');
   }
 
   /**
@@ -82,5 +99,13 @@ module.exports = class LinkBankTxReturnChargeback1768828142000 {
     );
 
     console.log('Unlinked chargeback and reset fiat amounts for BankTxReturn (bank_tx 186225)');
+
+    // Revert bank_tx 185618 type back to 'Internal'
+    await queryRunner.query(`UPDATE dbo.bank_tx SET type = @0 WHERE id = @1`, ['Internal', 185618]);
+    console.log('Reverted bank_tx 185618 type: BankTxReturn-Chargeback -> Internal');
+
+    // Revert transaction 295791 type back to 'Internal'
+    await queryRunner.query(`UPDATE dbo.[transaction] SET type = @0 WHERE id = @1`, ['Internal', 295791]);
+    console.log('Reverted transaction 295791 type: BankTxReturn-Chargeback -> Internal');
   }
 };
