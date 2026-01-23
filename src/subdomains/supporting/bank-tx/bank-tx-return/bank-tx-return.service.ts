b30dfbc8f1107cf2c915e1013e5ff5cd7c8829e8
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2026-01-23 18:39:23 +0100

    [DEV-4539] Dont overwrite creditorData

--- src/subdomains/supporting/bank-tx/bank-tx-return/bank-tx-return.service.ts
+++ src/subdomains/supporting/bank-tx/bank-tx-return/bank-tx-return.service.ts
@@ -200,7 +200,7 @@ export class BankTxReturnService {
     )
       throw new BadRequestException('IBAN not valid or BIC not available');
 
-    const creditorData = dto.creditorData ?? bankTxReturn.creditorData;
+    const creditorData = bankTxReturn.creditorData ?? dto.creditorData;
     if ((dto.chargebackAllowedDate || dto.chargebackAllowedDateUser) && !creditorData)
       throw new BadRequestException('Creditor data is required for chargeback');
 
