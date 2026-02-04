359b9c53b92e3a5bc67f81a363bd775837df33bd
Yannick <52333989+Yannick1712@users.noreply.github.com>
2026-02-04 16:32:10 +0100

    [DEV-4543] SupportIssue sepa template (#3076)
    * [DEV-4543] SupportIssue sepa template
    
    * [DEV-4543] fix linter
    
    * [DEV-4543] Refactoring
    
    * [DEV-4543] Refactoring 2
    
    * [DEV-4543] fix undefined bug
    
    * [DEV-4543] renaming setting

--- src/subdomains/supporting/support-issue/services/support-issue-job.service.ts
+++ src/subdomains/supporting/support-issue/services/support-issue-job.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CronExpression } from '@nestjs/schedule';
+import { SettingService } from 'src/shared/models/setting/setting.service';
 import { Process } from 'src/shared/services/process.service';
 import { DfxCron } from 'src/shared/utils/cron';
 import { Util } from 'src/shared/utils/util';
 import { CheckStatus } from 'src/subdomains/core/aml/enums/check-status.enum';
 import { FindOptionsWhere, In, IsNull, MoreThan, Not } from 'typeorm';
 import { MailFactory } from '../../notification/factories/mail.factory';
+import { TransactionRequestType } from '../../payment/entities/transaction-request.entity';
 import { SupportMessageTranslationKey } from '../dto/support-issue.dto';
 import { SupportIssue } from '../entities/support-issue.entity';
 import { AutoResponder } from '../entities/support-message.entity';
@@ -13,21 +15,52 @@ import { SupportIssueInternalState, SupportIssueReason, SupportIssueType } from 
 import { SupportIssueRepository } from '../repositories/support-issue.repository';
 import { SupportIssueService } from './support-issue.service';
 
+enum AutoResponse {
+  MONERO_COMPLETE = 'MoneroComplete',
+  SEPA = 'Sepa',
+}
+
 @Injectable()
 export class SupportIssueJobService {
   constructor(
     private readonly supportIssueRepo: SupportIssueRepository,
     private readonly supportIssueService: SupportIssueService,
     private readonly mailFactory: MailFactory,
+    private readonly settingsService: SettingService,
   ) {}
 
   @DfxCron(CronExpression.EVERY_MINUTE, { process: Process.SUPPORT_BOT, timeout: 1800 })
   async sendAutoResponses() {
-    await this.moneroComplete();
+    const disabledTemplates = await this.settingsService
+      .get('supportBot')
+      .then((s) => (s?.split(',') ?? []) as AutoResponse[]);
+
+    if (!disabledTemplates.includes(AutoResponse.MONERO_COMPLETE)) await this.moneroComplete();
+    if (!disabledTemplates.includes(AutoResponse.SEPA)) await this.sepa();
+  }
+
+  async sepa(): Promise<void> {
+    const issues = await this.getAutoResponseIssues({
+      type: SupportIssueType.TRANSACTION_ISSUE,
+      reason: In([SupportIssueReason.FUNDS_NOT_RECEIVED, SupportIssueReason.TRANSACTION_MISSING]),
+      transactionRequest: { type: TransactionRequestType.BUY },
+      transaction: { id: IsNull() },
+    });
+    if (!issues.length) return;
+
+    const [standard, weekend] = Util.partition(issues, (i) => {
+      const day = i.created.getDay();
+      const hour = i.created.getHours();
+
+      return (day === 2 && hour >= 14) || (day > 2 && day < 5) || (day === 5 && hour < 14);
+    });
+
+    await this.sendAutoResponse(SupportMessageTranslationKey.SEPA_STANDARD, standard);
+    await this.sendAutoResponse(SupportMessageTranslationKey.SEPA_WEEKEND, weekend);
   }
 
   async moneroComplete(): Promise<void> {
-    await this.sendAutoResponse(SupportMessageTranslationKey.MONERO_NOT_DISPLAYED, {
+    const issues = await this.getAutoResponseIssues({
       type: SupportIssueType.TRANSACTION_ISSUE,
       reason: In([SupportIssueReason.FUNDS_NOT_RECEIVED, SupportIssueReason.TRANSACTION_MISSING]),
       transaction: {
@@ -35,20 +68,24 @@ export class SupportIssueJobService {
       },
       created: MoreThan(Util.daysBefore(2)),
     });
+    await this.sendAutoResponse(SupportMessageTranslationKey.MONERO_NOT_DISPLAYED, issues);
   }
 
-  private async sendAutoResponse(
-    translationKey: SupportMessageTranslationKey,
-    where: FindOptionsWhere<SupportIssue>,
-  ): Promise<void> {
-    const entities = await this.supportIssueRepo.find({
+  // --- HELPER METHODS --- //
+  private async getAutoResponseIssues(where: FindOptionsWhere<SupportIssue>): Promise<SupportIssue[]> {
+    return this.supportIssueRepo.find({
       where: {
         state: SupportIssueInternalState.CREATED,
         messages: { author: Not(AutoResponder) },
         ...where,
       },
     });
+  }
 
+  private async sendAutoResponse(
+    translationKey: SupportMessageTranslationKey,
+    entities: SupportIssue[],
+  ): Promise<void> {
     for (const entity of entities) {
       const lang = entity.userData.language.symbol.toLowerCase();
       const message = this.mailFactory.translate(translationKey, lang);
