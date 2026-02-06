a2d0f211e890019db13527be5f9ad40598d4c8fd
Yannick1712 <52333989+Yannick1712@users.noreply.github.com>
2026-02-06 14:51:23 +0100

    [NOTASK] Refactoring

--- src/subdomains/core/buy-crypto/process/services/buy-crypto-preparation.service.ts
+++ src/subdomains/core/buy-crypto/process/services/buy-crypto-preparation.service.ts
@@ -79,7 +79,7 @@ export class BuyCryptoPreparationService {
         cryptoInput: { asset: { balance: true, liquidityManagementRule: true } },
         buy: true,
         cryptoRoute: true,
-        transaction: { user: { wallet: true }, userData: { kycSteps: true } },
+        transaction: { user: { wallet: true }, userData: true },
         bankData: true,
       },
     });
