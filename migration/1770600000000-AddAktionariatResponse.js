47617769193ad3c47600f884821cd343c74ea011
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-02-12 16:51:49 +0100

    fix: use JSDoc typedef pattern in migration file

--- migration/1770600000000-AddAktionariatResponse.js
+++ migration/1770600000000-AddAktionariatResponse.js
@@ -1,13 +1,26 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ * @typedef {import('typeorm').QueryRunner} QueryRunner
+ */
 
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
 module.exports = class AddAktionariatResponse1770600000000 {
-    name = 'AddAktionariatResponse1770600000000'
+  name = 'AddAktionariatResponse1770600000000';
 
-    async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" ADD "aktionariatResponse" nvarchar(MAX)`);
-    }
+  /**
+   * @param {QueryRunner} queryRunner
+   */
+  async up(queryRunner) {
+    await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" ADD "aktionariatResponse" nvarchar(MAX)`);
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" DROP COLUMN "aktionariatResponse"`);
-    }
-}
+  /**
+   * @param {QueryRunner} queryRunner
+   */
+  async down(queryRunner) {
+    await queryRunner.query(`ALTER TABLE "dbo"."transaction_request" DROP COLUMN "aktionariatResponse"`);
+  }
+};
