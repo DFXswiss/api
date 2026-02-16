f8c46e628f25e70caeb490db2e60e4dfbb2b6713
David May <david.leo.may@gmail.com>
2026-02-16 21:08:42 +0100

    fix: RU EUR price

--- src/subdomains/supporting/pricing/services/pricing.service.ts
+++ src/subdomains/supporting/pricing/services/pricing.service.ts
@@ -319,7 +319,8 @@ export class PricingService implements OnModuleInit {
 
   private getDirectPrice(from: Active, to: Active): Promise<Price> {
     try {
-      if ([from.name, to.name].every((n) => ['EUR', 'REALU'].includes(n)))
+      const names = [from.name, to.name];
+      if (names.includes('EUR') && names.includes('REALU'))
         return this.realunitService.getPrice(from.name, to.name);
     } catch {
       return undefined;
