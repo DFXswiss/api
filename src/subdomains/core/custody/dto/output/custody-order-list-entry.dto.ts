6e845feef399c3bda06ad09b7a8a25c6c468f33e
Bernd <bernd@dfx.swiss>
2026-03-16 16:44:02 +0100

    fix: remove fallback values from CustodyOrderListEntry DTO

--- src/subdomains/core/custody/dto/output/custody-order-list-entry.dto.ts
+++ src/subdomains/core/custody/dto/output/custody-order-list-entry.dto.ts
@@ -5,12 +5,12 @@ export class CustodyOrderListEntry {
   id: number;
   type: CustodyOrderType;
   status: CustodyOrderStatus;
-  inputAmount: number;
-  inputAsset: string;
-  outputAmount: number;
-  outputAsset: string;
-  userId: number;
-  userName: string;
+  inputAmount?: number;
+  inputAsset?: string;
+  outputAmount?: number;
+  outputAsset?: string;
+  userId?: number;
+  userName?: string;
   created: Date;
 
   static fromEntity(order: CustodyOrder): CustodyOrderListEntry {
@@ -18,12 +18,12 @@ export class CustodyOrderListEntry {
       id: order.id,
       type: order.type,
       status: order.status,
-      inputAmount: order.inputAmount ?? 0,
-      inputAsset: order.inputAsset?.name ?? '-',
-      outputAmount: order.outputAmount ?? 0,
-      outputAsset: order.outputAsset?.name ?? '-',
-      userId: order.user?.userData?.id ?? 0,
-      userName: order.user?.userData?.verifiedName ?? '-',
+      inputAmount: order.inputAmount,
+      inputAsset: order.inputAsset?.name,
+      outputAmount: order.outputAmount,
+      outputAsset: order.outputAsset?.name,
+      userId: order.user?.userData?.id,
+      userName: order.user?.userData?.verifiedName,
       created: order.created,
     };
   }
