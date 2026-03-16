36f0fe973a9d1c9dd53c71f20d59cb0b67147aa2
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-03-16 18:21:56 +0100

    Fix migration code style to match prettier config

--- migration/1773681554000-UpdateChargebackBankFeeRate.js
+++ migration/1773681554000-UpdateChargebackBankFeeRate.js
@@ -3,17 +3,17 @@ module.exports = class UpdateChargebackBankFeeRate1773681554000 {
 
   async up(queryRunner) {
     await queryRunner.query(`
-      UPDATE "dbo"."fee"
-      SET "rate" = 0.01, "label" = 'Chargeback Bank Fee 1%'
-      WHERE "id" = 112
-    `);
+            UPDATE "dbo"."fee"
+            SET "rate" = 0.01, "label" = 'Chargeback Bank Fee 1%'
+            WHERE "id" = 112
+        `);
   }
 
   async down(queryRunner) {
     await queryRunner.query(`
-      UPDATE "dbo"."fee"
-      SET "rate" = 0.001, "label" = 'Chargeback Bank Fee 0.1%'
-      WHERE "id" = 112
-    `);
+            UPDATE "dbo"."fee"
+            SET "rate" = 0.001, "label" = 'Chargeback Bank Fee 0.1%'
+            WHERE "id" = 112
+        `);
   }
 };
