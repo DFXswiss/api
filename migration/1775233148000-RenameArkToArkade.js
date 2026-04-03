3980ea07b0f96cc3ccf2e4377daeb8d523dc1d7d
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-04-03 18:29:54 +0200

    fix: moduleNameMapper path, description migration, revert old migration
    - Fix Jest moduleNameMapper: ark/ -> arkade/ for @arkade-os/sdk mock
    - Add description column update to rename migration
    - Revert already-run AddArkBtcAsset migration to original state

--- migration/1775233148000-RenameArkToArkade.js
+++ migration/1775233148000-RenameArkToArkade.js
@@ -2,7 +2,7 @@ module.exports = class RenameArkToArkade1775233148000 {
   name = 'RenameArkToArkade1775233148000'
 
   async up(queryRunner) {
-    await queryRunner.query(`UPDATE "dbo"."asset" SET "blockchain" = 'Arkade', "uniqueName" = REPLACE("uniqueName", 'Ark/', 'Arkade/') WHERE "blockchain" = 'Ark'`);
+    await queryRunner.query(`UPDATE "dbo"."asset" SET "blockchain" = 'Arkade', "uniqueName" = REPLACE("uniqueName", 'Ark/', 'Arkade/'), "description" = 'Arkade' WHERE "blockchain" = 'Ark'`);
     await queryRunner.query(`UPDATE "dbo"."user" SET "addressType" = 'Arkade' WHERE "addressType" = 'Ark'`);
     await queryRunner.query(`UPDATE "dbo"."payout_order" SET "chain" = 'Arkade' WHERE "chain" = 'Ark'`);
     await queryRunner.query(`UPDATE "dbo"."liquidity_order" SET "chain" = 'Arkade' WHERE "chain" = 'Ark'`);
@@ -12,7 +12,7 @@ module.exports = class RenameArkToArkade1775233148000 {
   }
 
   async down(queryRunner) {
-    await queryRunner.query(`UPDATE "dbo"."asset" SET "blockchain" = 'Ark', "uniqueName" = REPLACE("uniqueName", 'Arkade/', 'Ark/') WHERE "blockchain" = 'Arkade'`);
+    await queryRunner.query(`UPDATE "dbo"."asset" SET "blockchain" = 'Ark', "uniqueName" = REPLACE("uniqueName", 'Arkade/', 'Ark/'), "description" = 'Ark' WHERE "blockchain" = 'Arkade'`);
     await queryRunner.query(`UPDATE "dbo"."user" SET "addressType" = 'Ark' WHERE "addressType" = 'Arkade'`);
     await queryRunner.query(`UPDATE "dbo"."payout_order" SET "chain" = 'Ark' WHERE "chain" = 'Arkade'`);
     await queryRunner.query(`UPDATE "dbo"."liquidity_order" SET "chain" = 'Ark' WHERE "chain" = 'Arkade'`);
