f7d483ac773a5cf7827676d2b36184ab5c71040c
David May <david.leo.may@gmail.com>
2026-05-26 22:28:08 +0200

    fix: use NULLS NOT DISTINCT on both indexes instead of WHERE clause for nationality

--- migration/1779812989037-FixNullableUniqueIndexes.js
+++ migration/1779812989037-FixNullableUniqueIndexes.js
@@ -18,9 +18,9 @@ module.exports = class FixNullableUniqueIndexes1779812989037 {
         await queryRunner.query(`DROP INDEX "IDX_3a1150791476264753a67212a1"`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_3a1150791476264753a67212a1" ON "kyc_step" ("userDataId", "name", "type", "sequenceNumber") NULLS NOT DISTINCT`);
 
-        // user_data: add nationalityId IS NOT NULL to WHERE clause
+        // user_data: recreate unique index with NULLS NOT DISTINCT so NULL nationality is treated as equal
         await queryRunner.query(`DROP INDEX "IDX_99da8fce0c522a35d93b9499f4"`);
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_99da8fce0c522a35d93b9499f4" ON "user_data" ("identDocumentId", "nationalityId", "accountType", "kycType") WHERE "identDocumentId" IS NOT NULL AND "accountType" IS NOT NULL AND "kycType" IS NOT NULL AND "nationalityId" IS NOT NULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_99da8fce0c522a35d93b9499f4" ON "user_data" ("identDocumentId", "nationalityId", "accountType", "kycType") NULLS NOT DISTINCT WHERE "identDocumentId" IS NOT NULL AND "accountType" IS NOT NULL AND "kycType" IS NOT NULL`);
     }
 
     /**
--- src/subdomains/generic/user/models/user-data/user-data.entity.ts
+++ src/subdomains/generic/user/models/user-data/user-data.entity.ts
@@ -54,8 +54,7 @@ import {
   (userData: UserData) => [userData.identDocumentId, userData.nationality, userData.accountType, userData.kycType],
   {
     unique: true,
-    where:
-      '"identDocumentId" IS NOT NULL AND "accountType" IS NOT NULL AND "kycType" IS NOT NULL AND "nationalityId" IS NOT NULL',
+    where: '"identDocumentId" IS NOT NULL AND "accountType" IS NOT NULL AND "kycType" IS NOT NULL',
   },
 )
 export class UserData extends IEntity {
